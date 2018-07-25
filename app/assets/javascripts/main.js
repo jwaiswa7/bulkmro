f57770c3542f3aa4291b0b96eff0ff07d57ed45d
= <=>
2018-07-25 17:08:08 +0530

    Added support for listing the number of companies and contacts to the accounts listing page. Right aligned companies and contacts. Added the ability to add contact and company from accounts' index page. Removed sorting for the action bar. Fixed bootstrap validations by using jQuery to confirm DOM has loaded.

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -41,7 +41,7 @@ main = {
 
     // Bootstrap override default browser validation with Bootstrap's helper classes
     initBootstrapValidations: function() {
-        window.addEventListener('load', function() {
+        $(document).ready(function() {
             // Fetch all the forms we want to apply custom Bootstrap validation styles to
             var forms = document.getElementsByClassName('needs-validation');
             // Loop over them and prevent submission
