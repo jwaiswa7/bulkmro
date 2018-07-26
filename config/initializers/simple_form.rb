dfd6652ffd4d65c22f4dd72736e88452c969ed18
= <=>
2018-07-26 15:10:13 +0530

    Added dynamic labels for the new company and new account page. Made browser validations required for simple_form_for. Removed the required asterisk in the simple_form_for config. Added the sign out button for the desktop and mobile naviation bars. Added tooltip support for the new company and new contact buttons on the listing for accounts.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -112,6 +112,9 @@ SimpleForm.setup do |config|
   # How the label text should be generated altogether with the required text.
   # config.label_text = lambda { |label, required, explicit_label| "#{required} #{label}" }
 
+  # Removing the asterisk for required fields
+  config.label_text = lambda { |label, required, explicit_label| "#{label}" }
+
   # You can define the class to use on all labels. Default is nil.
   # config.label_class = nil
 
@@ -130,7 +133,7 @@ SimpleForm.setup do |config|
   # in this configuration, which is recommended due to some quirks from different browsers.
   # To stop SimpleForm from generating the novalidate option, enabling the HTML5 validations,
   # change this configuration to true.
-  config.browser_validations = false
+  config.browser_validations = true
 
   # Collection of methods to detect if a file type was given.
   # config.file_methods = [ :mounted_as, :file?, :public_filename, :attached? ]
