1265d1f85f61ea33d4f965810c9c62fc4ea2e7b9
= <=>
2018-07-26 16:48:34 +0530

    Created a custom layout for sign_in. Added support for custom checkbox to sign in page. Added sysadmin mailto link for people who can't sign in. Added placeholders for email address and password fields. Added a hidden button for 'Continue with Google'. Linked the BMRO logo for linking the company logo.

--- app/controllers/overseers/sessions_controller.rb
+++ app/controllers/overseers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class Overseers::SessionsController < Devise::SessionsController
-  layout 'overseers/layouts/application'
+  layout 'overseers/layouts/sign_in'
+
   private
   def after_sign_out_path_for(resource_or_scope)
     new_overseer_session_path
