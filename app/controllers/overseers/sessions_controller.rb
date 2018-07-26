61fa7495a0e6eea66510c4f98dd61440f989d7f7
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-07-26 11:34:13 +0530

    Temp fix for login page.

--- app/controllers/overseers/sessions_controller.rb
+++ app/controllers/overseers/sessions_controller.rb
@@ -1,5 +1,5 @@
 class Overseers::SessionsController < Devise::SessionsController
-
+  layout 'overseers/layouts/application'
   private
   def after_sign_out_path_for(resource_or_scope)
     new_overseer_session_path
