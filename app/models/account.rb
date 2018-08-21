45d6a0f5d622a3529dd7b7b6d6062ad6dd10375a
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-08-21 18:05:17 +0530

    Add alias to account, restrict size to 20 characters.

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,6 +4,10 @@ class Account < ApplicationRecord
 
   pg_search_scope :locate, :against => [:name], :associated_against => { }, :using => { :tsearch => {:prefix => true} }
 
+  validates_presence_of :alias
+  validates_uniqueness_of :alias
+  validates_length_of :alias, :maximum => 20
+
   has_many :companies
   has_many :contacts
 end
--- db/migrate/20180710132226_create_accounts.rb
+++ db/migrate/20180710132226_create_accounts.rb
@@ -2,6 +2,7 @@ class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
       t.string :name, index: { :unique => true }
+      t.string :alias, index: { :unique => true }
 
       t.timestamps
       t.userstamps
