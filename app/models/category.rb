fc30603241298b87a2c2d857372539a5921bf022
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-08-22 09:48:28 +0530

    Add tax_code ref to category, tax code must be present if category is a child.

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,10 +4,12 @@ class Category < ApplicationRecord
   pg_search_scope :locate, :against => [:name], :associated_against => { }, :using => { :tsearch => {:prefix => true} }
   has_closure_tree
 
+  belongs_to :tax_code, required: false
   has_many :category_suppliers
   has_many :suppliers, :through => :category_suppliers
 
   validates_presence_of :name
+  validates_presence_of :tax_code, :if => :child?
 
   def to_s
     ancestry_path.join(' > ')
