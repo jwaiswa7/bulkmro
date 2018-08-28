0d223d8b6b8d2f04aa7b2ba9385f554858e5325d
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2018-08-28 11:00:40 +0530

    Add Support for reading excel and saving valid products to the Inquiry. Add validation for imported excel to remove blank values

--- app/services/services/overseers/inquiries/excel_importer.rb
+++ app/services/services/overseers/inquiries/excel_importer.rb
@@ -1,4 +1,5 @@
 class Services::Overseers::Inquiries::ExcelImporter < Services::Shared::BaseService
+  include Rails.application.routes.url_helpers
   # RandomRecords.for(Product, 10).each do |p| puts "#{p.sku}, 5"; end
   def initialize(inquiry, excel_import)
     @inquiry = inquiry
@@ -10,25 +11,32 @@ class Services::Overseers::Inquiries::ExcelImporter < Services::Shared::BaseServ
 
   def call
     if excel_import.save
-      # loop_and_set_products
-      #
-      # ActiveRecord::Base.transaction do
-      #   add_existing_products_to_inquiry
-      #   log_failed_skus
-      # end
+
+      excel_items = get_excel_items(excel_import)
+      columns = ["id", "name", "brand", "mpn", "sku", "quantity"]
+      if (excel_items.first.first.downcase == 'id')
+        columns = excel_items.shift
+      end
+      columns = columns.map(&:downcase)
+      #raise()
+      loop_and_set_products(excel_items, columns)
+      ActiveRecord::Base.transaction do
+        add_existing_products_to_inquiry
+        log_failed_skus
+      end
     end
 
     true
   end
 
-  def loop_and_set_products
-    excel_import.import_text.split("\n").each do |excel_item|
-      tuples = excel_item.delete(' ').gsub(/[\r\n]/,'').split(',')
+  def loop_and_set_products(excel_items, columns)
 
-      sku = tuples[0]
-      quantity = tuples.length > 1 ? tuples[1] : 1
-
-      add_product(sku, quantity)
+    excel_items.each do |excel_item|
+      unless excel_item[1].nil?
+        sku = excel_item[columns.index('sku')]
+        quantity = excel_item[columns.index('quantity')]
+        add_product(sku, quantity)
+      end
     end
   end
 
@@ -52,6 +60,14 @@ class Services::Overseers::Inquiries::ExcelImporter < Services::Shared::BaseServ
     excel_import.update_attributes(
         failed_skus: failed_skus
     )
+  end
+
+  def get_excel_items(excel_import)
+    doc = SimpleXlsxReader.open(ActiveStorage::Blob.service.send(:path_for, excel_import.file.key))
+
+    excel_items = doc.sheets.first.rows
+    # Remove Empty or Blank rows from the
+    excel_items = excel_items.map {|e| e.instance_of?(Array) ? e.reject(&:blank?).collect {|ei| ei.to_s.strip} : e}.reject(&:empty?)
   end
 
   attr_accessor :inquiry, :excel_import, :existing_products, :failed_skus
