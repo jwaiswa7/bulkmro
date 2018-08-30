b3fe523aee1edf804aa5941a18e302fe1cdd5cc4
Ashwin Goyal <mail.ashwingoyal@gmail.com>
2018-08-30 09:47:51 +0000

    routes.rb edited online with Bitbucket

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root :to => 'overseers/dashboard#show'
   get '/overseers', to: redirect('/overseers/dashboard'), as: 'overseer_root'
 
-  devise_for :overseers, controllers: {sessions: 'overseers/sessions'}
+  devise_for :overseers, controllers: { sessions: 'overseers/sessions' }
 
   namespace 'overseers' do
     resource :dashboard, :controller => :dashboard
