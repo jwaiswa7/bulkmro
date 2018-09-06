2e522a247ce99edae9dd9144830f7696c997e2ad
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-09-06 18:03:27 +0530

    Update import policy.

--- app/policies/overseers/inquiry_import_policy.rb
+++ app/policies/overseers/inquiry_import_policy.rb
@@ -1,6 +1,6 @@
 class Overseers::InquiryImportPolicy < Overseers::ApplicationPolicy
   def manage_failed_skus?
-    record.rows.failed.any?
+    record.rows.failed.any? && record.excel?
   end
 
   def create_failed_skus?
