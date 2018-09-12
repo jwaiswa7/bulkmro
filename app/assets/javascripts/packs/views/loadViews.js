eed39917681871e46ef80c169cfa4477d78a1807
Amit Goyal <forsakenlegacy@gmail.com>
2018-09-12 15:54:43 +0530

    Removed console.log from loadViews.

--- app/assets/javascripts/packs/views/loadViews.js
+++ app/assets/javascripts/packs/views/loadViews.js
@@ -39,8 +39,6 @@ let camelize = function(text) {
         return '' + word.slice(0, 1).toUpperCase() + word.slice(1).toLowerCase();
     }));
 
-    console.log(camelized);
-
     return camelized.join('');
 };
 
