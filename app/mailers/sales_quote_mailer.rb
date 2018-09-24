6df5966fbf196155e5e15cd377f64d71cfcc9b04
Sandesh Raut <sandesh.raut@bulkmro.com>
2018-09-24 20:09:43 +0530

    Attachment using file path(tried to upload attachment using url)

--- app/mailers/sales_quote_mailer.rb
+++ app/mailers/sales_quote_mailer.rb
@@ -1,14 +1,15 @@
 class SalesQuoteMailer < ApplicationMailer
   before_action { @inquiry = params[:inquiry], @quote = params[:quote], @customer= Contact.find(params[:inquiry][:contact_id])  }
 
-  # after_action :set_delivery_options
+  #after_action :set_delivery_options
   #
   default from:   -> { params[:overseer][:email] },
           to:     -> { @customer.email }
           # reply_to: -> { @inviter.email_address_with_name }
   def sales_quote_ack
     subject =  "Quote Request #" + @quote.id.to_s + " : {{var caption}}"
-    #attachments['filename.jpg'] = File.read('/path/to/filename.jpg')
+    file = Rails.root + "app/assets/images/26159_Calc_Sheet.pdf"
+    attachments['salequote.pdf'] = File.open(file, 'rb'){|f| f.read}
     mail subject: subject
   end
 
