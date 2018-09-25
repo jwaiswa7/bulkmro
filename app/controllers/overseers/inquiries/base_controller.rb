77f38bc1dd6de4d0240f4bab150c9d57e3a9c3fc
Sandesh Raut <sandesh.raut@bulkmro.com>
2018-09-25 17:15:30 +0530

    SMTP cred validation and typo in csv fixed

--- app/controllers/overseers/inquiries/base_controller.rb
+++ app/controllers/overseers/inquiries/base_controller.rb
@@ -1,8 +1,15 @@
 class Overseers::Inquiries::BaseController < Overseers::BaseController
-  before_action :set_inquiry
+  before_action :set_inquiry, :check_smtp
 
   private
   def set_inquiry
     @inquiry = Inquiry.find(params[:inquiry_id])
   end
+
+  def check_smtp
+    if !GmailSmtp.exists?(email: current_overseer.email) || !GmailSmtp.where(:email => current_overseer.email).first.password.present?
+        flash[:error] = "Your SMTP is not Valid. Email will not be sent. Please contact web admin"
+    end
+  end
+
 end
