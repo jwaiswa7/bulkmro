b86cfce622cefa72bfb352f703665f2954327ed5
Amit Goyal <forsakenlegacy@gmail.com>
2018-09-25 12:21:16 +0530

    Added support for entity, intent, wit/number and wit/greetings for parsed queries. Added baseline support for replying back with messages using the console. Changed the defined learned intelligence to accomodate entities and intent.

--- sita.rb
+++ sita.rb
@@ -2,6 +2,9 @@ require 'wit'
 require_relative './config/boot'
 require_relative './config/environment'
 
+client = Wit.new(access_token: Settings.wit.auth_token)
+client.interactive
+
 def first_entity_value(entities, entity)
   return nil unless entities.has_key? entity
   val = entities[entity][0]['value']
@@ -11,13 +14,14 @@ end
 
 def handle_message(response)
   entities = response['entities']
+  entity = first_entity_value(entities, 'entity')
   intent = first_entity_value(entities, 'intent')
   greetings = first_entity_value(entities, 'greetings')
   number = first_entity_value(entities, 'number')
 
   case
   when intent
-    return data(intent, number)
+    return data(entity, intent, number)
   when greetings
     return "Hi! Try something like 'What's the status of order 29313?'"
   else
@@ -25,8 +29,8 @@ def handle_message(response)
   end
 end
 
-def data(intent, variable)
-  return "You want to '#{intent}' for '#{variable}'"
+def data(intent, entity, variable)
+  return "You want to '#{intent}' for '#{variable}'? " + Inquiry.find(variable).to_s + "!"
 end
 
 client = Wit.new(access_token: Settings.wit.auth_token)
