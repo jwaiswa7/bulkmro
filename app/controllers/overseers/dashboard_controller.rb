4a5bf12a7ae8d285fa3abce10ec96dea458ad0ca
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-09-27 18:04:59 +0530

    Add elasticsearch, chewy, make index, update index.json view, add indexer, add finder class to inquiries.

--- app/controllers/overseers/dashboard_controller.rb
+++ app/controllers/overseers/dashboard_controller.rb
@@ -1,5 +1,17 @@
 class Overseers::DashboardController < Overseers::BaseController
 	def show
 		authorize :dashboard, :show?
+
+	end
+
+	def chewy
+		# InquiryIndex.delete
+		# InquiryIndex.create!
+		# InquiryIndex.import
+		InquiryIndex.reset!
+		#
+		Inquiry.search('asd')
+
+		raise
 	end
 end
