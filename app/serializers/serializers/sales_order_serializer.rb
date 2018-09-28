a23328e3af37a90d93165adad056a16dce6d4df6
pradeep <pbketkale@gmail.com>
2018-09-28 20:33:29 +0530

    Serializer: Sales Quote Rows, Sales Order Rows

--- app/serializers/serializers/sales_order_serializer.rb
+++ app/serializers/serializers/sales_order_serializer.rb
@@ -1,4 +1,4 @@
 class Serializers::SalesOrderSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :sales_order_rows, :quantity
+  has_many :rows
 end
--- app/serializers/serializers/sales_quote_serializer.rb
+++ app/serializers/serializers/sales_quote_serializer.rb
@@ -1,4 +1,6 @@
 class Serializers::SalesQuoteSerializer
   include FastJsonapi::ObjectSerializer
   attribute :rows
+
+  has_many :sales_orders, serializer: Serializers::SalesOrderSerializer
 end
