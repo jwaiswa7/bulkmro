fd0e337abe73310c01b1cc7f5567a95d8effcdaa
Sandesh Raut <sandesh.raut@bulkmro.com>
2018-09-28 19:24:21 +0530

    Sales order PDF template created and fixed tax calulation as well

--- app/controllers/overseers/sales_orders_controller.rb
+++ app/controllers/overseers/sales_orders_controller.rb
@@ -7,22 +7,13 @@ class Overseers::SalesOrdersController < Overseers::BaseController
 
   def show_pdf
 
-    @sales_order = SalesOrder.find(391)
+    @sales_order = SalesOrder.find(params[:id])
     authorize @sales_order
-
-    render pdf: 'show', template: 'overseers/sales_orders/show.pdf.erb'
-    # respond_to do |format|
-    #   format.html
-    #   format.pdf do
-    #      render pdf: 'show.pdf', template: 'overseers/sales_orders/show.html.erb'
-    #   end
-    # end
-    # raise
-    # render pdf: 'show.pdf', template: 'overseers/sales_orders/show'
-
-    # respond_to do |format|
-    #   format.pdf { render pdf: 'show.pdf' }
-    # end
+    #render pdf: 'show', template: 'overseers/sales_orders/show.pdf.erb'
+    #  <%= link_to "Test PDF", booking_path(current_user.bookings.last, format: :pdf) %>
+    respond_to do |format|
+      format.pdf { render pdf: 'show', template: 'overseers/sales_orders/show.pdf.erb' }
+    end
   end
 
 end
