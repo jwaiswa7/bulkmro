b2746cf69fdbe0272d56f40fa7927bf8f6d4c3fa
Sandesh Raut <sandesh.raut@bulkmro.com>
2018-10-05 13:19:26 +0530

    Clock work job added for everyday 6am.

--- clock.rb
+++ clock.rb
@@ -2,11 +2,17 @@ require 'clockwork'
 include Clockwork
 require './config/boot'
 require './config/environment'
+require 'active_support/time'
 
 handler do |job, time|
   puts "Running #{job}, at #{time}"
 end
 
-every(15.seconds, 'do_something') do
+# every(15.seconds, 'do_something') do
+#
+# end
 
-end
+# every(15.seconds, CurrencyRate.save_fx_rate)
+
+
+every(1.day, CurrencyRate.save_fx_rate, :at => '06:00')
