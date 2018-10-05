3cd4de9df852f6e550b793a3306aacca26b8570f
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2018-10-05 12:04:24 +0530

    Hide Hide button in Sales Quote Form

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,7 @@ class Product < ApplicationRecord
   include Mixins::HasComments
 
   update_index('products#product') { self if self.approved? }
-  pg_search_scope :locate, :against => [[:sku, "A"], [:name, "B"]], :using => { :tsearch => { :prefix => true, :any_word => false } }
+  pg_search_scope :locate, :against => [ [:name, "B"],[:sku, "A"]], :using => { :tsearch => { :prefix => true, :any_word => true } }
   #pg_search_scope :locate, :against => [:sku, :name], :associated_against => { brand: [:name] }, :using => { :tsearch => { :prefix => true } }
 
   belongs_to :brand, required: false
