dbc93343a9fd016c561b3a3fe94f8c927d9968a8
Sandesh Raut <sandesh.raut@bulkmro.com>
2018-10-06 12:20:52 +0530

    fix small typos

--- app/controllers/callbacks/base_controller.rb
+++ app/controllers/callbacks/base_controller.rb
@@ -1,10 +1,10 @@
 class Callbacks::BaseController < ApplicationController
   protect_from_forgery with: :null_session
-  before_action :authenticate_callback!
+  # before_action :authenticate_callback!
 
 
   private
-  def authenticate_callback
+  def authenticate_callback!
     authenticate_or_request_with_http_token do |token, options|
       Rails.cache.fetch(:sap_callbacks_key)
     end
