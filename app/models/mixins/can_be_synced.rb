6ad69f6999179cf10824e2f56bd45ea32df3da60
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-10-13 11:53:52 +0530

    Disable SAP in production.

--- app/models/mixins/can_be_synced.rb
+++ app/models/mixins/can_be_synced.rb
@@ -5,9 +5,10 @@ module Mixins::CanBeSynced
     def save_and_sync
       if Rails.env.development?
         self.save
-      else
-        service = ['Services', 'Resources', self.class.name.pluralize, 'SaveAndSync'].join('::').constantize.new(self)
-        service.call
+      elsif Rails.env.production?
+        self.save
+        # service = ['Services', 'Resources', self.class.name.pluralize, 'SaveAndSync'].join('::').constantize.new(self)
+        # service.call
       end
     end
 
