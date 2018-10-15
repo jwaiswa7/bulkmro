ba6e57aab94cc8944528df0dfb56b1cbbefdb1f0
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-10-15 12:50:13 +0530

    Update pdf views.

--- app/controllers/overseers/inquiries/purchase_orders_controller.rb
+++ app/controllers/overseers/inquiries/purchase_orders_controller.rb
@@ -7,31 +7,31 @@ class Overseers::Inquiries::PurchaseOrdersController < Overseers::Inquiries::Bas
   end
 
   def show
-    @inquiry = Inquiry.find_by_inquiry_number(params[:PoEnquiryId])
-    @sales_purchase_order = SalesPurchaseOrder.create!(po_uid: params[:PoNum], inquiry: @inquiry, request_payload: params)
-
-    @po = @sales_purchase_order.request_payload.deep_symbolize_keys
-    @supplier_billing = Address.where(remote_uid: @po[:PoSupBillFrom]).first
-    @supplier_shipping = Address.where(remote_uid: @po[:PoSupShipFrom]).first
-
-    @po[:packing] = @po[:PoShippingCost].to_f > 0 ? @po[:PoShippingCost].to_f + ' Amount Extra' : 'Included'
-    @po[:item_subtotal] = 0
-    @po[:item_tax_amount] = 0
-    @po[:item_subtotal_incl_tax] = 0
-
-    @po[:ItemLine].each do |item|
-      @product = Product.find_by_sku(item[:PopProductSku])
-      item[:uom] = @product.measurement_unit.name
-      item[:sku] = @product.sku
-      item[:row_total] = item[:PopPriceHt].to_f * item[:PopQty].to_f
-      item[:tax_rate] = @product.tax_code.tax_percentage
-      item[:tax_amount] = (item[:row_total].to_f * item[:tax_rate] / 100).round(2)
-      item[:row_total_incl_tax] = item[:row_total].to_f + item[:tax_amount].to_f
-      @po[:item_subtotal] += item[:row_total]
-      @po[:item_tax_amount] += item[:tax_amount]
-      @po[:item_subtotal_incl_tax] += item[:row_total_incl_tax]
-    end
-    @sales_purchase_order.update_attributes(request_payload: @po)
+    # @inquiry = Inquiry.find_by_inquiry_number(params[:PoEnquiryId])
+    # @sales_purchase_order = SalesPurchaseOrder.create!(po_uid: params[:PoNum], inquiry: @inquiry, request_payload: params)
+    #
+    # @po = @sales_purchase_order.request_payload.deep_symbolize_keys
+    # @supplier_billing = Address.where(remote_uid: @po[:PoSupBillFrom]).first
+    # @supplier_shipping = Address.where(remote_uid: @po[:PoSupShipFrom]).first
+    #
+    # @po[:packing] = @po[:PoShippingCost].to_f > 0 ? @po[:PoShippingCost].to_f + ' Amount Extra' : 'Included'
+    # @po[:item_subtotal] = 0
+    # @po[:item_tax_amount] = 0
+    # @po[:item_subtotal_incl_tax] = 0
+    #
+    # @po[:ItemLine].each do |item|
+    #   @product = Product.find_by_sku(item[:PopProductSku])
+    #   item[:uom] = @product.measurement_unit.name
+    #   item[:sku] = @product.sku
+    #   item[:row_total] = item[:PopPriceHt].to_f * item[:PopQty].to_f
+    #   item[:tax_rate] = @product.tax_code.tax_percentage
+    #   item[:tax_amount] = (item[:row_total].to_f * item[:tax_rate] / 100).round(2)
+    #   item[:row_total_incl_tax] = item[:row_total].to_f + item[:tax_amount].to_f
+    #   @po[:item_subtotal] += item[:row_total]
+    #   @po[:item_tax_amount] += item[:tax_amount]
+    #   @po[:item_subtotal_incl_tax] += item[:row_total_incl_tax]
+    # end
+    # @sales_purchase_order.update_attributes(request_payload: @po)
 
     authorize @purchase_order
 
@@ -41,6 +41,11 @@ class Overseers::Inquiries::PurchaseOrdersController < Overseers::Inquiries::Bas
         render_pdf_for @purchase_order
       end
     end
+  end
+
+  private
+  def set_purchase_order
+    @purchase_order = @inquiry.purchase_orders.find(params[:id])
   end
 end
 
--- app/controllers/overseers/inquiries/sales_invoices_controller.rb
+++ app/controllers/overseers/inquiries/sales_invoices_controller.rb
@@ -7,21 +7,21 @@ class Overseers::Inquiries::SalesInvoicesController < Overseers::Inquiries::Base
   end
 
   def show
-    @sales_order = SalesOrder.find(params[:order_id])
-    @sales_invoice = SalesInvoice.create!(invoice_uid: params[:increment_id], sales_order: @sales_order, request_payload: params)
-    @invoice = @sales_invoice.request_payload.deep_symbolize_keys
-    @invoice[:ItemLine].each do |item|
-      @sales_order.rows.each do |row|
-        if row.sales_quote_row.product.sku == item[:sku]
-          item[:remote_name] = item[:name]
-          item[:name] = row.sales_quote_row.to_bp_catalog_s
-          item[:uom] = row.sales_quote_row.product.measurement_unit.name
-          item[:hsn] =  @sales_order.sales_quote.is_sez ? row.tax_code.chapter : row.tax_code.code
-          item[:tax_rate] = row.tax_code.tax_percentage
-        end
-      end
-    end
-    @sales_invoice.update_attributes(request_payload: @invoice)
+    # @sales_order = SalesOrder.find(params[:order_id])
+    # @sales_invoice = SalesInvoice.create!(invoice_uid: params[:increment_id], sales_order: @sales_order, request_payload: params)
+    # @invoice = @sales_invoice.request_payload.deep_symbolize_keys
+    # @invoice[:ItemLine].each do |item|
+    #   @sales_order.rows.each do |row|
+    #     if row.sales_quote_row.product.sku == item[:sku]
+    #       item[:remote_name] = item[:name]
+    #       item[:name] = row.sales_quote_row.to_bp_catalog_s
+    #       item[:uom] = row.sales_quote_row.product.measurement_unit.name
+    #       item[:hsn] =  @sales_order.sales_quote.is_sez ? row.tax_code.chapter : row.tax_code.code
+    #       item[:tax_rate] = row.tax_code.tax_percentage
+    #     end
+    #   end
+    # end
+    # @sales_invoice.update_attributes(request_payload: @invoice)
 
     authorize @sales_invoice
 
