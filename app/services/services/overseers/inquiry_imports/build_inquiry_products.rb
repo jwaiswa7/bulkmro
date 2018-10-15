37f80d54436a0508d8d75b5899395417e1a557d6
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2018-10-15 13:47:21 +0530

    Fixed Sr No new and Old reset issue Update inquiry for attachment issue

--- app/services/services/overseers/inquiry_imports/build_inquiry_products.rb
+++ app/services/services/overseers/inquiry_imports/build_inquiry_products.rb
@@ -5,12 +5,22 @@ class Services::Overseers::InquiryImports::BuildInquiryProducts < Services::Shar
   end
 
   def call
+    serial_numbers = inquiry.inquiry_products.pluck(:sr_no)
+
     excel_import.rows.each do |row|
       if row.failed?
+
+        id = row.metadata['id']
+        if serial_numbers.include?(id) || id <= serial_numbers.map(&:to_i).max
+          id = serial_numbers.map(&:to_i).max + 1
+          serial_numbers << id
+        else
+          serial_numbers << id
+        end
         row.build_inquiry_product(
             inquiry: inquiry,
             import: excel_import,
-            :sr_no => row.metadata['id'],
+            :sr_no => id,
             product: Product.new(
                 inquiry_import_row: row,
                 name: row.metadata['name'],
