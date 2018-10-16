dfd5cb2afd408499a573ced2bba4dc1f3fe8f85f
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-10-16 16:25:49 +0530

    Refactor sales order.

--- app/services/services/callbacks/sales_orders/create.rb
+++ app/services/services/callbacks/sales_orders/create.rb
@@ -1,54 +1,45 @@
 class Services::Callbacks::SalesOrders::Create < Services::Shared::BaseService
 
-  def initialize(params)
-    @params = params
-  end
-
   def call
-    #legacy_id = params['U_MgntDocID']
     sprint_order_id = params['U_MgntDocID']
     remote_status = params['Status']
     remote_comment = params['comment']
     remote_uid = params['DocEntry']
     order_number = params['DocNum']
-    comment_message = comment_message_for(remote_status, remote_comment)
-    sales_order = SalesOrder.find(sprint_order_id) # || SalesOrder.find_by_legacy_id(legacy_id)
+    comment_message = [
+        ['SAP Status Updated', remote_status].join(': '),
+        ['Comment', remote_comment].join(': '),
+    ].join(' | ')
+
+    sales_order = SalesOrder.find(sprint_order_id)
 
     if sales_order.present?
-
       case to_local_status(remote_status)
       when :'approved'
         if sales_order.approved?
-          {success: 1, status: 1, message: "Order Already Approved"}
-        end
-
-        if sales_order.remote_status.blank?
+          set_response("Order Already Approved")
+        elsif sales_order.remote_status.blank?
           begin
-            sales_order.update_attributes(:remote_status => :'Supplier PO: Request Pending' , :status => :'approved', :order_number => order_number, :remote_uid => remote_uid)
+            sales_order.update_attributes(:remote_status => :'Supplier PO: Request Pending', :status => :'approved', :order_number => order_number, :remote_uid => remote_uid)
             sales_order.inquiry.comments.create!(message: "SAP Approved", overseer: Overseer.default_approver)
-            {success: 1, status: 1, message: "Order Created Successfully"} #, response: sales_order.rows.to_json
-          rescue => ex
-            {success: 1, status: 1, message: ex.message}
+            set_response("Order Created Successfully")
+          rescue => e
+            set_response(e.message, 0)
           end
         end
-
       when :'SAP Rejected'
-        # if sales_order.status.in? ['Hold by Finance', 'SAP Rejected']
-        #   {success: 1, status: 1, message: "Order Already Approved"}
-        # end
         begin
           sales_order.update_attributes(:status => :'SAP Rejected')
           comment = sales_order.inquiry.comments.create!(message: comment_message, overseer: Overseer.default_approver)
           sales_order.create_rejection!(:comment => comment, :overseer => Overseer.default_approver)
           sales_order.approval.destroy!
-          {success: 1, status: 1, message: "Order Rejected Successfully"}
-        rescue => ex
-          {success: 0, status: 0, message: ex.message}
+          set_response("Order Rejected Successfully")
+        rescue => e
+          set_response(e.message, 0)
         end
       end
-
     else
-      {success: 0, status: 0, message: "Order Not Processed"}
+      set_response("Order Not Processed", 0)
     end
   end
 
@@ -62,15 +53,6 @@ class Services::Callbacks::SalesOrders::Create < Services::Shared::BaseService
       :'SAP Rejected'
     end
   end
-
-  def comment_message_for(remote_status, remote_comment)
-    [
-        ['SAP Status Updated', remote_status].join(': '),
-        ['Comment', remote_comment].join(': '),
-    ].join('\n')
-  end
-
-  attr_accessor :params
 end
 
 # {
