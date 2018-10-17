4e517e8ad9af15b141243f40300498a578614bb3
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-10-17 13:47:00 +0530

    Fix callbase.

--- app/services/services/overseers/finders/base_finder.rb
+++ app/services/services/overseers/finders/base_finder.rb
@@ -26,13 +26,13 @@ class Services::Overseers::Finders::BaseFinder < Services::Shared::BaseService
   end
 
   def call_base
-    @indexed_records = if query_string.present?
+    non_paginated_records = if query_string.present?
                          perform_query(query_string)
                        else
                          all_records
-                       end.page(page).per(per)
-
-    @records = model_klass.where(:id => indexed_records.pluck(:id)).with_includes
+                         end
+    @indexed_records = non_paginated_records.page(page).per(per) if non_paginated_records.present?
+    @records = model_klass.where(:id => indexed_records.pluck(:id)).with_includes if indexed_records.present?
   end
 
 
