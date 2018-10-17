9257d8b43666be3ed5190e6cfd23c32bc7307162
pradeep <pbketkale@gmail.com>
2018-10-17 14:35:06 +0530

    SAP Callback: Fixed error in response

--- app/services/services/callbacks/shared/base_callback.rb
+++ app/services/services/callbacks/shared/base_callback.rb
@@ -2,16 +2,10 @@ class Services::Callbacks::Shared::BaseCallback < Services::Shared::BaseService
 
   def initialize(params)
     @params = params
-
-    @response = {
-        success: nil,
-        status: nil,
-        message: nil
-    }
   end
 
   def set_response(message, status=1)
-    response.merge({ success: status, status: status, message: message})
+    { success: status, status: status, message: message}
   end
 
   attr_accessor :params
