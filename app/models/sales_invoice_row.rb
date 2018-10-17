d4b3d75339684c463d0691356e0b09dfa554b44f
Sandesh Raut <sandesh.raut@bulkmro.com>
2018-10-17 18:56:59 +0530

    sales invoice item calculations

--- app/models/sales_invoice_row.rb
+++ app/models/sales_invoice_row.rb
@@ -1,3 +1,37 @@
 class SalesInvoiceRow < ApplicationRecord
   belongs_to :sales_invoice
+
+  def sku
+    self.metadata['sku']
+  end
+
+  def quantity
+    self.metadata['qty']
+  end
+
+  def hsn
+    get_product.tax_code.chapter
+  end
+
+  def name
+    get_product.to_bp_catalog_s
+  end
+
+  def uom
+    get_product.measurement_unit.name if get_product.measurement_unit.present?
+  end
+
+  def brand
+    get_product.product.brand.name if get_product.product.brand.present?
+  end
+
+  def tax_rate
+    get_product.tax_code.tax_percentage
+  end
+
+  private
+  def get_product
+    sales_invoice.sales_order.sales_quote.rows.select { | supplier_row | supplier_row.product.sku == self.sku}.first
+  end
+
 end
