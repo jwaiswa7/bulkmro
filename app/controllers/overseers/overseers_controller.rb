2d07ceadf6a2b42969d10b44b0b7ea5baedef000
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-10-19 12:02:56 +0530

    Update approved alternatives.'

--- app/controllers/overseers/overseers_controller.rb
+++ app/controllers/overseers/overseers_controller.rb
@@ -2,7 +2,7 @@ class Overseers::OverseersController < Overseers::BaseController
   before_action :set_overseer, only: [:edit, :update]
 
   def index
-    @overseers = ApplyDatatableParams.to(Overseer.all, params)
+    @overseers = ApplyDatatableParams.to(Overseer.all.except_object(current_overseer), params)
     authorize @overseers
   end
 
