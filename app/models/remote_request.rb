e534b1611dec78b1b155ebfddb28d7b2dd26062b
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-10-19 16:18:20 +0530

    Fix remote request search.

--- app/models/remote_request.rb
+++ app/models/remote_request.rb
@@ -2,4 +2,6 @@ class RemoteRequest < ApplicationRecord
   include Mixins::CanBeStamped
   include Mixins::IsARequest
 
+  pg_search_scope :locate, :against => [:url], :associated_against => {}, :using => { :tsearch => { :prefix => true } }
+
 end
