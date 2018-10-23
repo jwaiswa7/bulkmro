133d9ad222717668a745ccfb55f597c3e29fa8ea
Amit Goyal <forsakenlegacy@gmail.com>
2018-10-23 13:44:05 +0530

    Removed test code that added Lopesh to the inquiries' controller smart queue method.

--- app/controllers/overseers/inquiries_controller.rb
+++ app/controllers/overseers/inquiries_controller.rb
@@ -33,7 +33,6 @@ class Overseers::InquiriesController < Overseers::BaseController
 
   def smart_queue
     @inquiries = ApplyDatatableParams.to(policy_scope(Inquiry.smart_queue), params)
-    puts "Lopesh"
     authorize @inquiries
   end
 
