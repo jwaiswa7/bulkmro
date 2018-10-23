f79447e314783b474301f490c08eaf62990a54f5
Lopesh <lopesh.durugkar@bulkmro.com>
2018-10-23 13:18:34 +0530

    add smart queue summary blocks

--- app/controllers/overseers/inquiries_controller.rb
+++ app/controllers/overseers/inquiries_controller.rb
@@ -33,6 +33,7 @@ class Overseers::InquiriesController < Overseers::BaseController
 
   def smart_queue
     @inquiries = ApplyDatatableParams.to(policy_scope(Inquiry.smart_queue), params)
+    puts "Lopesh"
     authorize @inquiries
   end
 
