d77001c4b2161db700eec3cd8b7ccac35a6d39c6
Amit Goyal <forsakenlegacy@gmail.com>
2018-10-23 10:54:32 +0530

    Added Today, Yesterday, Last 7 Days, This Month and Last Month to the date picker to allow selecting ranges instead of just custom date start and end. Removed Today, for now, since it's the default start and end date. Removed current financial year and last financial year, for now.

--- app/assets/javascripts/packs/components/daterangePicker.js
+++ app/assets/javascripts/packs/components/daterangePicker.js
@@ -1,6 +1,14 @@
 const daterangePicker = () => {
     $('body').on('click', '[data-toggle="daterangepicker"]', function() {
         $('[data-toggle="daterangepicker"]').daterangepicker({
+            ranges: {
+                // 'Today': [moment(), moment()],
+                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                'This Month': [moment().startOf('month'), moment().endOf('month')],
+                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            },
             opens: "left",
             minDate: moment('2015-01-01'),
             maxDate: moment().endOf('year'),
