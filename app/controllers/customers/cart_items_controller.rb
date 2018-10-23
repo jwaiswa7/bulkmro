4082e64e1bf6a517c9695ca6f08fec943eb78472
SOURABH RAJE <sourabh.raje@bulkmro.com>
2018-10-23 11:46:44 +0530

    fix issue for same cart item added and displayed multiple times

--- app/controllers/customers/cart_items_controller.rb
+++ app/controllers/customers/cart_items_controller.rb
@@ -1,14 +1,24 @@
 class Customers::CartItemsController < ApplicationController
 
-  def new
-  	debugger
-  end
-  
   def create
-    @cart = current_cart
-    @item = @cart.cart_items.new(quantity: params[:quantity].to_i, product_id: params[:product_id].to_i)
-    @cart.save
-    session[:cart_id] = @cart.id
+    cart = current_cart
+    product_id = params[:product_id].to_i
+    quantity = params[:quantity].to_i
+
+    if cart.cart_items.present?
+      product_ids = cart.cart_items.map(&:product_id)
+      if product_ids.include? product_id
+        cart_item = CartItem.find_by(product_id: product_id)
+        cart_item.quantity += 1
+        cart_item.save
+      else
+        new_cart_item(cart, quantity, product_id)
+      end
+    else
+      new_cart_item(cart, quantity, product_id)
+    end
+
+    session[:cart_id] = cart.id
     redirect_to customers_cart_path
   end
 
@@ -26,5 +36,8 @@ class Customers::CartItemsController < ApplicationController
     params.require(:cart_item).permit(:quantity, :product_id)
   end
 
-
+  def new_cart_item(cart, quantity, product_id)
+    cart.cart_items.new(quantity: quantity, product_id: product_id)
+    cart.save
+  end
 end
