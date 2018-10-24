01a193d82438a4de8b4b214add904e306b15fee5
Amit Goyal <forsakenlegacy@gmail.com>
2018-10-24 17:52:58 +0530

    Created a baseline dashboard for the customer portal that allows an overview of Quotes, orders and products. Added support for showing the recent products and quotes. Added support for showing the 5 most ordered products. Added status, subject and quote number to the quote summary fields. Added order number, subject and order status to order summary fields. Added product name and count to the most ordered products list.

--- app/controllers/customers/dashboard_controller.rb
+++ app/controllers/customers/dashboard_controller.rb
@@ -6,6 +6,10 @@ class Customers::DashboardController < Customers::BaseController
     @products_count = @contact_account.products.count
     @sales_quotes_count = @contact_account.sales_quotes.count
     @sales_orders_count = @contact_account.sales_orders.count
+
+    @recent_sales_quotes = SalesQuote.last(5)
+    @recent_sales_orders = SalesOrder.where(:status => :'Approved').last(5)
+    @most_ordered_products =  InquiryProduct.top(:product_id, 5).map { |id, c| [Product.find(id), c]}
   end
 
 end
