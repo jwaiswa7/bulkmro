a65d48096781cc9bc52aeb1be6b688e6e85abdd9
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2018-10-24 12:35:15 +0530

    updated conditions

--- app/services/services/overseers/inquiries/set_inquiry_status.rb
+++ app/services/services/overseers/inquiries/set_inquiry_status.rb
@@ -6,18 +6,15 @@ class Services::Overseers::Inquiries::SetInquiryStatus < Services::Shared::BaseS
   end
 
   def call
-    # set status for straight flow
-    puts "I status", action_name
     if (inquiry.status == 'New Inquiry' || inquiry.status == 'Lead by O/S') && action_name == "acknowledgement email sent"
       inquiry.update_attributes(:status => :'Acknowledgement Mail')
-    elsif inquiry.status == 'Acknowledgement Mail' && inquiry.products.present?
+    elsif inquiry.status == 'Acknowledgement Mail' && inquiry.products.present? && !action_name
       inquiry.update_attributes(:status => :'Cross Reference')
     elsif inquiry.status == ('Cross Reference' && inquiry.sales_quote.present?) || action_name == "quotation saved"
       inquiry.update_attributes(:status => :'Preparing Quotation')
     elsif inquiry.status == 'Preparing Quotation'|| action_name == "quotation sent"
       inquiry.update_attributes(:status => :'Quotation Sent')
     end
-
   end
 
   attr_accessor :inquiry, :action_name
