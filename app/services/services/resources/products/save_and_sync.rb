4d3b1f7f3614042e7608d5749db0d03a8d606258
pradeep <pbketkale@gmail.com>
2018-10-25 13:22:41 +0530

    SAP Item sync issue fix

--- app/services/services/resources/products/save_and_sync.rb
+++ app/services/services/resources/products/save_and_sync.rb
@@ -13,7 +13,11 @@ class Services::Resources::Products::SaveAndSync < Services::Shared::BaseService
   def call_later
     if product.persisted?
       remote_uid = ::Resources::Item.custom_find(product.sku)
-      product.update_attributes(:remote_uid => remote_uid) if remote_uid.present?
+      if remote_uid.present?
+        product.update_attributes(:remote_uid => remote_uid)
+      else
+        product.update_attributes(:remote_uid => nil)
+      end
 
       if product.remote_uid.present?
         ::Resources::Item.update(product.remote_uid, product)
