bbfeaecdaa6f8d398722652848b76ddeebeb2fbc
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-10-25 18:02:37 +0530

    Refactor warehouses.

--- db/seeds4.rb
+++ db/seeds4.rb
@@ -1,18 +1,21 @@
-service = Services::Shared::Migrations::Migrations.new(%w(sales_order_drafts))
-service.call
-
-service = Services::Shared::Migrations::Migrations.new(%w(sales_order_items))
-service.call
-
-service = Services::Shared::Migrations::Migrations.new(%w(sales_invoices))
-service.call
-
-service = Services::Shared::Migrations::Migrations.new(%w(sales_shipments))
-service.call
-
-service = Services::Shared::Migrations::Migrations.new(%w(inquiry_attachments))
-service.call
-
-service = Services::Shared::Migrations::Migrations.new
-service.perform_later
-
+service = Services::Shared::Spreadsheets::CsvImporter.new('warehouses_remote_uids.csv', 'seed_files')
+service.loop(nil) do |x|
+  Warehouse.where(:name => x.get_column('Warehouse Name')).first_or_create do |warehouse|
+    warehouse.legacy_metadata = x.get_row
+    warehouse.build_address(
+        :name => x.get_column('Account Name'),
+        :street1 => x.get_column('Street'),
+        :street2 => x.get_column('Block'),
+        :pincode => x.get_column('Zip Code'),
+        :city_name => x.get_column('City'),
+        :country_code => x.get_column('Country'),
+        :gst => x.get_column('GST'),
+        :state => AddressState.find_by_region_code(x.get_column('State'))
+    )
+  end.update_attributes!(remote_uid: x.get_column('Warehouse Code'),
+      legacy_id: x.get_column('Warehouse Code'),
+      location_uid: x.get_column('Location'),
+      remote_branch_name: x.get_column('Warehouse Name'),
+      remote_branch_code: x.get_column('Business Place ID')
+  )
+end
