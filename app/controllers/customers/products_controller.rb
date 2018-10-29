e244ee685fdd579f1a6f5f5c8795f87a6e5d7bf5
SOURABH RAJE <sourabh.raje@bulkmro.com>
2018-10-29 18:16:29 +0530

    refactor

--- app/controllers/customers/products_controller.rb
+++ app/controllers/customers/products_controller.rb
@@ -2,18 +2,16 @@ class Customers::ProductsController < Customers::BaseController
 
   def index
     @cart_item = current_cart.cart_items.new
-    service = Services::Customers::Finders::Products.new(params, current_contact)
-    service.call
-    @indexed_products = service.indexed_records
-    @products = service.records.try(:reverse)
     @products_paginate = current_contact.account.products.approved.page(params[:page])
     respond_to do |format|
       if params[:view] == "grid_view"
         format.js { render 'index.js.erb' }
       else
         format.json do
-          @indexed_products
-          @products
+          service = Services::Customers::Finders::Products.new(params, current_contact)
+          service.call
+          @indexed_products = service.indexed_records
+          @products = service.records.try(:reverse)
         end
       end
       format.html {}
