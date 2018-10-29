dbe2dee871bc32e501c78b5dd451d1fba8dd6f4d
pradeep <pbketkale@gmail.com>
2018-10-29 15:07:01 +0530

    Tax overhaul fixes

--- app/models/sales_order.rb
+++ app/models/sales_order.rb
@@ -9,8 +9,8 @@ class SalesOrder < ApplicationRecord
   include Mixins::HasComments
   include Mixins::CanBeSent
   include Mixins::CanBeSynced
-  include Mixins::HasRowCalculations
-  #include Mixins::HasConvertedCalculations
+  #include Mixins::HasRowCalculations
+  include Mixins::HasConvertedCalculations
 
   update_index('sales_orders#sales_order') {self}
   pg_search_scope :locate, :against => [], :associated_against => {:company => [:name], :inquiry => [:inquiry_number, :customer_po_number]}, :using => {:tsearch => {:prefix => true}}
