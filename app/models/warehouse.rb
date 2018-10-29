91cd6f25effbc3d9b192c20a9fd49ea867097bf4
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2018-10-29 10:28:55 +0530

    FIX Warehouse search locate method missing

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -2,6 +2,8 @@ class Warehouse < ApplicationRecord
   include Mixins::HasVisibility
 
   default_scope { order(:name => :asc) }
+  pg_search_scope :locate, :against => [:name], :associated_against => { address: [:name, :country_code, :street1, :street2, :state_name, :city_name, :pincode] }, :using => { :tsearch => { :prefix => true } }
+
   belongs_to :address, required: true
   has_many :bill_from_inquiries, :inverse_of => :bill_from, foreign_key: :bill_from_id, class_name: 'Inquiry', dependent: :nullify
   has_many :ship_from_inquiries, :inverse_of => :ship_from, foreign_key: :ship_from_id, class_name: 'Inquiry', dependent: :nullify
