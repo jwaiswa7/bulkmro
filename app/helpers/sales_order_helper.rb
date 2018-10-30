6d4d969e9d8abdbaa64c09d54a933ddc01bcc537
Amit Goyal <forsakenlegacy@gmail.com>
2018-10-30 12:13:11 +0530

    Refactored the sales order and contact helper status badges.

--- app/helpers/sales_order_helper.rb
+++ app/helpers/sales_order_helper.rb
@@ -1,5 +1,5 @@
 module SalesOrderHelper
-  def sales_order_status(status)
+  def sales_order_status_color(status)
     if status == "requested"
       "warning"
     elsif status == "SAP Approval Pending"
@@ -21,7 +21,7 @@ module SalesOrderHelper
     end
   end
 
-  def sales_order_format_label(status)
-    format_badge(status, sales_order_status(status))
+  def sales_order_status_badge(status)
+    format_badge(status, sales_order_status_color(status))
   end
 end
