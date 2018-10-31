8395919ec49d6a274f8e84c6ec6eac535ad7cf5a
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-10-31 13:21:19 +0530

    Inquiry policy.

--- app/policies/overseers/inquiry_policy.rb
+++ app/policies/overseers/inquiry_policy.rb
@@ -111,44 +111,4 @@ class Overseers::InquiryPolicy < Overseers::ApplicationPolicy
       end
     end
   end
-
-
-
-
-  # def edit_rfqs?
-  #   !record.rfqs_generated? && record.suppliers_selected?
-  # end
-  #
-  # def update_rfqs?
-  #   edit_rfqs?
-  # end
-
-  # def edit_rfqs_mailer_preview?
-  #   edit_rfqs?
-  # end
-
-  # def edit_quotations?
-  #   record.suppliers_selected? && record.rfqs_generated? && !record.sales_quote.present?
-  # end
-  #
-  # def update_quotations?
-  #   edit_quotations?
-  # end
-  #
-  # def new_sales_approval?
-  #   record.sales_quote.present? && !record.sales_approval.present?
-  # end
-  #
-  # def create_sales_approval?
-  #   new_sales_approval?
-  # end
-  #
-  # def new_sales_order?
-  #   record.sales_approval.present? && !record.sales_order.present?
-  # end
-  #
-  # def create_sales_order?
-  #   new_sales_order?
-  # end
-  #
 end
