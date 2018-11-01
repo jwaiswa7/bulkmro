c2bc1e478aed6ff10f89485c47c71afb4a8b0aa3
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2018-11-01 14:22:36 +0530

    Multiple changes: [WIP] Bot for addressing queries [WIP] Auto trigger notifications from bot regarding record updates

--- app/services/services/overseers/chat_messages/send_chat.rb
+++ app/services/services/overseers/chat_messages/send_chat.rb
@@ -4,4 +4,9 @@ class Services::Overseers::ChatMessages::SendChat < Services::Shared::ChatMessag
     super
   end
 
+  def send_chat_message(to,message,attachments)
+    super(to,message,attachments)
+    puts "In send chat"
+  end
+
 end
--- app/services/services/shared/chat_messages/base_service.rb
+++ app/services/services/shared/chat_messages/base_service.rb
@@ -4,6 +4,7 @@ class Services::Shared::ChatMessages::BaseService < Services::Shared::BaseServic
   end
 
   def send_chat_message(to, message, attachments)
+    puts "IN BASE"
     client.chat_postMessage(
         channel: to,
         text: message,
@@ -18,3 +19,6 @@ class Services::Shared::ChatMessages::BaseService < Services::Shared::BaseServic
 
   attr_accessor :client
 end
+
+
+# curl -X POST -H 'Authorization: Bearer xoxb-395906857286-467196181812-vQamBs539SJOj14FCakTtd58' -H 'Content-type: application/json' --data '{"channel":"test-channel","text":"I hope the tour went well, Mr. Wonka.","attachments": [{"text":"Who wins the lifetime supply of chocolate?","fallback":"You could be telling the computer exactly what it can do with a lifetime supply of chocolate.","color":"#3AA3E3","attachment_type":"default","callback_id":"select_simple_1234","actions":[{"name":"winners_list","text":"Who should win?","type":"select","data_source":"users"}]}]}' https://slack.com/api/chat.postMessage
