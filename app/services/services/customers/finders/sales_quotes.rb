76992efd7ac391c62079ae8a5d2726c60c66a85f
Malav <malav1410@gmail.com>
2018-11-14 18:20:04 +0530

    product id 0 in purchase order fixed. sales quote validation updated

--- app/services/services/customers/finders/sales_quotes.rb
+++ app/services/services/customers/finders/sales_quotes.rb
@@ -10,6 +10,8 @@ class Services::Customers::Finders::SalesQuotes < Services::Customers::Finders::
                         super.filter(filter_by_array('company_id', current_contact.companies.pluck(:id)))
                       end
 
+    indexed_records.filter({bool: {should: [{exists: {field: 'sent_at'}}]}})
+
     if search_filters.present?
       indexed_records = filter_query(indexed_records)
     end
@@ -21,11 +23,14 @@ class Services::Customers::Finders::SalesQuotes < Services::Customers::Finders::
     indexed_records
   end
 
+
+
   def perform_query(query_string)
     query_string = query_string[0, 35]
 
     indexed_records = index_klass.query({multi_match: {query: query_string, operator: 'and', fields: %w[inquiry_number_string^3 line_items valid_upto_s status_string quote_total inside_sales_owner ]}}).order(sort_definition)
 
+    indexed_records.filter({bool: {should: [{exists: {field: 'sent_at'}}]}})
     if current_contact.account_manager?
       indexed_records = indexed_records.filter(filter_by_array('company_id', current_contact.account.companies.pluck(:id)))
     else
@@ -44,7 +49,6 @@ class Services::Customers::Finders::SalesQuotes < Services::Customers::Finders::
     if range_filters.present?
       indexed_records = range_query(indexed_records)
     end
-
     indexed_records
   end
 
