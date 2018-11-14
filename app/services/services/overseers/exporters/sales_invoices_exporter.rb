728dec2f614286472494a1c37add87afab8ea587
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2018-11-14 11:22:29 +0530

    Minor change

--- app/services/services/overseers/exporters/sales_invoices_exporter.rb
+++ app/services/services/overseers/exporters/sales_invoices_exporter.rb
@@ -37,7 +37,7 @@ class Services::Overseers::Exporters::SalesInvoicesExporter < Services::Overseer
                     :total_with_freight => ('%.2f' % sales_order.calculated_total_cost), #Doubt
                     :tax_amount => ('%.2f' % sales_order.calculated_total_tax),
                     :gross_amount => ('%.2f' % sales_order.calculated_total_with_tax),
-                    :bill_from_branch => inquiry.bill_from.address.state.name,
+                    :bill_from_branch => if inquiry.bill_from then inquiry.bill_from.address.state.name else "" end,
                     :invoice_status => sales_invoice.sales_order.remote_status
                 })
     end
