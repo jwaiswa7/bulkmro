e8c1db737ab647ab26906794fd99a9ee9a8bd24a
Malav <malav1410@gmail.com>
2018-11-14 10:49:43 +0530

    show pdf render fixed. referenced to new shared pdf layouts

--- app/controllers/customers/inquiries/base_controller.rb
+++ app/controllers/customers/inquiries/base_controller.rb
@@ -10,8 +10,8 @@ class Customers::Inquiries::BaseController < Customers::BaseController
   def render_pdf_for(record)
     render(
         pdf: record.filename,
-        template: ['customers', 'inquiries', record.class.name.pluralize.underscore, 'show'].join('/'),
-        layout: 'customers/inquiries/layouts/show',
+        template: ['shared', 'layouts', 'pdf_templates', record.class.name.pluralize.underscore, 'show'].join('/'),
+        layout: 'shared/layouts/pdf_templates/show',
         page_size: 'Legal',
         footer: {
             center: '[page] of [topage]'
