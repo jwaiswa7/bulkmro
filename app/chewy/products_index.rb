9dc7ebe7c13b01df7fc17280379c898cd0437c8d
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2018-11-16 19:29:53 +0530

    Fix Issue of Sentry where product index asks for images?

--- app/chewy/products_index.rb
+++ app/chewy/products_index.rb
@@ -4,7 +4,7 @@ class ProductsIndex < BaseIndex
     field :brand, value: -> (record) { record.brand.to_s }, analyzer: 'substring'
     field :category, value: -> (record) { record.category.to_s }, analyzer: 'substring'
     field :approved, value: -> (record) { record.approval.try(:created_at) }
-    field :has_images, value: -> (record) { record.images.attached? }
+    field :has_images, value: -> (record) { record.image.attached? }
     field :sku, analyzer: 'sku_substring'
     field :name, analyzer: 'substring'
     field :mpn, value: -> (record) { record.mpn.to_s },analyzer: 'substring'
