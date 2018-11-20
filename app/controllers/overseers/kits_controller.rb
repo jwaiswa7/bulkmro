d6ba6a2fd2c93a92e64373e0f02300e86a767c12
Prikesh Savla <prikesh.savla@bulkmro.com>
2018-11-20 08:16:57 +0000

    kits_controller.rb edited online with Bitbucket

--- app/controllers/overseers/kits_controller.rb
+++ app/controllers/overseers/kits_controller.rb
@@ -11,7 +11,7 @@ class Overseers::KitsController < Overseers::BaseController
   end
 
   def new
-    @kit = Kit.new(:overseer => current_overseer, :inquiry_id => params[:inquiry_id].present? ? params[:inquiry_id] : nil)
+    @kit = Kit.new(:overseer => current_overseer, :inquiry_id => inquiry)
     @kit.build_product
 
     authorize @kit
@@ -24,10 +24,10 @@ class Overseers::KitsController < Overseers::BaseController
     if @kit.save
 
       if @kit.inquiry.present?
-        @kit.inquiry.inquiry_products.each do |ip|
-          @kit.kit_product_rows.where(:product_id => ip.product.id).first_or_initialize do |kp|
-            kp.assign_attributes(
-                quantity: ip.quantity
+        @kit.inquiry.inquiry_products.each do |inquiry_product|
+          @kit.kit_product_rows.where(:product_id => ip.product.id).first_or_initialize do |row|
+            row.assign_attributes(
+                quantity: inquiry_product.quantity
             )
           end
         end
@@ -54,6 +54,9 @@ class Overseers::KitsController < Overseers::BaseController
   end
 
   private
+  def inquiry
+    params[:inquiry_id].present? ? params[:inquiry_id] : nil
+  end
 
   def kit_params
     params.require(:kit).permit(
