a589f8cbdfbcfcd631f130a7db3295b7b290226b
pradeep <pbketkale@gmail.com>
2018-11-23 14:44:20 +0530

    Customer Portal Comments

--- app/models/inquiry_comment.rb
+++ app/models/inquiry_comment.rb
@@ -2,7 +2,7 @@ class InquiryComment < ApplicationRecord
   include Mixins::CanBeStamped
 
   belongs_to :inquiry
-  belongs_to :contact
+  belongs_to :contact, required:false
   belongs_to :sales_order, required: false
   has_one :approval, class_name: 'SalesOrderApproval', dependent: :destroy
   has_one :rejection, class_name: 'SalesOrderRejection', dependent: :destroy
