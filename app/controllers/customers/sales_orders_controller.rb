55f96e22be755fe1b8923037aade2254aac26b0c
Ashwin Goyal <mail.ashwingoyal@gmail.com>
2018-11-23 04:37:56 +0000

    sales_orders_controller.rb edited online with Bitbucket

--- app/controllers/customers/sales_orders_controller.rb
+++ app/controllers/customers/sales_orders_controller.rb
@@ -7,6 +7,9 @@ class Customers::SalesOrdersController < Customers::BaseController
     respond_to do |format|
       format.html {}
       format.json do
+        service = Services::Customers::Finders::SalesOrders.new(params, current_contact)
+        service.call
+
         @indexed_sales_orders = service.indexed_records
         @sales_orders = service.records.try(:reverse)
       end
