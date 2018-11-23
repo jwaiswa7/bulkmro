e317c79f85ece984282854d3a6b0e6c007604fd4
pradeep <pbketkale@gmail.com>
2018-11-23 17:41:54 +0530

    Internal and Customer comments section

--- app/models/inquiry_comment.rb
+++ app/models/inquiry_comment.rb
@@ -7,7 +7,18 @@ class InquiryComment < ApplicationRecord
   has_one :approval, class_name: 'SalesOrderApproval', dependent: :destroy
   has_one :rejection, class_name: 'SalesOrderRejection', dependent: :destroy
 
+  scope :internal_comments, -> {where(:show_to_customer => [false, nil])}
+  scope :customer_comments, -> {where(:show_to_customer => true)}
+
   def author
     self.contact || self.created_by
+  end
+
+  def author_role
+    if self.contact.present?
+      self.contact.role.titleize
+    elsif self.created_by.present?
+      self.created_by.role.titleize
+    end
   end
 end
