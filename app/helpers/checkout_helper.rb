f96833fb4fe32344f5bf219b250190e73605ddf3
SOURABH RAJE <sourabh.raje@bulkmro.com>
2018-11-27 10:16:50 +0530

    Add telephone number, gst number and show po reference in the summary

--- app/helpers/checkout_helper.rb
+++ app/helpers/checkout_helper.rb
@@ -1,14 +1,19 @@
-module CheckoutsHelper
+module CheckoutHelper
 
-  def order_total(cart_items)
+  def get_order_total(cart_items)
     cart_items.inject(0){|sum, cart_item| sum + cart_item.product.latest_unit_cost_price.to_f * cart_item.quantity}
   end
 
-  def billing_address(billing_address_id)
+  def get_billing_address(billing_address_id)
     Address.find(billing_address_id).to_multiline_s
   end
 
-  def shipping_address(shipping_address_id)
+  def get_shipping_address(shipping_address_id)
     Address.find(shipping_address_id).to_multiline_s
   end
+
+  def get_gst_no(address_id)
+    Address.find(address_id).gst
+  end
+
 end
