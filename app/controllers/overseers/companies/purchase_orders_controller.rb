e2d378494fe1eda4e577cd27d5d4fc91b22f15da
vishal sb <vishal.biradar@bulkmro.com>
2018-11-29 18:53:52 +0530

    modified the file and displayed the data of particular companies purchase order

--- app/controllers/overseers/companies/purchase_orders_controller.rb
+++ app/controllers/overseers/companies/purchase_orders_controller.rb
@@ -1,6 +1,22 @@
 class Overseers::Companies::PurchaseOrdersController < Overseers::Companies::BaseController
 
   def index
+    @base_filter = {}
+  company = Company.find(params[:company_id])
+    @base_filter[:base_filter_key] = "supplier_id"
+    @base_filter[:base_filter_value] =  company.id
+
+
     authorize :purchase_order
+    respond_to do |format|
+      format.html {}
+      format.json do
+
+        service = Services::Overseers::Finders::PurchaseOrders.new(params.merge(@base_filter), current_overseer)
+        service.call
+        @indexed_purchase_orders = service.indexed_records
+        @purchase_orders = service.records.try(:reverse)
+      end
+    end
   end
 end
--- app/services/services/overseers/finders/base_finder.rb
+++ app/services/services/overseers/finders/base_finder.rb
@@ -2,7 +2,7 @@ class Services::Overseers::Finders::BaseFinder < Services::Shared::BaseService
   def initialize(params, current_overseer = nil)
     @search_filters = []
     @range_filters = []
-
+    @base_filter = []
     if params[:columns].present?
       params[:columns].each do |index, column|
         if column[:searchable] && column[:search][:value].present?
@@ -14,6 +14,13 @@ class Services::Overseers::Finders::BaseFinder < Services::Shared::BaseService
         end
       end
     end
+
+    if params[:base_filter_key].present? && params[:base_filter_value].present?
+
+      @base_filter = filter_by_value(params[:base_filter_key], params[:base_filter_value])
+
+    end
+
 
     @query_string = if params[:search].present? && params[:search][:value].present?
                       params[:search][:value]
@@ -188,5 +195,5 @@ class Services::Overseers::Finders::BaseFinder < Services::Shared::BaseService
     end
   end
 
-  attr_accessor :query_string, :page, :per, :records, :indexed_records, :current_overseer, :search_filters, :range_filters
+  attr_accessor :query_string, :page, :per, :records, :indexed_records, :current_overseer, :search_filters, :range_filters, :base_filter
 end
