083507fd26804c5b8d8c14d79321e90e305ec93d
vishal sb <vishal.biradar@bulkmro.com>
2018-11-30 14:54:53 +0530

    formatted code in companies purchase orders controller

--- app/controllers/overseers/companies/purchase_orders_controller.rb
+++ app/controllers/overseers/companies/purchase_orders_controller.rb
@@ -1,17 +1,13 @@
 class Overseers::Companies::PurchaseOrdersController < Overseers::Companies::BaseController
-
   def index
     @base_filter = {}
-  company = Company.find(params[:company_id])
+    company = Company.find(params[:company_id])
     @base_filter[:base_filter_key] = "supplier_id"
     @base_filter[:base_filter_value] =  company.id
-
-
     authorize :purchase_order
     respond_to do |format|
       format.html {}
       format.json do
-
         service = Services::Overseers::Finders::PurchaseOrders.new(params.merge(@base_filter), current_overseer)
         service.call
         @indexed_purchase_orders = service.indexed_records
