ed6e2c4c79fa4bcfce399c9f37d96659f3bc8a8b
Malav <malav1410@gmail.com>
2018-12-01 15:54:38 +0530

    piramal migration to create products and then create customer product catalog based on products

--- db/seeds10.rb
+++ db/seeds10.rb
@@ -8,3 +8,6 @@ service.call
 service = Services::Shared::Migrations::Migrations.new(%w(purchase_order_callback_data), folder: 'seed_files')
 service.call
 # service = Services::Shared::Migrations::Migrations.new(%w(generate_customer_products_from_existing_products), folder: 'seed_files')
+
+service = Services::Shared::Migrations::Migrations.new(%w(add_products_and_customer_products_to_company), folder: 'seed_files')
+service.call
