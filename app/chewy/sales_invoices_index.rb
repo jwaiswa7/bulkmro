8e28e5ccd3be4fbb1bfe5539b493331d2d42d288
Sandesh Raut <sandesh.raut@bulkmro.com>
2018-12-03 16:58:15 +0530

    fixed overseers sales invoice partial search along with added and indexed missing colunms to finder from SI view

--- app/chewy/sales_invoices_index.rb
+++ app/chewy/sales_invoices_index.rb
@@ -9,6 +9,10 @@ class SalesInvoicesIndex < BaseIndex
     field :invoice_number, value: -> (record) {record.invoice_number.to_i}, type: 'integer'
     field :inquiry_present, value: -> (record) { record.inquiry.present? }, type: 'boolean'
     field :inquiry_number, value: -> (record) {record.inquiry.inquiry_number.to_i if record.inquiry.present?}, type: 'integer'
+    field :sales_order_number_string, value: -> (record) {record.sales_order.order_number.to_s if record.sales_order.present?}, analyzer: 'substring'
+    field :invoice_number_string, value: -> (record) { record.invoice_number.to_s }, analyzer: 'substring'
+    field :inquiry_number_string, value: -> (record) { record.inquiry.inquiry_number.to_s if record.inquiry.present?}, analyzer: 'substring'
+    field :status_string, value: -> (record) {record.status.to_s}, analyzer: 'substring'
     field :status, value: -> (record) {statuses[record.status]}
     field :company_id, value: -> (record) { record.inquiry.company.id if record.inquiry.present?}, type: 'integer'
     field :legacy, value: -> (record) {record.is_legacy}
