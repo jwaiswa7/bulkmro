f504ebdb11a6ff3adf61693104785ba717de1624
vishal sb <vishal.biradar@bulkmro.com>
2018-12-03 14:37:25 +0530

    added a base filter ib the products controller

--- app/controllers/overseers/companies/products_controller.rb
+++ app/controllers/overseers/companies/products_controller.rb
@@ -1,11 +1,17 @@
 class Overseers::Companies::ProductsController < Overseers::Companies::BaseController
 
   def index
-    service = Services::Overseers::Finders::Products.new(params)
-    service.call
+    @base_filter = {}
+    @company = Company.find(params[:company_id])
+    @product_id = Product.joins(:inquiry_products,:inquiry_product_suppliers).where(inquiry_product_suppliers: { supplier_id: @company.id }).pluck(:id)
+    @base_filter[:base_filter_key] = "id"
+    @base_filter[:base_filter_value] =  @product_id
 
-    @indexed_products = service.indexed_records
+    service = Services::Overseers::Finders::Products.new(params.merge(@base_filter))
+    service.call
     @products = service.records
+    @indexed_products = service.indexed_records
     authorize @products
-    end
   end
+end
+
