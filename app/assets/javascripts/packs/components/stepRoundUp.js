06c687fee972048ccf019b56937cbc77aab088dd
Amit Goyal <forsakenlegacy@gmail.com>
2018-12-05 21:17:28 +0530

    Updated gemlock. Fixed an issue with stepRoundUp causing number fields across the site to fail; removed for 'any'.

--- app/assets/javascripts/packs/components/stepRoundUp.js
+++ app/assets/javascripts/packs/components/stepRoundUp.js
@@ -4,9 +4,10 @@ const stepRoundUp = () => {
         $('[step]').bindWithDelay('keyup', function() {
             let value = $(this).val();
             let step = $(this).attr('step');
-            let rounded = Math.ceil(value/step)*step != 0 ? Math.ceil(value/step)*step : step;
-
-            $(this).val(rounded);
+            if (step != 'any') {
+                let rounded = Math.ceil(value/step)*step != 0 ? Math.ceil(value/step)*step : step;
+                $(this).val(rounded);
+            }
         }, 300);
     });
 };
