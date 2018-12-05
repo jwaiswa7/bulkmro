d4de832dbcb6efdaf548482cbf9662b86ae45175
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-12-05 20:34:56 +0530

    Fix quantity.

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,5 +3,13 @@ class CartItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart
 
+  validates_presence_of :quantity
+
+  after_initialize :set_defaults, :if => :new_record?
+
+  def set_defaults
+    self.quantity ||= 1
+  end
+
   delegate :best_tax_rate, to: :customer_product
 end
