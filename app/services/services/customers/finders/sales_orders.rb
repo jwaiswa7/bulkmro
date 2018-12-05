6dd3c9dc7715b6bdd01e86bf44251d496a339793
Sandesh Raut <sandesh.raut@bulkmro.com>
2018-12-05 18:51:05 +0530

    SO added ship to

--- app/services/services/customers/finders/sales_orders.rb
+++ app/services/services/customers/finders/sales_orders.rb
@@ -35,7 +35,7 @@ class Services::Customers::Finders::SalesOrders < Services::Customers::Finders::
   def perform_query(query_string)
     query_string = query_string[0, 35]
 
-    indexed_records = index_klass.query({multi_match: {query: query_string, operator: 'and', fields: %w[inquiry_number_string^1 order_number^3 status_string customer_po_number_string company^2 quote_total remote_status_string cp_status_s cp_order_date_s cp_committed_date_s cp_created_at_s cp_po_number_s cp_calculated_total_s cp_quote_id_s cp_subject_s cp_company_s cp_contact_email_s cp_billing_city_s cp_shipping_city_s cp_billing_gst_s cp_shipping_gst_s sales_order_rows.sku sales_order_rows.brand sales_order_rows.name sales_order_rows.mpn ]}}).order(sort_definition)
+    indexed_records = index_klass.query({multi_match: {query: query_string, operator: 'and', fields: %w[inquiry_number_string^1 order_number^3 status_string customer_po_number_string company^2 quote_total remote_status_string cp_status_s cp_order_date_s cp_committed_date_s cp_created_at_s cp_po_number_s cp_calculated_total_s cp_quote_id_s cp_ship_to_s cp_subject_s cp_company_s cp_contact_email_s cp_billing_city_s cp_shipping_city_s cp_billing_gst_s cp_shipping_gst_s sales_order_rows.sku sales_order_rows.brand sales_order_rows.name sales_order_rows.mpn ]}}).order(sort_definition)
 
     indexed_records = indexed_records.filter(filter_by_value("approval_status", "approved"))
 
