b7d0e7a4cccde166138063bed47923ef5e0d148d
Amit Goyal <forsakenlegacy@gmail.com>
2018-12-07 16:16:12 +0530

    Changed the policy for chewy to be relative to developer; same for reset_index. Added policy to make sure that the menu item is shown only on development OR if the dashboard chewy button is allowed.

--- app/policies/overseers/dashboard_policy.rb
+++ app/policies/overseers/dashboard_policy.rb
@@ -4,11 +4,11 @@ class Overseers::DashboardPolicy < Overseers::ApplicationPolicy
   end
 
   def chewy?
-    admin?
+    developer?
   end
 
   def reset_index?
-    admin?
+    developer?
   end
 
   def migrations?
