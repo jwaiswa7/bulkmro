39fff2ba85d1a1bf452445b3e1e719d2770f80cd
Lopesh <lopesh.durugkar@bulkmro.com>
2018-12-10 16:21:11 +0530

    make is_service with category dropdown list for kit-product, Product and other forms.

--- app/assets/javascripts/packs/views/products/new.js
+++ app/assets/javascripts/packs/views/products/new.js
@@ -1,18 +1,34 @@
 import select2s from "../../components/select2s";
 
-const newAction = () => {
+var categoryStatus = 0;
 
-    onIsServiceChecked($("#"+$('input[name*=is_service]')[1].id).is("checked"));
+const newAction = () => {
+    let is_service = $('input[name*=is_service]')[1].id;
+    //onIsServiceChecked($("#"+is_service).is("checked"));
+
+    if($('select[name*=category_id]').length != 0){
+        categoryStatus = 1;
+        console.log("In If Condition");
+        onIsServiceChecked($("#"+is_service).is("checked"));
+    }
+    else{
+        categoryStatus = 0;
+        onIsServiceChecked($("#"+is_service).is("checked"));
+    }
 
     $('form').on('change', '[name*=is_service]', function (event) {
-        //console.log($('input[name*=tax_code_id]'));
         $("#"+$('select[name*=tax_code_id]')[0].id).val(null).trigger("change");
+        $("#"+$('select[name*=category_id]')[0].id).val(null).trigger("change");
         onIsServiceChecked(event.target.checked);
+        categoryStatus=1;
     })
 };
 
 let onIsServiceChecked = (is_service_status) => {
     $("#"+$('select[name*=tax_code_id]')[0].id).attr('data-source', Routes.autocomplete_overseers_tax_codes_path({"is_service": is_service_status})).select2('destroy');
+    if(categoryStatus==1){
+        $("#"+$('select[name*=category_id]')[0].id).attr('data-source', Routes.autocomplete_closure_tree_overseers_categories_path({"is_service_categories": is_service_status})).select2('destroy');
+    }
     select2s();
 };
 
