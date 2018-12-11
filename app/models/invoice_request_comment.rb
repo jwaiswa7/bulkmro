f70b00a1e4c31e719cc88e09b17ba4824a6c645b
pradeep <pbketkale@gmail.com>
2018-12-11 13:48:31 +0530

    Invoice Request: Auto status, and other minor changes

--- app/models/invoice_request_comment.rb
+++ app/models/invoice_request_comment.rb
@@ -2,4 +2,12 @@ class InvoiceRequestComment < ApplicationRecord
   include Mixins::CanBeStamped
 
   belongs_to :invoice_request
+
+  def author
+    self.created_by
+  end
+
+  def author_role
+    author.role.titleize
+  end
 end
