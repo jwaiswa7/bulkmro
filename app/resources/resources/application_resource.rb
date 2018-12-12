839156cff51781a1ffe667db3653533b62f9acaa
Malav <malav1410@gmail.com>
2018-12-12 16:25:47 +0530

    used application controller defined staging? and production? method to check env conditions

--- app/resources/resources/application_resource.rb
+++ app/resources/resources/application_resource.rb
@@ -69,7 +69,7 @@ ulmwwTdSSRVmjSfz4OxPuSNQdXmYhHDkXMKfewl4mkEJSp92a1HHXw==
                          })
   end
 
-  if Rails.env.production?
+  if production?
     ENDPOINT = URI.parse('https://35.200.144.191:50000/b1s/v1')
     DATABASE = 'BULKMRO_PRODUCTION'
     USERNAME = 'manager'
@@ -119,7 +119,7 @@ ulmwwTdSSRVmjSfz4OxPuSNQdXmYhHDkXMKfewl4mkEJSp92a1HHXw==
 
   end
 
-  if Rails.env.staging?
+  if staging?
     ENDPOINT = URI.parse('https://35.200.144.191:50000/b1s/v1')
     DATABASE = 'BMRO_29NOV'
     USERNAME = 'manager'
