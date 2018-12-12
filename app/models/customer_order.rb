811372200531a2720130bf74b6656f082a257747
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-12-12 14:50:30 +0530

    Update company address.

--- app/models/customer_order.rb
+++ app/models/customer_order.rb
@@ -5,8 +5,8 @@ class CustomerOrder < ApplicationRecord
   belongs_to :company
   belongs_to :inquiry, required: false
   has_many :rows, dependent: :destroy, class_name: 'CustomerOrderRow'
-  belongs_to :billing_address, -> (record) {where(company_id: record.id)}, class_name: 'Address', foreign_key: :billing_address_id, required: false
-  belongs_to :shipping_address, -> (record) {where(company_id: record.id)}, class_name: 'Address', foreign_key: :shipping_address_id, required: false
+  belongs_to :billing_address, -> (record) {where(company_id: record.company_id)}, class_name: 'Address', foreign_key: :billing_address_id, required: false
+  belongs_to :shipping_address, -> (record) {where(company_id: record.company_id)}, class_name: 'Address', foreign_key: :shipping_address_id, required: false
 
   def to_s
     super.gsub!('CustomerOrder', 'Order')
