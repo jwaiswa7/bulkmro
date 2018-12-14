4368b016ff3175f2faad2e0dace69be75a03a829
pradeep <pbketkale@gmail.com>
2018-12-14 16:34:36 +0530

    payment request purchase order number column removal

--- db/migrate/20181214051619_remove_po_number_from_po_request.rb
+++ db/migrate/20181214051619_remove_po_number_from_po_request.rb
@@ -1,5 +1,5 @@
 class RemovePoNumberFromPoRequest < ActiveRecord::Migration[5.2]
   def change
-    remove_column :po_requests, :purchase_order_number, :integer
+    # remove_column :po_requests, :purchase_order_number, :integer
   end
 end
