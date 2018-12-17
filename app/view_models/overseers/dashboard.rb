1add3271543ad254f28bab295c016e27560ce86c
Amit Goyal <forsakenlegacy@gmail.com>
2018-12-17 11:07:37 +0530

    Dropped sales quotes from inside sales' dashboard. Resized both columns to col-6. Added Doc ID and SAP status to sales orders. Added better responsive rules. Fixed an issue with a floating menu divider in the main menu.

--- app/view_models/overseers/dashboard.rb
+++ app/view_models/overseers/dashboard.rb
@@ -7,20 +7,12 @@ class Overseers::Dashboard
     Inquiry.with_includes.where(:inside_sales_owner => overseer).where('updated_at > ?', Date.new(2018, 04, 01)).latest
   end
 
-  def sales_quotes
-    SalesQuote.with_includes.joins(:inquiry).where('inquiries.inside_sales_owner' => overseer).where("sales_quotes.updated_at > ?",Date.new(2018, 04, 01)).latest
-  end
-
   def sales_orders
-    SalesOrder.with_includes.joins(:inquiry).where('inquiries.inside_sales_owner' => overseer).where("sales_orders.updated_at > ?",Date.new(2018, 04, 01)).latest
+    SalesOrder.with_includes.joins(:inquiry).where('inquiries.inside_sales_owner' => overseer).where("sales_orders.updated_at > ?", Date.new(2018, 04, 01)).latest
   end
 
   def recent_inquiries
     inquiries.first(10)
-  end
-
-  def recent_sales_quotes
-    sales_quotes.first(10)
   end
 
   def recent_sales_orders
