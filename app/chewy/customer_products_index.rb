1603fc17c4d474942f524ffcaf103313e36a1713
rucha parab <rucha.parab@bulkmro.com>
2018-12-17 12:05:25 +0530

    tags filter added to the customer product

--- app/chewy/customer_products_index.rb
+++ app/chewy/customer_products_index.rb
@@ -8,6 +8,7 @@ class CustomerProductsIndex < BaseIndex
     field :name, analyzer: 'substring'
     field :brand, value: -> (record) { record.brand.to_s }, analyzer: 'substring'
     field :category, value: -> (record) { record.category.to_s }, analyzer: 'substring'
+    field :tags, value: -> (record) { record.tags.ids.map(&:to_s) }, analyzer: 'substring'
     field :customer_price, type: 'integer'
     field :created_at, type: 'date'
     field :updated_at, type: 'date'
