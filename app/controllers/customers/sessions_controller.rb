102e4fc30e9a58c770a98571819db18a0c2b85ac
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-12-18 10:28:09 +0530

    Fix logout issue.

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -9,6 +9,14 @@ class Customers::SessionsController < Devise::SessionsController
     current_contact
   end
 
+  def destroy
+    signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
+    reset_session
+    set_flash_message! :notice, :signed_out if signed_out
+    yield if block_given?
+    respond_to_on_destroy
+  end
+
   private
   def after_sign_in_path_for(resource_or_scope)
     customers_dashboard_path
