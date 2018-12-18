e84f24a6fc6dff624cf72008152c18875467d27d
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2018-12-18 16:23:57 +0530

    Add condition to send erroraneos request to API calls

--- app/services/services/customers/image_readers/image_reader_create.rb
+++ app/services/services/customers/image_readers/image_reader_create.rb
@@ -3,17 +3,17 @@ class Services::Customers::ImageReaders::ImageReaderCreate < Services::Shared::B
     URL = 'https://api.playment.in/v1/project/fd3f4026-a21e-4191-9373-3e775c494d3e/feedline'
     CONTAINER = 'images'
     TAG = 'bulkmro'
-    KEY = "Uhs8H1Qrkf0VsQM0Owz7nX5jFUc28rhSTlYPRUSXo5o"
+    KEY = 'Uhs8H1Qrkf0VsQM0Owz7nX5jFUc28rhSTlYPRUSXo5o'
   elsif Rails.env.staging?
     URL = 'https://api.playment.in/v1/project/10574135-d26d-4352-96e9-adffd9a032d8/feedline'
     CONTAINER = 'staging'
-    TAG = 'bulkmro_testing'
-    KEY = "qJsaya4SP75m9Wtsz0hj+90PoROrTUlrOYmtmRAEZ0o"
+    TAG = 'bulkmro_mock_testing'
+    KEY = 'qJsaya4SP75m9Wtsz0hj+90PoROrTUlrOYmtmRAEZ0o'
   else
     URL = 'http://localhost:3002/catch'
     CONTAINER = 'staging'
-    TAG = 'bulkmro_testing'
-    KEY = "qJsaya4SP75m9Wtsz0hj+90PoROrTUlrOYmtmRAEZ0o"
+    TAG = 'bulkmro_mock_testing'
+    KEY = 'qJsaya4SP75m9Wtsz0hj+90PoROrTUlrOYmtmRAEZ0o'
   end
 
   def initialize
@@ -32,101 +32,67 @@ class Services::Customers::ImageReaders::ImageReaderCreate < Services::Shared::B
 
   def call_later
     read_images
-    #remove_old_files
+    # remove_old_files
   end
 
-  def read_images
+  private
 
+  def read_images
     begin
-      # Create a BlobService object
       blob_client = Azure::Storage::Blob::BlobService.create(
           storage_account_name: azure_storage_config[:name],
           storage_access_key: azure_storage_config[:key]
       )
-      container_name = azure_storage_config[:container]
-
-      # List the blobs in the container
-      # puts "\nList blobs in the container following continuation token"
-
       nextMarker = nil
-      # i = 0
-
       loop do
         @images = []
-
-        blobs = blob_client.list_blobs(container_name, {marker: nextMarker, max_results: 1000})
+        blobs = blob_client.list_blobs(azure_storage_config[:container], {marker: nextMarker, max_results: 1000})
         blobs.each do |blob|
-          puts "\tBlob name #{blob.name} #{blob.properties[:last_modified]}"
-          puts "\tLink #{[azure_storage_config[:base_url], blob.name].join}"
           images << {blob: blob.to_json, image_url: [azure_storage_config[:base_url], blob.name].join, image_name: blob.name}
         end
-
-        # i += 1
-        # puts "\t Iteration #{i} #{blobs.continuation_token}"
-
         nextMarker = blobs.continuation_token
 
-        send_and_register_request(images)
-        # puts images, images.size
 
+        send_and_register_request(images)
         break unless (nextMarker && !nextMarker.empty?)
       end
-
-
     rescue Exception => e
-
       puts e.message
-
     ensure
-
       # Clean up resources. This includes the container and the temp files
-
     end
-
-
   end
 
   def send_and_register_request(images)
-
     images.each do |image|
-
-
-      # request = HTTParty.post("https://api.playment.in/v1/project/fd3f4026-a21e-4191-9373-3e775c494d3e/feedline", body: {reference_id: reference_id, data: {image_url: image_url}, tag: "bulkmro"}.to_json)
-
-=begin
-       response = {
-                    "feed_line_unit": {
-                      "flu_id": "33abc3fe-9fbc-4d7c-b951-a1d1bfb6f80",
-                      "reference_id": "ref_id_1",
-                      "tag": "image_qc"
-                    },
-                    "success": true
-                  }
-=end
       begin
         ActiveRecord::Base.transaction do
-
-
           blob = image[:blob]
           image_name = image[:image_name]
           image_url = image[:image_url]
 
-          if (ImageReader.find_by_image_url(image_url).blank?)
-
+          if ImageReader.where(image_url: image_url).where.not(status: :failed).blank?
 
             image_reader = ImageReader.where(image_url: image_url).first_or_create! do |record|
               record.image_name = image_name
               record.status = :pending
             end
+
             reference_id = image_reader.to_sgid(expires_in: (24 * 7).hours, for: 'image_reader').to_s
             image_reader.reference_id = reference_id
             request = {reference_id: reference_id, data: {image_url: image_url}, tag: TAG}
             image_reader.request = request.merge({blob: blob})
-
             image_reader.save
 
 
-            response = HTTParty.post(URL, body: request.to_json, headers: {:"x-client-key" => KEY})
+            request = if image_reader.id % 2 == 0 && !Rails.env.production?
+                        request
+                      else
+                        request.to_json
+                      end
+
+
+            response = HTTParty.post(URL, body: request, headers: {:"x-client-key" => KEY})
             validated_response = get_validated_response(response)
 
             status = validated_response[:feed_line_unit].present? ? :successful : :failed
@@ -152,8 +118,6 @@ class Services::Customers::ImageReaders::ImageReaderCreate < Services::Shared::B
       {raw_response: raw_response, error_message: raw_response.to_s}
     end
   end
-
-  # private
 
   attr_reader :azure_storage_config, :images
 end
