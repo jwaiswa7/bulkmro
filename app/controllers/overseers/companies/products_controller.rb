5333ead5d5ef9e3cb563333b6901a70bbf079d8e
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2018-12-20 16:55:26 +0530

    Refactor code for basefilter request

--- app/controllers/overseers/companies/products_controller.rb
+++ app/controllers/overseers/companies/products_controller.rb
@@ -1,13 +1,12 @@
 class Overseers::Companies::ProductsController < Overseers::Companies::BaseController
 
   def index
-    @base_filter = {}
-    @company = Company.find(params[:company_id])
-    @product_id = Product.joins(:inquiry_products,:inquiry_product_suppliers).where(inquiry_product_suppliers: { supplier_id: @company.id }).pluck(:id)
-    @base_filter[:base_filter_key] = "id"
-    @base_filter[:base_filter_value] =  @product_id
+    base_filter = {
+        :base_filter_key => "id",
+        :base_filter_value => Product.joins(:inquiry_products, :inquiry_product_suppliers).where(inquiry_product_suppliers: {supplier_id: params[:company_id]}).pluck(:id)
+    }
 
-    service = Services::Overseers::Finders::Products.new(params.merge(@base_filter))
+    service = Services::Overseers::Finders::Products.new(params.merge(base_filter))
     service.call
     @products = service.records
     @indexed_products = service.indexed_records
