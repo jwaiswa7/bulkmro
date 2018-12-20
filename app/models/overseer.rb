a844e6dd7eb53d79080203d5db764a4b8be7d419
Ashwin Goyal <mail.ashwingoyal@gmail.com>
2018-12-20 10:58:05 +0000

    overseer.rb edited online with Bitbucket

--- app/models/overseer.rb
+++ app/models/overseer.rb
@@ -8,7 +8,6 @@ class Overseer < ApplicationRecord
   include Mixins::HasRole
 
   has_many :activities, foreign_key: :created_by_id
-  has_many :po_requests, foreign_key: :logistics_owner_id
   has_one_attached :file
 
   pg_search_scope :locate, :against => [:first_name, :last_name, :email], :associated_against => {}, :using => {:tsearch => {:prefix => true}}
