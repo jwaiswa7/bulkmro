5dec684bc40b4bceab9e94a7e76feb673da789a6
Amit Goyal <forsakenlegacy@gmail.com>
2018-12-20 17:06:08 +0530

    Dropped support for sibling-based class modification for select2 change event; redundant. Refactored for easier readability.

--- app/assets/javascripts/packs/components/statusChange.js
+++ app/assets/javascripts/packs/components/statusChange.js
@@ -4,11 +4,7 @@ const statusChange = function (classname, select) {
     $(classname).on('click', function (e) {
         let id = $(this).attr('id').replace("status_", "");
         let selectBox = $(select).find('select');
-        let sibling = selectBox.siblings();
-
-        selectBox.select2().val(id).trigger('change');
-        sibling.removeClass('select2-container--default');
-        sibling.addClass('select2-container--bootstrap');
+        selectBox.val(id).trigger('change');
 
         e.preventDefault();
         return false;
