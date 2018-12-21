a3ebc79e8b819406cc61635404b188ef1896f11c
Ashwin Goyal <ashwin.goyal@bulkmro.com>
2018-12-21 18:20:14 +0530

    Refactor code.

--- app/models/customer_product_import.rb
+++ app/models/customer_product_import.rb
@@ -1,8 +1,10 @@
 class CustomerProductImport < ApplicationRecord
   HEADERS = %w(sku price name material_code brand hsn tax_percentage moq uom url).freeze
   TEMPLATE_HEADERS = %w(sku price name material_code brand hsn tax_percentage moq uom url).freeze
+
   include Mixins::CanBeStamped
-  has_one_attached :file
+  include Mixins::IsAnImport
+
   belongs_to :company
   has_many :rows, :class_name => 'CustomerProductImportRow'
 end
--- db/migrate/20181219043625_create_customer_product_import.rb
+++ db/migrate/20181219043625_create_customer_product_import.rb
@@ -1,9 +1,10 @@
 class CreateCustomerProductImport < ActiveRecord::Migration[5.2]
   def change
     create_table :customer_product_imports do |t|
-      t.references :company
-      t.integer :import_type
+      t.references :company, foreign_key: true
+      t.integer :import_type, index: true
       t.text :import_text
+
       t.timestamps
       t.userstamps
     end
