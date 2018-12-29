d5aaf39411c6e01f1e6ac7bae124ef2a75a319dc
rucha parab <rucha.parab@bulkmro.com>
2018-12-29 11:45:49 +0530

    created by is set to inside sales owner of a respective payment requests inquiry

--- app/services/services/callbacks/payment_requests/create.rb
+++ app/services/services/callbacks/payment_requests/create.rb
@@ -4,16 +4,15 @@ class Services::Callbacks::PaymentRequests::Create < Services::Callbacks::Shared
     po_request = params[:po_request]
     purchase_order = po_request.purchase_order
     inquiry = po_request.inquiry
+    overseer = inquiry.inside_sales_owner
     begin
       if purchase_order.present?
         if purchase_order.payment_request.nil?
           payment_request = PaymentRequest.new
-          payment_request.update_attributes(:purchase_order => purchase_order, :inquiry => inquiry, :po_request => po_request)
+          payment_request.update_attributes(:purchase_order => purchase_order, :inquiry => inquiry, :po_request => po_request, :created_by => overseer)
           return_response("Payment Request created successfully.")
-          puts "Payment Request created successfully."
         else
           return_response("Payment Request is already created.")
-          puts "Payment Request is already created."
         end
       else
         return_response("Purchase order not found.", 0)
