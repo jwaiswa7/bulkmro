3c561978a2feb7ae0b4d8801e200da99beb3682d
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-01-05 13:43:38 +0530

    Refactored query for categorywise revenue chart

--- app/services/services/customers/charts/category_wise_revenue.rb
+++ app/services/services/customers/charts/category_wise_revenue.rb
@@ -22,21 +22,25 @@ class Services::Customers::Charts::CategoryWiseRevenue < Services::Customers::Ch
 
       }
 
-      sales_orders = SalesOrder.includes(:rows).remote_approved.where(:created_at => start_at..end_at).joins(:company).where(companies: {id: company.id}).joins(:products)
-      total_revenue = sales_orders.map {|so| so.calculated_total }.sum
+      sales_orders = SalesOrder.remote_approved.where(:created_at => start_at..end_at).includes(:rows).joins(:company).where(companies: {id: company.id}).includes(:products, :categories)
+      total_revenue = 0
       categorywise_revenue = {}
 
       sales_orders.each do |so|
         so.rows.each do |row|
+          category = row.product.category.name
           if row.product.category.ancestors.present?
-            categorywise_revenue[row.product.category.ancestors.third_to_last.name] ||= 0
-            categorywise_revenue[row.product.category.ancestors.third_to_last.name] = categorywise_revenue[row.product.category.ancestors.third_to_last.name] + row.total_selling_price
-          else
-            categorywise_revenue[row.product.category.name] ||= 0
-            categorywise_revenue[row.product.category.name] = categorywise_revenue[row.product.category.name] + row.total_selling_price
+            categories = row.product.category.ancestors.pluck(:name) -  Category.default_ancestors
+            if categories.any?
+              category = categories.last
+            end
           end
+          categorywise_revenue[category] ||= 0
+          categorywise_revenue[category] = categorywise_revenue[category] + row.total_selling_price
+          total_revenue += row.total_selling_price
         end
       end
+
       categorywise_revenue.each do |category|
         @data[:labels].push(category[0])
         @data[:datasets][0][:data].push(category[1]/total_revenue)*100
