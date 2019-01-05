5035a9e3f468b780526f4069dbb17b54fff903d3
Malav <malav1410@gmail.com>
2019-01-05 15:04:32 +0530

    Sync warehouse inventory info from SAP to products in sprint.

---
+++ app/services/services/resources/products/update_recent_inquiry_product_inventory.rb
@@ -0,0 +1,14 @@
+class Services::Resources::Products::UpdateRecentInquiryProductInventory < Services::Shared::BaseService
+
+  def initialize
+  end
+
+  def call
+    perform_later()
+  end
+
+  def call_later
+    products = InquiryProduct.all.includes(:product).order(created_at: :desc).limit(1000).map{|ip| ip.product }.uniq
+    Services::Resources::Products::UpdateInventory.new(products).resync
+  end
+end
