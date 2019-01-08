3e18748cf2970a8b5f3fc0518bb9358b9322cfa3
Lopesh <lopesh.durugkar@bulkmro.com>
2019-01-08 17:57:16 +0530

    Add Elasticsearch functionality with filters to remote_request and callback_request index page.

---
+++ app/services/services/overseers/finders/remote_requests.rb
@@ -0,0 +1,51 @@
+class Services::Overseers::Finders::RemoteRequests < Services::Overseers::Finders::BaseFinder
+  def call
+    call_base
+  end
+
+  def model_klass
+    RemoteRequest
+  end
+
+  def all_records
+    indexed_records = super
+
+    if @base_filter.present?
+      indexed_records=  indexed_records.filter(@base_filter)
+    end
+
+    if search_filters.present?
+      indexed_records = filter_query(indexed_records)
+    end
+
+    if range_filters.present?
+      indexed_records = range_query(indexed_records)
+    end
+    indexed_records
+  end
+
+  def perform_query(query)
+    query = query[0, 35]
+
+    index_klass.query({
+                          multi_match: {
+                              query: query,
+                              operator: 'and',
+                              fields: %w[request resource],
+                              minimum_should_match: '100%'
+                          }
+                      }).order(sort_definition)
+    if search_filters.present?
+      indexed_records = filter_query(indexed_records)
+    end
+
+    if range_filters.present?
+      indexed_records = range_query(indexed_records)
+    end
+    indexed_records
+  end
+
+  def sort_definition
+    {:created_at => :asc}
+  end
+end
