24dffa99f91531546a4ce97dc3e9c9a25babf765
Malav <malav1410@gmail.com>
2019-01-09 11:16:16 +0530

    removed code which is not in use in customer's dashboard controller

--- app/controllers/customers/dashboard_controller.rb
+++ app/controllers/customers/dashboard_controller.rb
@@ -1,12 +1,6 @@
 class Customers::DashboardController < Customers::BaseController
 
   def show
-    indexed_sales_quotes = Services::Customers::Finders::SalesQuotes.new(params.merge(page: 1).merge(per: 5), current_contact, current_company)
-    @recent_sales_quotes = indexed_sales_quotes.call.records.try(:reverse)
-    indexed_sales_orders = Services::Customers::Finders::SalesOrders.new(params.merge(page: 1).merge(per: 5), current_contact, current_company)
-    @recent_sales_orders = indexed_sales_orders.call.records
-    indexed_sales_invoices = Services::Customers::Finders::SalesInvoices.new(params.merge(page: 1).merge(per: 5), current_contact, current_company)
-    @recent_sales_invoices = indexed_sales_invoices.call.records.try(:reverse)
     @dashboard = Customers::Dashboard.new(current_contact, current_company, params)
     authorize :dashboard
   end
