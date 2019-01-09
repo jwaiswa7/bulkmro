95b471d6ead4e240aa57fda079573af6d17f7f30
Malav <malav1410@gmail.com>
2019-01-09 12:09:56 +0530

    pdf render fixed for sales invoice in customer portal

--- app/controllers/customers/base_controller.rb
+++ app/controllers/customers/base_controller.rb
@@ -13,6 +13,22 @@ class Customers::BaseController < ApplicationController
 
   private
 
+  def render_pdf_for(record, locals={})
+    render(
+        pdf: record.filename,
+        template: ['shared', 'layouts', 'pdf_templates', record.class.name.pluralize.underscore, 'show'].join('/'),
+        layout: 'shared/layouts/pdf_templates/show',
+        page_size: 'A4',
+        footer: {
+            center: '[page] of [topage]'
+        },
+        # show_as_html: true,
+        locals: {
+            record: record
+        }.merge(locals)
+    )
+  end
+
   def redirect_if_required
     redirect_to_path = if session[:current_company_id].blank? || current_company.blank?
                          edit_current_company_customers_sign_in_steps_path
