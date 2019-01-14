c199a4a565234a5b63fc8665ca8a7fda07a16bf7
Lopesh <lopesh.durugkar@bulkmro.com>
2019-01-14 12:41:00 +0530

    Catalog - Add sorting to sales invoice index page for created_date.

--- app/services/services/overseers/finders/sales_invoices.rb
+++ app/services/services/overseers/finders/sales_invoices.rb
@@ -26,7 +26,7 @@ class Services::Overseers::Finders::SalesInvoices < Services::Overseers::Finders
   end
 
   def perform_query(query_string)
-    indexed_records = index_klass.query({multi_match: {query: query_string, operator: 'and', fields: %w[invoice_number_string^3 sales_order_number_string status_string inquiry_number_string inside_sales_owner outside_sales_owner]}})
+    indexed_records = index_klass.query({multi_match: {query: query_string, operator: 'and', fields: %w[invoice_number_string^3 sales_order_number_string status_string inquiry_number_string inside_sales_owner outside_sales_owner mis_date created_at]}})
 
     indexed_records = indexed_records.filter(filter_by_value("inquiry_present", true))
 
