28ab50c98fb94e0c701952b9025f52069a600ffa
Ruta kambli <ruta.kambli@bulkmro.com>
2019-01-15 17:06:12 +0530

    modifications in js

--- app/assets/javascripts/packs/views/activities/new.js
+++ app/assets/javascripts/packs/views/activities/new.js
@@ -6,12 +6,14 @@ const newAction = () => {
         $('.existing-company-form, .new-company').addClass('d-none');
         $('[name="activity[company_creation_request_attributes][account_id]"],[name="activity[company_creation_request_attributes][name]"] ').attr('required', true)
 
+
     })
     $('.existing-company').unbind('click').bind('click', function (event) {
         event.stopPropagation();
         $('.new-company-form, .existing-company-form,.new-company ').removeClass('d-none');
         $('.new-company-form, .existing-company').addClass('d-none');
         $('.new-company-form').find('select, input').attr('required', false)
+        $('.new-company-form').find("input[type=text], textarea").val("")
     })
     $('.new-account').unbind('click').bind('click', function (event) {
         event.stopPropagation();
@@ -19,6 +21,7 @@ const newAction = () => {
         $('.existing-account-form, .new-account').addClass('d-none');
         $('[name="activity[company_creation_request_attributes][account_id]"]').attr('required', false)
         $('.new-account-form').find('input, select').attr('required', true)
+
 
 
     })
