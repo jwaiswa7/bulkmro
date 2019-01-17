0538a439b8064f783d5fd62f348bf412eacad353
rucha parab <rucha.parab@bulkmro.com>
2019-01-17 10:33:25 +0530

    changed the formula for average company review rating calculation

--- app/models/company_rating.rb
+++ app/models/company_rating.rb
@@ -1,4 +1,8 @@
 class CompanyRating < ApplicationRecord
   belongs_to :company_review
   belongs_to :review_question
+
+  def calculate_rating
+    self.rating * self.review_question.weightage / 100
+  end
 end
