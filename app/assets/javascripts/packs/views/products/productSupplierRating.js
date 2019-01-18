582590a4ef1eb553fbdb87069a9dcc992ed57c35
Lopesh <lopesh.durugkar@bulkmro.com>
2019-01-18 16:00:26 +0530

    - Add Star rating to inquiry_supplier.

--- app/assets/javascripts/packs/views/products/productSupplierRating.js
+++ app/assets/javascripts/packs/views/products/productSupplierRating.js
@@ -1,5 +1,5 @@
 const productsSupplierRating = () => {
-    $.each($('.datatable>tbody>tr>td>div.product_supplier_ratings'),function(){
+    $.each($('div.product_supplier_ratings'),function(){
         $(this).raty({'readOnly': true , 'score': $(this).attr('data-ratings') , 'precision': true, 'hints': ['bad','poor','average','good','best']})
     })
 }
