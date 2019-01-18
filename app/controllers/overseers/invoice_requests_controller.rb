be67ea72d3fcbd2cb8dd33ea37cdd4c6dec9c36d
rucha parab <rucha.parab@bulkmro.com>
2019-01-18 15:27:43 +0530

    multiple suppliers logistics company review

--- app/controllers/overseers/invoice_requests_controller.rb
+++ app/controllers/overseers/invoice_requests_controller.rb
@@ -101,17 +101,26 @@ class Overseers::InvoiceRequestsController < Overseers::BaseController
   private
 
   def create_company_review(order)
-    @supplier = order.inquiry.suppliers.first
-    @can_review = !order.inquiry.suppliers.first.company_reviews.present? || !order.inquiry.suppliers.first.company_reviews.reviewed(current_overseer,:'Logistics').present?
-
-    if @can_review
-      @company_review = CompanyReview.where(overseer: current_overseer, survey_type: :'Logistics', company: order.inquiry.suppliers.first).first_or_create!
-
-      ReviewQuestion.logistics.each do |question|
-        CompanyRating.where({company_review_id: @company_review.id, review_question_id: question.id}).first_or_create!
+    suppliers_not_reviewed = {}
+    order.inquiry.suppliers.uniq.each do |supplier|
+      if !supplier.company_reviews.reviewed(current_overseer,:'Logistics').present?
+        suppliers_not_reviewed[supplier.id] = false
       end
-    else
-      @company_review = CompanyReview.new
+    end
+
+    if !suppliers_not_reviewed.empty?
+      @supplier = Company.where(id: suppliers_not_reviewed.keys.first).first
+      @can_review = !@supplier.company_reviews.present? || !@supplier.company_reviews.reviewed(current_overseer,:'Logistics').present?
+
+      if @can_review
+        @company_review = CompanyReview.where(overseer: current_overseer, survey_type: :'Logistics', company: @supplier).first_or_create!
+
+        ReviewQuestion.logistics.each do |question|
+          CompanyRating.where({company_review_id: @company_review.id, review_question_id: question.id}).first_or_create!
+        end
+      else
+        @company_review = CompanyReview.new
+      end
     end
   end
 
