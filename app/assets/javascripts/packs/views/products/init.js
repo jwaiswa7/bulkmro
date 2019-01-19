65d4fca6eb6de1591f4cbac7be0539baa3c22786
pradeep <pradeep.ketkale@bulkmro.com>
2019-01-19 15:54:37 +0530

    Product js fix

--- app/assets/javascripts/packs/views/products/init.js
+++ app/assets/javascripts/packs/views/products/init.js
@@ -1,14 +1,12 @@
-import productsShow from "./show";
 import productsEdit from "./edit";
 import productsNew from "./new";
-import productsShow from "./show"
+import productsShow from "./show";
 
 
 let products = {
     show: productsShow,
     edit: productsEdit,
-    new: productsNew,
-    show: productsShow
+    new: productsNew
 }
 
 export default products
