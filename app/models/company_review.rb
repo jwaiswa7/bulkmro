c985cd843a9fbb4fc09c136b944a450d212e2b4c
Ruta kambli <ruta.kambli@bulkmro.com>
2019-01-19 12:34:47 +0530

    pg search added and show page star readonly added

--- app/models/company_review.rb
+++ app/models/company_review.rb
@@ -1,5 +1,6 @@
 class CompanyReview < ApplicationRecord
   include Mixins::CanBeStamped
+  pg_search_scope :locate, :against => [:rating], :associated_against => {created_by: [:first_name, :last_name], company: [:name],}, :using => {:tsearch => {:prefix => true}}
 
   belongs_to :company
   has_many :company_ratings, dependent: :destroy
