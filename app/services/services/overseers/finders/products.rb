3f454be3cbecbfa6def6da7c59b6dd9b1cfa2d1c
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2019-01-21 15:30:25 +0530

    Fix Finder Issue

--- app/services/services/overseers/finders/products.rb
+++ app/services/services/overseers/finders/products.rb
@@ -28,7 +28,7 @@ class Services::Overseers::Finders::Products < Services::Overseers::Finders::Bas
                                             multi_match: {
                                                 query: query,
                                                 operator: 'and',
-                                                fields: %w[sku^3 sku_edge name brand category mpn total_pos total_quotes],
+                                                fields: %w[sku^3 sku_edge name brand category mpn],
                                                 minimum_should_match: '100%'
                                             }
                                         })
