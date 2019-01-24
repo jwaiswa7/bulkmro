39de92fef744e2db482e88795ed8dcd599e735c6
Ruta kambli <ruta.kambli@bulkmro.com>
2019-01-24 17:26:13 +0530

    new purchase order page rating popup added. material pickup page rating added. new_overseers_po_request_path replaced with new_purchase_orders_requests_overseers_sales_order_path

---
+++ app/assets/javascripts/packs/views/salesOrders/newPurchaseOrdersRequests.js
@@ -0,0 +1,15 @@
+import bindRatingModalTabClick from "../common/bindRatingModalTabClick"
+import updateRatingForm from "../common/updateRatingForm"
+const newPurchaseOrdersRequests = () => {
+
+    bindRatingModalTabClick();
+    $('.rating-modal a').click();
+
+    var customTabSelector =  $('#multipleRatingForm .custom-tab')
+    customTabSelector.eq(0).removeClass('disabled')
+    customTabSelector[0].click();
+    updateRatingForm();
+
+};
+
+export default newPurchaseOrdersRequests
