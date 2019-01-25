405cdb183ecd4a4235cd212ebb7af9f99595b394
pradeep <pradeep.ketkale@bulkmro.com>
2019-01-25 13:01:37 +0530

    Sourabh's changes

--- app/services/services/shared/migrations/migrations.rb
+++ app/services/services/shared/migrations/migrations.rb
@@ -2401,6 +2401,7 @@ class Services::Shared::Migrations::Migrations < Services::Shared::BaseService
       so.order_total = so.calculated_total
       so.invoice_total = so.invoices.map {|i| i.metadata.present? ? (i.metadata['base_grand_total'].to_f - i.metadata['base_tax_amount'].to_f) : 0.0}.inject(0) {|sum, x| sum + x}
       so.save
+
     end
   end
 
