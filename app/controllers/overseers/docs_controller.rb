a88cb1a53c92936daa36c681902ef46eb9323547
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-01-29 14:59:04 +0530

    - prepend view path in docs controller - move the views to the correct directory - refactoring

--- app/controllers/overseers/docs_controller.rb
+++ app/controllers/overseers/docs_controller.rb
@@ -1,4 +1,6 @@
 class Overseers::DocsController < Overseers::BaseController
+  prepend_view_path("app/views/overseers/docs")
+
   def index
     authorize :doc
     render params[:page], layout: false
