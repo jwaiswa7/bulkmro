b8448f58348c83dfc9ea69416aabc35c68417d4a
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2019-01-30 19:08:29 +0530

    Update Po Callback Duplicate Products issue

--- app/models/purchase_order_row.rb
+++ app/models/purchase_order_row.rb
@@ -60,8 +60,7 @@ class PurchaseOrderRow < ApplicationRecord
   end
 
   def get_product
-    product = Product.where(legacy_id: self.metadata['PopProductId'].to_i) || Product.where(id: self.metadata['PopProductId'])
-    product.first if product.present?
+    Product.where(legacy_id: self.metadata['PopProductId'].to_i).or(Product.where(id: Product.decode_id(self.metadata['PopProductId']))).try(:first)
   end
 
 end
