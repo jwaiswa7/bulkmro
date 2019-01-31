df0823f064ca093e57341eecb1226fbd2a3aa68d
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-01-31 12:28:09 +0530

    - Add new fields(dispatched_by, shipped_to, logistics_partner, tracking_number) to material pickup

--- app/models/material_pickup_request.rb
+++ app/models/material_pickup_request.rb
@@ -24,13 +24,50 @@ class MaterialPickupRequest < ApplicationRecord
       'Material Delivered': 20
   }
 
+  enum dispatched_bies: {
+      'Supplier': 10,
+      'Bulk MRO': 20
+  }
+
+  enum shipped_tos: {
+      'BM Warehouse': 10,
+      'Customer Warehouse': 20
+  }
+
+  enum logistics_partners: {
+      'Aramex': 10,
+      'FedEx': 11,
+      'Spoton': 12,
+      'Safex': 13,
+      'Professional': 14,
+      'DTDC': 15,
+      'Delhivery': 16,
+      'UPS': 17,
+      'Blue Dart': 18,
+      'BM Runner': 20,
+      'Drop Ship': 30
+  }
+
   scope :with_includes, -> {includes(:inquiry).includes(:purchase_order)}
+  scope :'3PL', -> {where(logistics_partner: [10,11,12,13,14,15,16,17,18])}
   after_initialize :set_defaults, if: :new_record?
 
   validates_length_of :rows, minimum: 1, message: "must have at least one product", :on => :update
   validates :attachments, presence: true, if: :material_delivered?
   validates :document_type, presence: true, if: :attachments?
   validate :date_validation
+
+  def grouped_status
+    grouped_status = {}
+    status_category = {10 => '3PL', 20 => 'BM Runner', 30 => 'Drop Ship'}
+    status_category.each do |index, category|
+      grouped_status[category] = MaterialPickupRequest.logistics_partners.collect {|status, v| ;
+      if v.between?(index, index + 9);
+        status;
+      end}.compact
+    end
+    grouped_status
+  end
 
   def material_delivered?
     status == 'Material Delivered'
