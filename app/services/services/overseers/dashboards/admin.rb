945541de3a2cfc7473265d7e75537bab15451731
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2019-02-01 14:29:30 +0530

    Refactor Code

--- app/services/services/overseers/dashboards/admin.rb
+++ app/services/services/overseers/dashboards/admin.rb
@@ -3,25 +3,23 @@ class Services::Overseers::Dashboards::Admin < Services::Shared::BaseService
   end
 
   def call
-    @data = OpenStruct.new(
-        {
-            entries: {}
-        }
-    )
+
 
     start_at = Date.new(2018, 10, 01).beginning_of_day
     end_at = Date.today.end_of_day
 
-    @data = Rails.cache.fetch('admin_dashboard_data', expires_in: 10.minutes) do
+    @data = Rails.cache.fetch('admin_dashboard_data', expires_in: 30.minutes) do
+      @data = OpenStruct.new({entries: {}})
       ActiveRecord::Base.default_timezone = :utc
-      inquiries = Inquiry.includes(:products).where(:created_at => start_at.beginning_of_month..end_at.end_of_month)
+      filter_by_dates = start_at.beginning_of_month..end_at.end_of_month
 
+      inquiries = Inquiry.includes(:products).where(:created_at => filter_by_dates)
       sales_quotes_array = SalesQuote.find_by_sql ["(select *From  sales_quotes sq where sq.created_at = (select max(sales_quotes.created_at) from sales_quotes where sq.inquiry_id = sales_quotes.inquiry_id and sales_quotes.sent_at is not null ) and sq.created_at between ? and ? order by sq.created_at desc) union( select sq.* from sales_quotes as sq left join sales_orders as so on sq.id = so.sales_quote_id where ( so.status = ? or so.legacy_request_status = ? ) and sq.created_at between ? and ?)", start_at.beginning_of_month, end_at.end_of_month, SalesOrder.statuses[:'Approved'], SalesOrder.statuses[:'Approved'], start_at.beginning_of_month, end_at.end_of_month]
       sales_quotes = SalesQuote.includes(:rows).where(id: sales_quotes_array.map(&:id))
 
-      sales_orders = SalesOrder.includes(:rows).where(:mis_date => start_at.beginning_of_month..end_at.end_of_month).where('sales_orders.status = ? OR sales_orders.legacy_request_status = ?', SalesOrder.statuses[:'Approved'], SalesOrder.statuses[:'Approved'])
-      purchase_orders = PurchaseOrder.not_cancelled.includes(:rows).where(:created_at => start_at.beginning_of_month..end_at.end_of_month)
-      sales_invoices = SalesInvoice.not_cancelled.includes(:rows).where(:created_at => start_at.beginning_of_month..end_at.end_of_month).where.not(sales_order_id: nil).where.not(metadata: nil)
+      sales_orders = SalesOrder.includes(:rows).where(:mis_date => filter_by_dates).remote_approved
+      purchase_orders = PurchaseOrder.not_cancelled.includes(:rows).where(:created_at => filter_by_dates)
+      sales_invoices = SalesInvoice.not_cancelled.includes(:rows).where(:created_at => filter_by_dates).where.not(sales_order_id: nil).where.not(metadata: nil)
 
       inquiry_groups = inquiries.group_by_month(:created_at, default_value: nil).count
       sales_quotes_groups = sales_quotes.group_by_month(:created_at, default_value: nil).count
@@ -34,36 +32,38 @@ class Services::Overseers::Dashboards::Admin < Services::Shared::BaseService
       months.reverse.each do |month|
         @data.entries[month.to_s] = {:inquiry => 0, :sales_quotes => 0, :sales_order => 0, :sales_invoice => 0, :purchase_order => 0}
 
+        filter_by_dates = month.to_date.beginning_of_month..month.to_date.end_of_month
+
         # INQUIRIES
-        i = inquiries.where(:created_at => month.to_date.beginning_of_month..month.to_date.end_of_month)
+        i = inquiries.where(:created_at => filter_by_dates)
         @data.entries[month.to_s][:inquiry] = {
             :count => inquiry_groups[month].present? ? inquiry_groups[month] : 0,
             :total => i.where.not(:potential_amount => nil).sum(&:potential_amount)
         }
 
         # SALES QUOTES
-        sq = sales_quotes.where(:created_at => month.to_date.beginning_of_month..month.to_date.end_of_month)
+        sq = sales_quotes.where(:created_at => filter_by_dates)
         @data.entries[month.to_s][:sales_quotes] = {
             :count => sales_quotes_groups[month].present? ? sales_quotes_groups[month] : 0,
             :total => sq.sum(&:calculated_total)
         }
 
         # SALES ORDERS
-        so = sales_orders.where(:mis_date => month.to_date.beginning_of_month..month.to_date.end_of_month)
+        so = sales_orders.where(:mis_date => filter_by_dates)
         @data.entries[month.to_s][:sales_order] = {
             :count => sales_order_groups[month].present? ? sales_order_groups[month] : 0,
             :total => so.sum(&:calculated_total)
         }
 
         # INVOICES
-        si = sales_invoices.where(:created_at => month.to_date.beginning_of_month..month.to_date.end_of_month)
+        si = sales_invoices.where(:mis_date => filter_by_dates)
         @data.entries[month.to_s][:sales_invoice] = {
             :count => sales_invoices_groups[month].present? ? sales_invoices_groups[month] : 0,
             :total => si.sum(&:calculated_total)
         }
 
         # PURCHASE ORDERS
-        po = purchase_orders.where(:created_at => month.to_date.beginning_of_month..month.to_date.end_of_month)
+        po = purchase_orders.where(:created_at => filter_by_dates)
         @data.entries[month.to_s][:purchase_order] = {
             :count => purchase_orders_groups[month].present? ? purchase_orders_groups[month] : 0,
             :total => po.sum(&:calculated_total)
