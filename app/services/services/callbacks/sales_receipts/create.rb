a875be62219f4ed2d0d95de19d57fb64c7e76341
Ruta kambli <ruta.kambli@bulkmro.com>
2019-02-04 12:01:39 +0530

    sales receipt additional fields added

--- app/services/services/callbacks/sales_receipts/create.rb
+++ app/services/services/callbacks/sales_receipts/create.rb
@@ -4,12 +4,18 @@ class Services::Callbacks::SalesReceipts::Create < Services::Callbacks::Shared::
     begin
       invoice = SalesInvoice.find_by_invoice_number(params['p_invoice_no'])
       company = Company.find_by_remote_uid!(params['cmp_id'])
+      currency = Currency.find_by_remote_uid!(params['p_amount_currency'])
 
       SalesReceipt.where(:remote_reference => params['p_sap_reference_number']).first_or_create! do |sales_receipt|
         sales_receipt.assign_attributes(
             :sales_invoice => invoice,
             :company => company,
-            :metadata => params.to_json
+            :metadata => params.to_json,
+            :currency => currency,
+            :payment_type => (params['on_account'] == "1" ? 10 : (params['on_account'] == "0" ? 20 : nil) ),
+            :payment_received_date => params['p_received_date'],
+            :payment_amount_received => params['p_amount_received']
+
         )
       end
       return_response("Sales Receipt created successfully.")
