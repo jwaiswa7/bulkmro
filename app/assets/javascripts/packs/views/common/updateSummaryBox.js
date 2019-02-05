095c55f0aa5733193df3a4137118fc98e1b16376
sakshiyadav95 <sakshi.yadav@bulkmro.com>
2019-02-05 15:36:58 +0530

    number seperator added in overseers inquiries summary page and validation done for nan value

--- app/assets/javascripts/packs/views/common/updateSummaryBox.js
+++ app/assets/javascripts/packs/views/common/updateSummaryBox.js
@@ -5,9 +5,9 @@ const updateSummaryBox = () => {
     table.on('xhr', function () {
         let json = table.ajax.json() ? table.ajax.json() : {};
         $.each(json.recordsSummary, function (index, summary) {
-            let statusSize = summary["size"].toLocaleString(undefined, { minimumFractionDigits: 0 });
+            let statusSize = parseInt(summary["size"].toLocaleString(undefined, { minimumFractionDigits: 0 })) || 0;
             let statusId = ".status-" + summary["status_id"];
-            $(statusId).find('.status-count-' + summary["status_id"]).text(statusSize);
+            $(statusId).find('.status-count-' + summary["status_id"]).html(new Intl.NumberFormat('en-US',{maximumFractionDigits: 0}).format(statusSize));
         });
 
         $.each(json.recordsTotalValue, function (index, total_value) {
