3196afd5e39ef3e516d907e9e8f96a1920db75a0
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2019-02-05 16:09:44 +0530

    Update JS validation for Addresses

--- app/assets/javascripts/packs/views/poRequests/validatePoRequestAddresses.js
+++ app/assets/javascripts/packs/views/poRequests/validatePoRequestAddresses.js
@@ -1,32 +1,75 @@
 const validatePoRequestAddresses = () => {
+    // window.Parsley.addValidator('locations', {
+    //     validateString: function (_value, locations, parsleyInstance) {
+    //         var locations = locations.split(',');
+    //         var supplier_po_type = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('select[name*=supplier_po_type]').val();
+    //         var selectedWarehouse = $(parsleyInstance.$element[0]).find(':selected').data('warehouse');
+    //         var selectedWarehouseState = $(parsleyInstance.$element[0]).find(':selected').data('warehouse-state');
+    //         var selectedBillingSupplierState = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('[name*=bill_from_id] :selected').data('supplier-state');
+    //         var selectedShippingSupplierState = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('[name*=ship_from_id] :selected').data('supplier-state');
+    //
+    //
+    //         var selectedBillToCity = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('[name*=bill_to_id] :selected').data('warehose-state');
+    //         var selectedShipToCity = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('[name*=ship_to_id] :selected').data('warehose-state');
+    //
+    //
+    //         var warehouseStates = $(parsleyInstance.$element[0]).data('warehouse-list').split(',');
+    //         console.log(selectedWarehouse);
+    //
+    //         if (selectedBillingSupplierState && selectedShippingSupplierState && !warehouseStates.includes(selectedBillingSupplierState) && !warehouseStates.includes(selectedShippingSupplierState)) {
+    //             return selectedWarehouseState == locations[2];
+    //         }
+    //
+    //         if ((supplier_po_type == "drop_ship" || supplier_po_type == "route_through") && selectedWarehouse == locations[0]) {
+    //             return true;
+    //         } else if (supplier_po_type == "regular" && selectedBillToCity == selectedShipToCity) {
+    //             return true;
+    //         }
+    //
+    //         return false
+    //     },
+    //     messages: {
+    //         en: 'Selected Location should be in the same City / State as order\'s City / State Or Must be the Sales Order Bill From'
+    //     }
+    // });
+
+
     window.Parsley.addValidator('locations', {
         validateString: function (_value, locations, parsleyInstance) {
+            var locations = locations.split(',');
             var supplier_po_type = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('select[name*=supplier_po_type]').val();
 
-            var locations = locations.split(',');
+            var selectedWarehouse = $(parsleyInstance.$element[0]).find(':selected').data('warehouse');
+            var selectedWarehouseState = $(parsleyInstance.$element[0]).find(':selected').data('warehouse-state');
+            var selectedBillingSupplierState = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('[name*=bill_from_id] :selected').data('supplier-state');
+            var selectedShippingSupplierState = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('[name*=ship_from_id] :selected').data('supplier-state');
 
-            if (supplier_po_type == "drop_ship" || supplier_po_type == "route_through") {
-                var selectedWarehouse = $(parsleyInstance.$element[0]).find(':selected').data('warehouse');
-                var selectedWarehouseState = $(parsleyInstance.$element[0]).find(':selected').data('warehouse-state');
-                if (selectedWarehouse == locations[0]) {
-                    return true;
-                } else {
-                    return selectedWarehouseState == locations[2];
-                }
-            } else if (supplier_po_type == "regular") {
-                var selectedWarehouse = $(parsleyInstance.$element[0]).find(':selected').data('warehouse');
-                var selectedWarehouseState = $(parsleyInstance.$element[0]).find(':selected').data('warehouse-state');
-                if (selectedWarehouse == locations[1]) {
-                    return true;
-                } else {
-                    return selectedWarehouseState == locations[2];
-                }
+            var selectedBillToCity = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('[name*=bill_to_id] :selected').data('warehouse-city');
+            var selectedShipToCity = $(parsleyInstance.$element[0]).closest('div.po-request-form').find('[name*=ship_to_id] :selected').data('warehouse-city');
+
+            if (supplier_po_type == "regular" && selectedBillToCity == selectedShipToCity) {
+
+                return true;
             }
+
+            var warehouseStates = $(parsleyInstance.$element[0]).data('warehouse-list').split(',');
+            console.log(selectedWarehouse);
+
+            if (selectedBillingSupplierState && selectedShippingSupplierState && !warehouseStates.includes(selectedBillingSupplierState) && !warehouseStates.includes(selectedShippingSupplierState)) {
+                return selectedWarehouseState == locations[2];
+            }
+
+            if ((supplier_po_type == "drop_ship" || supplier_po_type == "route_through") && selectedWarehouse == locations[0]) {
+                return true;
+            }
+
+            return false
         },
         messages: {
             en: 'Selected Location should be in the same City / State as order\'s City / State Or Must be the Sales Order Bill From'
         }
     });
+
 
     $('.supplier-committed-date').daterangepicker({
         singleDatePicker: true,
