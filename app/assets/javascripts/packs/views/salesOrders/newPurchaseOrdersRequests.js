8940ec61754e71d4ba803541fc283a66ab5b7d64
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2019-02-06 18:40:55 +0530

    Update Po for auto populate phone and email. make phone and email compulsory for po request

--- app/assets/javascripts/packs/views/salesOrders/newPurchaseOrdersRequests.js
+++ app/assets/javascripts/packs/views/salesOrders/newPurchaseOrdersRequests.js
@@ -2,23 +2,16 @@ import bindRatingModalTabClick from "../common/bindRatingModalTabClick"
 import updateRatingForm from "../common/updateRatingForm"
 import updateRowTotal from "../poRequests/updateRowTotal"
 import validatePoRequestAddresses from "../poRequests/validatePoRequestAddresses"
+import updateOnContactSelect from "../poRequests/updateOnContactSelect";
 
-import select2s from "../../components/select2s";
 
 const newPurchaseOrdersRequests = () => {
-
-/*    bindRatingModalTabClick();
-    $('.rating-modal a').click();
-    var customTabSelector = $('#multipleRatingForm .custom-tab')
-    customTabSelector.eq(0).removeClass('disabled')
-    customTabSelector[0].click();
-    updateRatingForm();*/
-
-
     validatePoRequestAddresses();
     updateRowTotal();
 
+    updateOnContactSelect();
 };
+
 
 
 export default newPurchaseOrdersRequests
