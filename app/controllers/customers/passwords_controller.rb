b15299baa889bf7967e5a7544ad00bde44974666
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-02-08 17:03:03 +0530

    Updated route after password reset

--- app/controllers/customers/passwords_controller.rb
+++ app/controllers/customers/passwords_controller.rb
@@ -3,6 +3,7 @@ class Customers::PasswordsController < Devise::PasswordsController
 
   protected
   def after_resetting_password_path_for(resource_or_scope)
+    sign_out(resource)
     new_contact_session_path
   end
 end
