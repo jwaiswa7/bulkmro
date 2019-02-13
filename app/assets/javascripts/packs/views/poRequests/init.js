a4e270adf545c364a53d4581853ad54f04dafc8c
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2019-02-13 13:58:37 +0530

    FIX material Followup edit page

--- app/assets/javascripts/packs/views/poRequests/init.js
+++ app/assets/javascripts/packs/views/poRequests/init.js
@@ -2,11 +2,13 @@ import poRequestsEdit from "./edit";
 import poRequestsNew from "./new";
 import pending from "./pendingAndRejected";
 import index from "./index";
+import amended from "./amended";
 
 let poRequests= {
     new: poRequestsNew,
     edit: poRequestsEdit,
     pendingAndRejected: pending,
+    amended: amended,
     index: index
 }
 
