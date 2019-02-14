6a427d2fad5d1df9e52f8644411be7f2a3d9ce46
Ruta kambli <ruta.kambli@bulkmro.com>
2019-02-14 19:02:08 +0530

    On create of sales receipts, sales_invoice payment_status modified

--- app/services/services/callbacks/sales_receipts/create.rb
+++ app/services/services/callbacks/sales_receipts/create.rb
@@ -1,25 +1,46 @@
 class Services::Callbacks::SalesReceipts::Create < Services::Callbacks::Shared::BaseCallback
-
   def call
     begin
       invoice = SalesInvoice.find_by_invoice_number(params['p_invoice_no'])
       company = Company.find_by_remote_uid!(params['cmp_id'])
       currency = Currency.find_by_name(params['p_amount_currency'])
       account = company.present? ? company.account.id : nil
-      SalesReceipt.where(:remote_reference => params['p_sap_reference_number']).first_or_create! do |sales_receipt|
+      payment_type = nil
+      if params['cmp_id'].present? && !params['p_invoice_no'].present?
+        amount_recived = params[:on_account]
+        payment_type = 'On Account'
+      elsif params['p_invoice_no'].present?
+        amount_recived = params[:p_amount_received]
+        payment_type = 'Against Invoice'
+      else
+        amount_recived = 0.0
+      end
+      SalesReceipt.where(remote_reference: params['p_sap_reference_number']).first_or_create! do |sales_receipt|
         sales_receipt.assign_attributes(
-            :sales_invoice => invoice,
-            :company => company,
-            :account => account,
-            :metadata => params,
-            :currency => currency,
-            :payment_type => (params['on_account'] == "1" ? 10 : (params['on_account'] == "0" ? 20 : nil) ), # have to change as per new data
-            :payment_received_date => params['p_received_date'],
-            :payment_amount_received => params['p_amount_received']
+          sales_invoice: invoice,
+          company: company,
+          account: account,
+          metadata: params,
+          currency: currency,
+          payment_type: payment_type, # have to change as per new data
+          payment_received_date: params['p_received_date'],
+          payment_amount_received: amount_recived
 
         )
       end
-      return_response("Sales Receipt created successfully.")
+      if invoice.present?
+        receivable_amount = invoice.calculated_total_with_tax
+        received_amount = invoice.sales_receipts.sum(:payment_amount_received)
+        if received_amount == 0.0
+          payment_status = 'Unpaid'
+        elsif received_amount < receivable_amount
+          payment_status = 'Partially Paid'
+        elsif receivable_amount == received_amount
+          payment_status = 'Fully Paid'
+        end
+      end
+      invoice.update__attributes(payment_status: payment_status)
+      return_response('Sales Receipt created successfully.')
     rescue => e
       return_response(e.message, 0)
     end
@@ -39,4 +60,3 @@ end
 #     "on_account":"163077.18",
 #     "cmp_id":"1"
 # }
-
