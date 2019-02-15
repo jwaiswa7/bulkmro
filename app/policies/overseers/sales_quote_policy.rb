bf4f9c8cbe8398f759f669e9d1033cb6baca832c
MeenakshiNaik <meenu688naik@gmail.com>
2019-02-15 16:28:39 +0530

    Last synced quote id set after sales quote sync response success issue fixed + inquiry persisted policy added for resync products link

--- app/policies/overseers/sales_quote_policy.rb
+++ app/policies/overseers/sales_quote_policy.rb
@@ -16,7 +16,7 @@ class Overseers::SalesQuotePolicy < Overseers::ApplicationPolicy
   end
 
   def new_sales_order?
-    new_revision? && record.inquiry.synced? && record.synced? && record.inquiry.valid_for_new_sales_order? && record.email_messages.present? && record.sales_quote_quantity_not_fulfilled?
+    new_revision? && record.inquiry.synced? && record.synced? && record.inquiry.valid_for_new_sales_order? && record.email_messages.present? && record.sales_quote_quantity_not_fulfilled? && (record.inquiry.last_synced_quote_id.present? && (record.inquiry.final_sales_quote.id == record.inquiry.last_synced_quote_id))
   end
 
   def reset_quote?
