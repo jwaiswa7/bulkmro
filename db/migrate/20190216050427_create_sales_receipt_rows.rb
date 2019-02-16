b516998ca9e27260484dd4f298e388a83357ff72
pradeep <pradeep.ketkale@bulkmro.com>
2019-02-16 13:06:32 +0530

    Sales receipt rows model, changes in api and model

--- db/migrate/20190216050427_create_sales_receipt_rows.rb
+++ db/migrate/20190216050427_create_sales_receipt_rows.rb
@@ -1,5 +1,6 @@
 class CreateSalesReceiptRows < ActiveRecord::Migration[5.2]
   def change
+    drop_table :sales_receipt_rows
     create_table :sales_receipt_rows do |t|
       t.references :sales_receipt, foreign_key: true
       t.references :sales_invoice, foreign_key: true
