2f328528a9846167f09c434a80f23a01c09a82e4
Sandesh Raut <sandesh.raut@bulkmro.com>
2019-02-20 14:40:20 +0530

    Activity company creation completion notification to IS and OS

--- app/services/services/overseers/notifications/notify.rb
+++ app/services/services/overseers/notifications/notify.rb
@@ -3,6 +3,15 @@ class Services::Overseers::Notifications::Notify < Services::Shared::Notificatio
     super(from, namespace)
   end
 
+  def send_company_creation_confirmation(entity, action, notifiable, url, *msg)
+    @action = action; @notifiable = notifiable; @url = url
+    @message = "#{msg[0]} has been approved and created in Sprint"
+    @to = entity.activity.created_by
+    send
+    @to = notifiable.inside_sales_owner
+    send
+  end
+
   def send_product_import_confirmation(tos, action, notifiable, url, *msg)
     @action = action; @notifiable = notifiable; @url = url
     @message = "#{msg[0]} uploaded for approval in Inquiry ##{msg[1]}"
