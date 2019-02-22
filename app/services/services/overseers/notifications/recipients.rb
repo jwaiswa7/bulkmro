4bc0bd15eaeed47f6080e843356ba13ac31272f3
Sandesh Raut <sandesh.raut@bulkmro.com>
2019-02-22 12:39:16 +0530

    added recipients group services in notifications

---
+++ app/services/services/overseers/notifications/recipients.rb
@@ -0,0 +1,11 @@
+class Services::Overseers::Notifications::Recipients
+  class << self
+    def logistics_owners
+      ['priyanka.rajpurkar@bulkmro.com', 'nilesh.desai@bulkmro.com', 'lavanya.j@bulkmro.com']
+    end
+
+    def customer_so_managers
+      ['dinesh.kumar@bulkmro.com']
+    end
+  end
+end
