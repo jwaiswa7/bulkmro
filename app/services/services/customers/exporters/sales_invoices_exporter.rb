70ba85fba0ebc58a181be5e402657daaecd507aa
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-02-26 16:10:50 +0530

    rubocop auto correct fixes

--- app/services/services/customers/exporters/sales_invoices_exporter.rb
+++ app/services/services/customers/exporters/sales_invoices_exporter.rb
@@ -1,5 +1,4 @@
 class Services::Customers::Exporters::SalesInvoicesExporter < Services::Customers::Exporters::BaseExporter
-
   def initialize(headers, company)
     @filename = 'sales_invoices_for_customer'
     super(headers, @filename)
@@ -30,20 +29,18 @@ class Services::Customers::Exporters::SalesInvoicesExporter < Services::Customer
     Enumerator.new do |yielder|
       yielder << CSV.generate_line(rows)
       model.where.not(sales_order_id: nil, metadata: nil).joins(:inquiry).where('inquiries.company_id = ?', company.id).where(created_at: @start_at..@end_at).order(invoice_number: :asc).each do |sales_invoice|
-        sales_order = sales_invoice.sales_order
-        inquiry = sales_invoice.inquiry
-        rows.push({
-                      inquiry_number: sales_invoice.inquiry.inquiry_number.to_s,
-                      invoice_number: sales_invoice.invoice_number,
-                      invoice_date: sales_invoice.created_at.to_date.to_s,
-                      order_number: sales_invoice.sales_order.order_number.to_s,
-                      order_date: sales_invoice.sales_order.created_at.to_date.to_s,
-                      customer_name: sales_invoice.inquiry.company.name.to_s,
-                      invoice_net_amount: ('%.2f' % sales_invoice.metadata['subtotal'] if sales_invoice.metadata['subtotal']),
-                      tax_amount: ('%.2f' % sales_invoice.metadata['tax_amount'] if sales_invoice.metadata['tax_amount']),
-                      gross_amount: ('%.2f' % sales_invoice.metadata['grand_total'] if sales_invoice.metadata['grand_total']),
-                      invoice_status: sales_invoice.sales_order.remote_status
-                  })
+        rows.push(
+          inquiry_number: sales_invoice.inquiry.inquiry_number.to_s,
+          invoice_number: sales_invoice.invoice_number,
+          invoice_date: sales_invoice.created_at.to_date.to_s,
+          order_number: sales_invoice.sales_order.order_number.to_s,
+          order_date: sales_invoice.sales_order.created_at.to_date.to_s,
+          customer_name: sales_invoice.inquiry.company.name.to_s,
+          invoice_net_amount: ('%.2f' % sales_invoice.metadata['subtotal'] if sales_invoice.metadata['subtotal']),
+          tax_amount: ('%.2f' % sales_invoice.metadata['tax_amount'] if sales_invoice.metadata['tax_amount']),
+          gross_amount: ('%.2f' % sales_invoice.metadata['grand_total'] if sales_invoice.metadata['grand_total']),
+          invoice_status: sales_invoice.sales_order.remote_status
+                  )
       end
       rows.drop(columns.count).each do |row|
         yielder << CSV.generate_line(row.values)
