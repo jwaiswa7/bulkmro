7ee85dfbfdad74567536c01e4d49fb45e3689d9e
Amit Goyal <forsakenlegacy@gmail.com>
2019-02-27 18:22:45 +0530

    Added comments to hasher.js to make it easier to understand.

--- app/assets/javascripts/packs/components/hasher.js
+++ app/assets/javascripts/packs/components/hasher.js
@@ -1,5 +1,6 @@
 const hasher = () => {
     window.hasher = {
+        // This gets a URL hash parameter using a key
         getParam: function(param) {
             let string = this.getHashString();
             let url = new URL('https://dummy.bulkmro.com'); // A dummy url for using searchParams API
@@ -7,6 +8,7 @@ const hasher = () => {
             return url.searchParams.get(param) ? url.searchParams.get(param) : "";
         },
 
+        // This sets a parameter in the URL hash using a key/value pair
         setParam: function(param, value) {
             let string = this.getHashString();
             let url = new URL('https://dummy.bulkmro.com'); // A dummy url for using searchParams API
@@ -21,6 +23,7 @@ const hasher = () => {
             window.location.hash = ['!', url.search.substring(1)].join('');
         },
 
+        // Generic function to see if there are any URL hashes defined, skips the '#!' from the return string
         getHashString: function() {
             return window.location.hash.substr(2);
         },
