0b3a90ba943c67ba4a389637741ba5fd71ec99d3
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-02-27 17:49:43 +0530

    add request for po and si creation if record doesn't exist

--- app/controllers/overseers/document_creations_controller.rb
+++ app/controllers/overseers/document_creations_controller.rb
@@ -10,12 +10,14 @@ class Overseers::DocumentCreationsController < Overseers::BaseController
         redirect_to new_overseers_document_creation_path, :notice => "Purchase Order already exists"
       else
         redirect_to new_overseers_document_creation_path, :notice => "Purchase Order creation request initiated"
+        Resources::PurchaseOrder.set_purchase_order_items([doc_creation_params["number"].to_i])
       end
     else
       if SalesInvoice.where(invoice_number: doc_creation_params["number"]).first.present?
         redirect_to new_overseers_document_creation_path, :notice => "Sales Invoice already exists"
       else
         redirect_to new_overseers_document_creation_path, :notice => "Sales Invoice creation request initiated"
+        Resources::Invoice.set_invoice_items([doc_creation_params["number"].to_i])
       end
     end
   end
