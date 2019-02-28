88e4239fa21b52b208b1064a4f6c654464eb0e27
Ruta kambli <ruta.kambli@bulkmro.com>
2019-02-28 13:50:20 +0530

    other_reject_reason added.

--- db/migrate/20190227100536_add_reason_to_invoice_requests.rb
+++ db/migrate/20190227100536_add_reason_to_invoice_requests.rb
@@ -1,5 +1,6 @@
 class AddReasonToInvoiceRequests < ActiveRecord::Migration[5.2]
   def change
-    add_column :invoice_requests, :rejection_reason, :string
+    add_column :invoice_requests, :rejection_reason, :integer
+    add_column :invoice_requests, :other_rejection_reason, :string
   end
 end
