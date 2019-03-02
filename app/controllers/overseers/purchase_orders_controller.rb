3234044d739594ff54793c940315a84e2c90b041
prikesh-bulkmro <42164786+prikesh-bulkmro@users.noreply.github.com>
2019-03-02 15:53:15 +0530

    Update purchase_orders_controller.rb

--- app/controllers/overseers/purchase_orders_controller.rb
+++ app/controllers/overseers/purchase_orders_controller.rb
@@ -132,6 +132,7 @@ class Overseers::PurchaseOrdersController < Overseers::BaseController
   end
 
   def autocomplete
+    purchase_orders = PurchaseOrder.all
     if params[:inquiry_number].present?
       purchase_orders = PurchaseOrder.joins(:inquiry).where(inquiries: { inquiry_number: params[:inquiry_number] })
       purchase_orders = purchase_orders.where(id: purchase_orders.pluck(:id))
@@ -142,6 +143,7 @@ class Overseers::PurchaseOrdersController < Overseers::BaseController
   end
 
   def autocomplete_without_po_requests
+    purchase_orders = PurchaseOrder.all
     if params[:inquiry_number].present?
       purchase_orders = PurchaseOrder.joins(:inquiry).where(inquiries: { inquiry_number: params[:inquiry_number] })
       purchase_orders = purchase_orders.where(id: purchase_orders.reject { |r| r.po_request.present? }.pluck(:id))
