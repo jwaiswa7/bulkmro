41dbf359a08925b251eadab6a014672b6edc0d47
sakshiyadav95 <sakshi.yadav@bulkmro.com>
2019-03-04 11:23:11 +0530

    salesorder sorting added

--- app/controllers/overseers/sales_orders_controller.rb
+++ app/controllers/overseers/sales_orders_controller.rb
@@ -1,10 +1,11 @@
 class Overseers::SalesOrdersController < Overseers::BaseController
-  before_action :set_sales_order, only: [ :resync, :new_purchase_orders_requests,  :preview_purchase_orders_requests, :create_purchase_orders_requests]
+  before_action :set_sales_order, only: [:resync, :new_purchase_orders_requests, :preview_purchase_orders_requests, :create_purchase_orders_requests]
+
   def pending
     authorize :sales_order
 
     respond_to do |format|
-      format.html { render 'pending' }
+      format.html {render 'pending'}
       format.json do
         service = Services::Overseers::Finders::PendingSalesOrders.new(params, current_overseer, paginate: false)
         service.call
@@ -26,7 +27,7 @@ class Overseers::SalesOrdersController < Overseers::BaseController
   def cancelled
     authorize :sales_order
     respond_to do |format|
-      format.html { render 'pending' }
+      format.html {render 'pending'}
       format.json do
         service = Services::Overseers::Finders::CancelledSalesOrders.new(params, current_overseer, paginate: false)
         service.call
@@ -79,13 +80,12 @@ class Overseers::SalesOrdersController < Overseers::BaseController
   def index
     authorize :sales_order
     respond_to do |format|
-      format.html { }
+      format.html {}
       format.json do
         service = Services::Overseers::Finders::SalesOrders.new(params, current_overseer)
         service.call
-
         @indexed_sales_orders = service.indexed_records
-        @sales_orders = service.records.try(:reverse)
+        @sales_orders = service.records
 
         status_service = Services::Overseers::Statuses::GetSummaryStatusBuckets.new(@indexed_sales_orders, SalesOrder, remote_status: true)
         status_service.call
@@ -101,13 +101,13 @@ class Overseers::SalesOrdersController < Overseers::BaseController
   def not_invoiced
     authorize :sales_order
     respond_to do |format|
-      format.html { render 'not_invoiced' }
+      format.html {render 'not_invoiced'}
       format.json do
         service = Services::Overseers::Finders::NotInvoicedSalesOrders.new(params, current_overseer)
         service.call
 
         @indexed_sales_orders = service.indexed_records
-        @sales_orders = service.records.try(:reverse)
+        @sales_orders = service.records
 
         status_service = Services::Overseers::Statuses::GetSummaryStatusBuckets.new(@indexed_sales_orders, SalesOrder, remote_status: true)
         status_service.call
@@ -135,7 +135,7 @@ class Overseers::SalesOrdersController < Overseers::BaseController
 
     sales_orders = SalesOrder.where.not(sent_at: nil).where(draft_uid: nil, status: :'SAP Approval Pending').not_legacy
     respond_to do |format|
-      format.html { }
+      format.html {}
       format.json do
         @drafts_pending_count = sales_orders.count
         @sales_orders = ApplyDatatableParams.to(sales_orders, params)
@@ -183,52 +183,52 @@ class Overseers::SalesOrdersController < Overseers::BaseController
 
   private
 
-    def set_sales_order
-      @sales_order = SalesOrder.find(params[:id])
-    end
+  def set_sales_order
+    @sales_order = SalesOrder.find(params[:id])
+  end
 
-    def new_purchase_orders_requests_params
-      if params.has_key?(:sales_order)
-        params.require(:sales_order).permit(
+  def new_purchase_orders_requests_params
+    if params.has_key?(:sales_order)
+      params.require(:sales_order).permit(
           :id,
-            po_requests_attributes: [
-                :id,
-                :supplier_id,
-                :inquiry_id,
-                :_destroy,
-                :logistics_owner_id,
-                :bill_from_id,
-                :ship_from_id,
-                :bill_to_id,
-                :ship_to_id,
-                :contact_id,
-                :payment_option_id,
-                :supplier_po_type,
-                :status,
-                :supplier_committed_date,
-                :contact_email,
-                :contact_phone,
-                :blobs,
-                attachments: [],
-                rows_attributes: [
-                    :id,
-                    :_destroy,
-                    :status,
-                    :quantity,
-                    :sales_order_row_id,
-                    :product_id,
-                    :brand_id,
-                    :tax_code_id,
-                    :tax_rate_id,
-                    :lead_time,
-                    :measurement_unit_id,
-                    :discount_percentage,
-                    :unit_price
-                ]
-            ]
-        )
-      else
-        {}
-      end
+          po_requests_attributes: [
+              :id,
+              :supplier_id,
+              :inquiry_id,
+              :_destroy,
+              :logistics_owner_id,
+              :bill_from_id,
+              :ship_from_id,
+              :bill_to_id,
+              :ship_to_id,
+              :contact_id,
+              :payment_option_id,
+              :supplier_po_type,
+              :status,
+              :supplier_committed_date,
+              :contact_email,
+              :contact_phone,
+              :blobs,
+              attachments: [],
+              rows_attributes: [
+                  :id,
+                  :_destroy,
+                  :status,
+                  :quantity,
+                  :sales_order_row_id,
+                  :product_id,
+                  :brand_id,
+                  :tax_code_id,
+                  :tax_rate_id,
+                  :lead_time,
+                  :measurement_unit_id,
+                  :discount_percentage,
+                  :unit_price
+              ]
+          ]
+      )
+    else
+      {}
     end
+  end
 end
