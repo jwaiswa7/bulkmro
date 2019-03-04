6ab6ff9087fee0cd8b0c6ec4b5c8bc77ca1c870e
rucha parab <rucha.parab@bulkmro.com>
2019-03-04 19:09:48 +0530

    status changed logic is changed from if condition to switch case for readability

--- app/services/services/overseers/invoice_requests/update.rb
+++ app/services/services/overseers/invoice_requests/update.rb
@@ -11,32 +11,14 @@ class Services::Overseers::InvoiceRequests::Update < Services::Shared::BaseServi
     ActiveRecord::Base.transaction do
       if @invoice_request.status_changed?
 
-          if @invoice_request.grpo_number_changed?
-            @invoice_request_comment = InvoiceRequestComment.new(message: "GRPO Number Changed: #{@invoice_request.grpo_number}; Status changed: #{@invoice_request.status}", invoice_request: @invoice_request, overseer: current_overseer)
-          end
+        if @invoice_request.grpo_number_changed?
+          @invoice_request_comment = InvoiceRequestComment.new(message: "GRPO Number Changed: #{@invoice_request.grpo_number}; Status changed: #{@invoice_request.status}", invoice_request: @invoice_request, overseer: current_overseer)
+        end
 
-          if ['GRPO Request Rejected'].include?(@invoice_request.status)
-            @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{@invoice_request.status}.<br/> Rejection Reason: #{@invoice_request.grpo_rejection_reason} ", invoice_request: @invoice_request, overseer: current_overseer)
-            material_pickup_requests = @invoice_request.material_pickup_requests
-            material_pickup_requests.each do |material_pickup_request|
-              material_pickup_request.update_attributes(:status => @invoice_request.status)
-            end
-          elsif ['AP Request Rejected'].include?(@invoice_request.status)
-            @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{@invoice_request.status}.<br/> Rejection Reason: #{@invoice_request.ap_rejection_reason} ", invoice_request: @invoice_request, overseer: current_overseer)
-            material_pickup_requests = @invoice_request.material_pickup_requests
-            material_pickup_requests.each do |material_pickup_request|
-              material_pickup_request.update_attributes(:status => @invoice_request.status)
-            end
-          elsif ['Cancelled GRPO','Cancelled AP Invoice'].include?(@invoice_request.status)
-            @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{@invoice_request.status}.<br/> Cancellation Reason: #{@invoice_request.grpo_cancellation_reason} ", invoice_request: @invoice_request, overseer: current_overseer)
-          elsif ['Cancelled AP Invoice'].include?(@invoice_request.status)
-            @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{@invoice_request.status}.<br/> Cancellation Reason: #{@invoice_request.ap_cancellation_reason} ", invoice_request: @invoice_request, overseer: current_overseer)
-          else
-            @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{@invoice_request.status}", invoice_request: @invoice_request, overseer: current_overseer)
-          end
+        status_changed(@invoice_request)
 
-          @invoice_request.save!
-          @invoice_request_comment.save!
+        @invoice_request.save!
+        @invoice_request_comment.save!
       elsif @invoice_request.grpo_rejection_reason_changed? && @invoice_request.grpo_rejection_reason != 'Others'
         @invoice_request.grpo_other_rejection_reason = nil
         @invoice_request.save!
@@ -46,6 +28,29 @@ class Services::Overseers::InvoiceRequests::Update < Services::Shared::BaseServi
       else
         @invoice_request.save!
       end
+    end
+  end
+
+  def status_changed(invoice_request)
+    case invoice_request.status.to_sym
+    when :'GRPO Request Rejected'
+      @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{invoice_request.status}.<br/> GRPO Request Rejection Reason: #{invoice_request.grpo_rejection_reason} ", invoice_request: invoice_request, overseer: current_overseer)
+      material_pickup_requests = invoice_request.material_pickup_requests
+      material_pickup_requests.each do |material_pickup_request|
+        material_pickup_request.update_attributes(:status => invoice_request.status)
+      end
+    when :'AP Request Rejected'
+      @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{invoice_request.status}.<br/> AP Request Rejection Reason: #{invoice_request.ap_rejection_reason} ", invoice_request: invoice_request, overseer: current_overseer)
+      material_pickup_requests = invoice_request.material_pickup_requests
+      material_pickup_requests.each do |material_pickup_request|
+        material_pickup_request.update_attributes(:status => invoice_request.status)
+      end
+    when :'Cancelled GRPO'
+      @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{invoice_request.status}.<br/> GRPO Request Cancellation Reason: #{invoice_request.grpo_cancellation_reason} ", invoice_request: invoice_request, overseer: current_overseer)
+    when :'Cancelled AP Invoice'
+      @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{invoice_request.status}.<br/> AP Request Cancellation Reason: #{invoice_request.ap_cancellation_reason} ", invoice_request: invoice_request, overseer: current_overseer)
+    else
+      @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{invoice_request.status}", invoice_request: invoice_request, overseer: current_overseer)
     end
   end
 
