8314d10187186cd3dd859a373338d7f8b99e8475
pradeep <pradeep.ketkale@bulkmro.com>
2019-03-04 16:06:28 +0530

    Invoice requests: Service for updates

--- app/controllers/overseers/invoice_requests_controller.rb
+++ app/controllers/overseers/invoice_requests_controller.rb
@@ -124,45 +124,8 @@ class Overseers::InvoiceRequestsController < Overseers::BaseController
     @invoice_request.assign_attributes(invoice_request_params.merge(overseer: current_overseer))
     authorize @invoice_request
     if @invoice_request.valid?
-      @invoice_request.status = "Pending AP Invoice" if @invoice_request.grpo_number.present? && @invoice_request.grpo_number_valid?
-      @invoice_request.update_status(@invoice_request.status)
-      ActiveRecord::Base.transaction do
-        if @invoice_request.status_changed?
-          if @invoice_request.grpo_number_changed?
-            @invoice_request_comment = InvoiceRequestComment.new(message: "GRPO Number Changed: #{@invoice_request.grpo_number}; Status changed: #{@invoice_request.status}", invoice_request: @invoice_request, overseer: current_overseer)
-          else
-            @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{@invoice_request.status}", invoice_request: @invoice_request, overseer: current_overseer)
-          end
-          if ['GRPO Request Rejected', 'AP Invoice Request Rejected'].include?(@invoice_request.status)
-            @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{@invoice_request.status}.<br/> Rejection Reason: #{@invoice_request.rejection_reason} ", invoice_request: @invoice_request, overseer: current_overseer)
-            material_pickup_requests = @invoice_request.material_pickup_requests
-            @invoice_request.cancellation_reason = nil # ( Can canceled Invoice again open? )
-            material_pickup_requests.each do |material_pickup_request|
-              material_pickup_request.status = @invoice_request.status
-              material_pickup_request.save
-            end
-          elsif ['Cancelled','Cancelled AR Invoice'].include?(@invoice_request.status)
-            @invoice_request.rejection_reason = nil
-            @invoice_request.other_rejection_reason = nil
-          else
-            @invoice_request_comment = InvoiceRequestComment.new(message: "Status Changed: #{@invoice_request.status}.<br/> Cancellation Reason: #{@invoice_request.cancellation_reason} ", invoice_request: @invoice_request, overseer: current_overseer)
-            @invoice_request.rejection_reason = nil
-            @invoice_request.other_rejection_reason = nil
-            @invoice_request.cancellation_reason = nil # ( Can canceled Invoice again open? )
-          end
-          @invoice_request.save!
-          @invoice_request_comment.save!
-        elsif @invoice_request.rejection_reason_changed? && @invoice_request.rejection_reason != 'Others'
-          @invoice_request.other_rejection_reason = nil
-          @invoice_request.save!
-        elsif @invoice_request.grpo_number_changed?
-          @invoice_request_comment = InvoiceRequestComment.new(message: "GRPO Number Changed: #{@invoice_request.grpo_number}", invoice_request: @invoice_request, overseer: current_overseer)
-          @invoice_request_comment.save!
-        else
-          @invoice_request.save!
-        end
-      end
-
+      service = Services::Overseers::InvoiceRequests::Update.new(@invoice_request)
+      service.call
       redirect_to overseers_invoice_request_path(@invoice_request), notice: flash_message(@invoice_request, action_name)
     else
       render 'edit'
