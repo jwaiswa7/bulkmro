f3214671329d393e590b1dc2e374856afd4b68ca
rucha parab <rucha.parab@bulkmro.com>
2019-03-05 17:41:39 +0530

    Logistics Outward Pending POD Dashboard [ch1134] - added a table in sales invoice page, added a field in sales invoice index - aggregation by date histogram implemented in elastic search base finder

--- app/controllers/overseers/sales_invoices_controller.rb
+++ app/controllers/overseers/sales_invoices_controller.rb
@@ -5,7 +5,17 @@ class Overseers::SalesInvoicesController < Overseers::BaseController
     authorize :sales_invoice
 
     respond_to do |format|
-      format.html { }
+      format.html {
+        service = Services::Overseers::Finders::SalesInvoices.new(params, current_overseer)
+        service.call
+        indexed_sales_invoices = service.indexed_records
+
+        @invoice_over_month = indexed_sales_invoices.aggregations['invoice_over_time']['buckets']
+        pod_over_month_histogram = indexed_sales_invoices.aggregations['pod_over_time']['buckets']
+        @list_of_months = @invoice_over_month.map{|i| i['key_as_string']}
+        @pod_over_month = {}
+        pod_over_month_histogram.map{|p| @pod_over_month[p['key_as_string']]=p['doc_count']}
+      }
       format.json do
         service = Services::Overseers::Finders::SalesInvoices.new(params, current_overseer)
         service.call
