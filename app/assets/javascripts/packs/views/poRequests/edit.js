2c8cf8506f050888b13d90432796ec66d04a453b
sakshiyadav95 <sakshi.yadav@bulkmro.com>
2019-03-05 12:52:26 +0530

    js added for cancel dropdown

--- app/assets/javascripts/packs/views/poRequests/edit.js
+++ app/assets/javascripts/packs/views/poRequests/edit.js
@@ -50,12 +50,32 @@ const edit = () => {
     $('form').on('click','.add-review',function (e) {
         $('.rating-modal a').click();
     })
-    $('.confirm-cancel').click(function(event) {
-        if( !confirm('Do you want to Cancel the PoRequest') ){
-            event.preventDefault();
+
+    // $('.confirm-cancel').click(function(event) {
+    //     if( !confirm('Do you want to Cancel the PoRequest') ){
+    //         event.preventDefault();
+    //     }
+    //
+    // });
+
+    $('.submit-form').unbind().bind('click', function (event) {
+        let form_changed_data = $("form").serializeArray(),
+            changed_status = form_changed_data.filter(x => x.name === "po_request[status]"),
+            original_status = form_original_data.filter(x => x.name === "po_request[status]");
+        if(changed_status){
+            let is_cancel_status = changed_status[0]['value'].toLowerCase().indexOf('cancel') != -1
+            if(is_cancel_status && original_status && (changed_status[0]['value'] != original_status[0]['value'])){
+                if (changed_status[0]['value'] == 'Cancelled'){
+                    if( !confirm('Do you want to Cancel the Po Request ?') ){
+                        event.preventDefault();
+                    }
+                }
+            }
         }
 
+
     });
+
 };
 
 export default edit
