605a1254805c97c00a0657b8221886008a598321
Lopesh <lopesh.durugkar@bulkmro.com>
2019-03-06 14:58:39 +0530

    - Add Customer PO searchable.

--- app/services/services/overseers/finders/sales_invoices.rb
+++ app/services/services/overseers/finders/sales_invoices.rb
@@ -35,7 +35,7 @@ class Services::Overseers::Finders::SalesInvoices < Services::Overseers::Finders
   end
 
   def perform_query(query_string)
-    indexed_records = index_klass.query(multi_match: { query: query_string, operator: 'and', fields: %w[invoice_number_string^3 sales_order_number_string account_string company_string^4 status_string inquiry_number_string inside_sales_owner outside_sales_owner] }).order(sort_definition).order(sort_definition)
+    indexed_records = index_klass.query(multi_match: { query: query_string, operator: 'and', fields: %w[invoice_number_string^3 cp_po_number_s  sales_order_number_string account_string company_string^4 status_string inquiry_number_string inside_sales_owner outside_sales_owner] }).order(sort_definition).order(sort_definition)
 
     indexed_records = indexed_records.filter(filter_by_value('inquiry_present', true))
 
