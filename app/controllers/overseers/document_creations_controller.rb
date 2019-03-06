c800a2ecb215d086a22fe5c6532b4164e527d31f
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-03-06 16:19:49 +0530

    more refactoring

--- app/controllers/overseers/document_creations_controller.rb
+++ app/controllers/overseers/document_creations_controller.rb
@@ -5,31 +5,31 @@ class Overseers::DocumentCreationsController < Overseers::BaseController
 
   def create
     authorize :document_creation
-    if doc_creation_params["option"] == "Purchase Order"
-      @po = PurchaseOrder.find_by_po_number(doc_creation_params["document_number"])
+    if doc_creation_params['option'] == 'Purchase Order'
+      @po = PurchaseOrder.find_by_po_number(doc_creation_params['document_number'])
       if @po.present?
-        redirect_to new_overseers_document_creation_path(id: @po.id, number: @po.po_number, option: doc_creation_params["option"]), notice: "Purchase Order already exists"
+        redirect_to new_overseers_document_creation_path(id: @po.id, number: @po.po_number, option: doc_creation_params['option']), notice: 'Purchase Order already exists'
       else
-        Resources::PurchaseOrder.set_purchase_order_items([doc_creation_params["document_number"].to_i])
-        redirect_to new_overseers_document_creation_path, notice: "Purchase Order created"
+        Resources::PurchaseOrder.set_purchase_order_items([doc_creation_params['document_number'].to_i])
+        redirect_to new_overseers_document_creation_path, notice: 'Purchase Order created'
       end
     else
-      @si = SalesInvoice.find_by_invoice_number(doc_creation_params["document_number"])
+      @si = SalesInvoice.find_by_invoice_number(doc_creation_params['document_number'])
       if @si.present?
-        redirect_to new_overseers_document_creation_path(id: @si.id, number: @si.invoice_number, option: doc_creation_params["option"]), notice: "Sales Invoice already exists"
+        redirect_to new_overseers_document_creation_path(id: @si.id, number: @si.invoice_number, option: doc_creation_params['option']), notice: 'Sales Invoice already exists'
       else
-        Resources::Invoice.set_invoice_items([doc_creation_params["document_number"].to_i])
-        redirect_to new_overseers_document_creation_path, notice: "Sales Invoice created"
+        Resources::Invoice.set_invoice_items([doc_creation_params['document_number'].to_i])
+        redirect_to new_overseers_document_creation_path, notice: 'Sales Invoice created'
       end
     end
   end
 
   private
 
-  def doc_creation_params
-    params.require(:document_creation).permit(
-      :option,
-      :document_number
-    )
-  end
+    def doc_creation_params
+      params.require(:document_creation).permit(
+        :option,
+        :document_number
+      )
+    end
 end
