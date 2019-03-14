e5bcd7ddaef2e0086554c3f8d91b10b17a3d3a28
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-03-14 16:05:25 +0530

    Corrected a typo in po_request's policy for update

--- app/controllers/overseers/po_requests_controller.rb
+++ app/controllers/overseers/po_requests_controller.rb
@@ -87,7 +87,7 @@ class Overseers::PoRequestsController < Overseers::BaseController
     if @po_request.valid?
       # todo allow only in case of zero form errors
       @po_request.status = 'PO Created' if @po_request.purchase_order.present? && @po_request.status == 'Requested'
-      @po_request.status = 'Requested' if @po_request.status == 'Rejected' && policy(@po_request).is_manager_or_sales?
+      @po_request.status = 'Requested' if @po_request.status == 'Rejected' && policy(@po_request).manager_or_sales?
       ActiveRecord::Base.transaction do if @po_request.status_changed?
                                           if @po_request.status == 'Cancelled'
                                             @po_request_comment = PoRequestComment.new(message: "Status Changed: #{@po_request.status} PO Request for Purchase Order number #{@po_request.purchase_order.po_number} \r\n Cancellation Reason: #{@po_request.cancellation_reason}", po_request: @po_request, overseer: current_overseer)
