1a70a32c65dbfc32baeacde44a9dc8728b3af28e
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2019-03-18 18:33:00 +0530

    Update Migration to run on all purchase orders

--- app/services/services/shared/migrations/add_company_to_purchase_order.rb
+++ app/services/services/shared/migrations/add_company_to_purchase_order.rb
@@ -1,7 +1,7 @@
 class Services::Shared::Migrations::AddCompanyToPurchaseOrder < Services::Shared::Migrations::Migrations
 
   def call
-    PurchaseOrder.last(10).each do |purchase_order|
+    PurchaseOrder.all.each do |purchase_order|
       purchase_order.update_attributes(company: purchase_order&.get_supplier)
       purchase_order.rows.each do |purchase_order_row|
         purchase_order_row.update_attributes(product: purchase_order_row&.get_product)
