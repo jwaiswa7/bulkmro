e087e9197f34f7129990aff42557af578b1c0d98
Amit Goyal <forsakenlegacy@gmail.com>
2019-03-18 14:45:44 +0530

    Added loader for treant.

--- app/assets/javascripts/packs/components/treant.js
+++ app/assets/javascripts/packs/components/treant.js
@@ -3,6 +3,10 @@ const treant = () => {
 
     let url = $('.treant').data('nodes');
     let nodeStructure = null;
+
+    let spinner = '<div class="text-center spinner"><i class=" fal fa-spinner-third fa-spin fa-3x fa-fw"></i></div>';
+    spinner = $(spinner).insertAfter($('.treant'));
+
     $.get(url, function (response) {
         if (response['data'] != undefined) {
             nodeStructure = response['data'];
@@ -25,6 +29,7 @@ const treant = () => {
         };
 
         new Treant(config, function() {}, $);
+        spinner.remove();
     });
 };
 
