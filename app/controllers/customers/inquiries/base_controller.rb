fccf9afc794212600e1a8332e59fbf459816cfab
Amit Goyal <forsakenlegacy@gmail.com>
2019-03-19 11:57:34 +0530

    Made the default render PDF page size as A4, instead of Legal. Attach to [branch ch1596].

--- app/controllers/customers/inquiries/base_controller.rb
+++ app/controllers/customers/inquiries/base_controller.rb
@@ -14,7 +14,7 @@ class Customers::Inquiries::BaseController < Customers::BaseController
         pdf: record.filename,
         template: ['shared', 'layouts', 'pdf_templates', record.class.name.pluralize.underscore, 'show'].join('/'),
         layout: 'shared/layouts/pdf_templates/show',
-        page_size: 'Legal',
+        page_size: 'A4',
         footer: {
           center: '[page] of [topage]'
         },
