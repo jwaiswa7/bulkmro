00fb71ecab002a51823c1a7ba83127d922757b77
Ruta kambli <ruta.kambli@bulkmro.com>
2019-03-20 11:33:36 +0530

    Payment based on status fixed.

--- app/services/services/overseers/finders/payment_requests.rb
+++ app/services/services/overseers/finders/payment_requests.rb
@@ -9,14 +9,23 @@ class Services::Overseers::Finders::PaymentRequests < Services::Overseers::Finde
 
   def all_records
     indexed_records = if current_overseer.present? && !current_overseer.allow_inquiries?
-      super.filter(filter_by_owner(current_overseer.self_and_descendant_ids).merge(filter_by_status))
-    else
-      super
-    end
+                        super.filter(filter_by_owner(current_overseer.self_and_descendant_ids))
+                      else
+                        super
+                      end
 
     if @status.present?
-      indexed_records = indexed_records.filter(filter_by_value(:status, @status))
+      if @status == 'Completed'
+        indexed_records = indexed_records.filter(filter_by_array(:status, [50]))
+      elsif @status == 'Rejected'
+        reject_statuses = ['Supplier Info: Bank Details Missing', 'Supplier Info: Bank Details Incorrect', 'Supplier Info: PI mismatch', 'Rejected: Others']
+        status_array = reject_statuses.map{|status| PaymentRequest.statuses[status]}
+        indexed_records = indexed_records.filter(filter_by_array(:status, status_array))
+      end
     end
+    # if @owner_type.present?
+    #   indexed_records = indexed_records.filter(filter_by_value(:request_owner, 'Accounts'))
+    # end
 
     if search_filters.present?
       indexed_records = filter_query(indexed_records)
@@ -27,16 +36,22 @@ class Services::Overseers::Finders::PaymentRequests < Services::Overseers::Finde
   end
 
   def perform_query(query_string)
-    indexed_records = index_klass.query(multi_match: { query: query_string, operator: 'and', fields: %w[updated_by_id status_string inquiry_number_string^5 inside_sales_owner outside_sales_owner ] }).order(sort_definition)
+    indexed_records = index_klass.query(multi_match: { query: query_string, operator: 'and', fields: %w[updated_by_id status_string inquiry_number_string^5 inside_sales_owner outside_sales_owner request_owner ] }).order(sort_definition)
 
     if current_overseer.present? && !current_overseer.allow_inquiries?
-      indexed_records = indexed_records.filter(filter_by_owner(current_overseer.self_and_descendant_ids).merge(filter_by_status))
+      indexed_records = indexed_records.filter(filter_by_owner(current_overseer.self_and_descendant_ids))
     else
       indexed_records = indexed_records
     end
 
     if @status.present?
-      indexed_records = indexed_records.filter(filter_by_value(:status, @status.to_i))
+      if @status == 'Completed'
+        indexed_records = indexed_records.filter(filter_by_array(:status, [50]))
+      elsif @status == 'Rejected'
+        reject_statuses = ['Supplier Info: Bank Details Missing', 'Supplier Info: Bank Details Incorrect', 'Supplier Info: PI mismatch', 'Rejected: Others']
+        status_array = reject_statuses.map{|status| PaymentRequest.statuses[status]}
+        indexed_records = indexed_records.filter(filter_by_array(:status, status_array))
+      end
     end
 
     if search_filters.present?
