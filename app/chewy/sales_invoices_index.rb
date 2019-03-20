fda6df6ed65bfae9dd6a50396a31d42277bd3fcc
rucha parab <rucha.parab@bulkmro.com>
2019-03-20 18:56:52 +0530

    sales invoice index fixes,removed field pod_created_at[ch1134]

--- app/chewy/sales_invoices_index.rb
+++ app/chewy/sales_invoices_index.rb
@@ -34,10 +34,10 @@ class SalesInvoicesIndex < BaseIndex
     field :cp_po_number_s, value: -> (record) { record.inquiry.customer_po_number.to_s if record.inquiry.present? && record.inquiry.customer_po_number.present? }, analyzer: 'substring'
     field :cp_order_date_s, value: -> (record) { record.inquiry.customer_order_date.strftime('%d-%b-%Y').to_s if record.inquiry.present? && record.inquiry.customer_order_date.present? }, analyzer: 'substring'
     field :potential_value, value: -> (record) { record.report_total }, type: 'double'
-    field :pod_created_at, value: -> (record) {record.mis_date if !record.has_attachment? && record.status == 'Cancelled'}, type: 'date'
-    field :is_pod, value: -> (record) {(record.has_attachment? ? 1 : 0 )if record.status == 'Cancelled'}, type: 'integer'
-    field :regular_pod, value: -> (record) {record.mis_date if !record.has_attachment? && record.inquiry.present? && record.inquiry.opportunity_type != 'route_through' && record.status == 'Cancelled' }, type: 'date'
-    field :route_through_pod, value: -> (record) {record.mis_date if !record.has_attachment? && record.inquiry.present? && record.inquiry.opportunity_type == 'route_through' && record.status == 'Cancelled'}, type: 'date'
+    field :invoice_created_at, value: -> (record) { record.mis_date if record.status != 'Cancelled' }, type: 'date'
+    field :is_pod, value: -> (record) {(record.has_attachment? ? 1 : 0 )if record.status != 'Cancelled'}, type: 'integer'
+    field :regular_pod, value: -> (record) {record.mis_date if !record.has_attachment? && record.inquiry.present? && record.inquiry.opportunity_type != 'route_through' && record.status != 'Cancelled' }, type: 'date'
+    field :route_through_pod, value: -> (record) {record.mis_date if !record.has_attachment? && record.inquiry.present? && record.inquiry.opportunity_type == 'route_through' && record.status != 'Cancelled'}, type: 'date'
     field :opportunity_type, value: -> (record) {opportunity_type[record.inquiry.opportunity_type] if record.inquiry.present?}, type: 'integer'
   end
 end
