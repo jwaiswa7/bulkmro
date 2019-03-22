ddb89d80867db2c12cc1e2d6057313064bfe6e4d
Amit Goyal <forsakenlegacy@gmail.com>
2019-03-22 11:48:01 +0530

    Resolved Hound issues; those that are worth fixing.

--- app/controllers/overseers/inquiries_controller.rb
+++ app/controllers/overseers/inquiries_controller.rb
@@ -1,5 +1,5 @@
 class Overseers::InquiriesController < Overseers::BaseController
-  before_action :set_inquiry, only: [:show, :edit, :update, :edit_suppliers, :update_suppliers, :export, :calculation_sheet, :stages, :relationship_map, :get_relationship_map_json, :resync_inquiry_products, :resync_unsync_inquiry_products ]
+  before_action :set_inquiry, only: [:show, :edit, :update, :edit_suppliers, :update_suppliers, :export, :calculation_sheet, :stages, :relationship_map, :get_relationship_map_json, :resync_inquiry_products, :resync_unsync_inquiry_products]
 
   def index
     authorize :inquiry
@@ -86,7 +86,7 @@ class Overseers::InquiriesController < Overseers::BaseController
     authorize @inquiry
 
     send_file(
-      "#{Rails.root}/public/calculation_sheet/Calc_Sheet.xlsx",
+        "#{Rails.root}/public/calculation_sheet/Calc_Sheet.xlsx",
         filename: "##{@inquiry.inquiry_number} Calculation Sheet.xlsx"
     )
   end
@@ -189,9 +189,10 @@ class Overseers::InquiriesController < Overseers::BaseController
   def get_relationship_map_json
     authorize @inquiry
     purchase_order = PurchaseOrder.includes(po_request: :sales_order).where(inquiry_id: @inquiry).where(po_requests: {id: nil}, sales_orders: {id: nil})
-    inquiry_json = Services::Overseers::Inquiries::RelationshipMap.new(@inquiry, @inquiry.sales_quotes,purchase_order).call
+    inquiry_json = Services::Overseers::Inquiries::RelationshipMap.new(@inquiry, @inquiry.sales_quotes, purchase_order).call
     render json: {data: inquiry_json}
   end
+
   def create_purchase_orders_requests
     @inquiry = Inquiry.find(new_purchase_orders_requests_params[:id])
     authorize @inquiry
@@ -212,64 +213,64 @@ class Overseers::InquiriesController < Overseers::BaseController
 
   private
 
-    def set_inquiry
-      @inquiry ||= Inquiry.find(params[:id])
-    end
+  def set_inquiry
+    @inquiry ||= Inquiry.find(params[:id])
+  end
 
-    def inquiry_params
-      params.require(:inquiry).permit(
+  def inquiry_params
+    params.require(:inquiry).permit(
         :project_uid,
-          :company_id,
-          :contact_id,
-          :industry_id,
-          :inside_sales_owner_id,
-          :outside_sales_owner_id,
-          :sales_manager_id,
-          :billing_address_id,
-          :billing_company_id,
-          :shipping_address_id,
-          :shipping_company_id,
-          :shipping_contact_id,
-          :bill_from_id,
-          :ship_from_id,
-          :status,
-          :opportunity_type,
-          :opportunity_source,
-          :subject,
-          :gross_profit_percentage,
-          :quotation_date,
-          :customer_committed_date,
-          :customer_order_date,
-          :valid_end_time,
-          :quotation_followup_date,
-          :procurement_date,
-          :expected_closing_date,
-          :quote_category,
-          :price_type,
-          :potential_amount,
-          :freight_option,
-          :freight_cost,
-          :total_freight_cost,
-          :customer_po_number,
-          :packing_and_forwarding_option,
-          :payment_option_id,
-          :weight_in_kgs,
-          :customer_po_sheet,
-          :final_supplier_quote,
-          :copy_of_email,
-          :is_sez,
-          :calculation_sheet,
-          :commercial_terms_and_conditions,
-          :comments,
-          :product_type,
-          supplier_quotes: [],
-          inquiry_products_attributes: [:id, :product_id, :sr_no, :quantity, :bp_catalog_name, :bp_catalog_sku, :_destroy]
-      )
-    end
+        :company_id,
+        :contact_id,
+        :industry_id,
+        :inside_sales_owner_id,
+        :outside_sales_owner_id,
+        :sales_manager_id,
+        :billing_address_id,
+        :billing_company_id,
+        :shipping_address_id,
+        :shipping_company_id,
+        :shipping_contact_id,
+        :bill_from_id,
+        :ship_from_id,
+        :status,
+        :opportunity_type,
+        :opportunity_source,
+        :subject,
+        :gross_profit_percentage,
+        :quotation_date,
+        :customer_committed_date,
+        :customer_order_date,
+        :valid_end_time,
+        :quotation_followup_date,
+        :procurement_date,
+        :expected_closing_date,
+        :quote_category,
+        :price_type,
+        :potential_amount,
+        :freight_option,
+        :freight_cost,
+        :total_freight_cost,
+        :customer_po_number,
+        :packing_and_forwarding_option,
+        :payment_option_id,
+        :weight_in_kgs,
+        :customer_po_sheet,
+        :final_supplier_quote,
+        :copy_of_email,
+        :is_sez,
+        :calculation_sheet,
+        :commercial_terms_and_conditions,
+        :comments,
+        :product_type,
+        supplier_quotes: [],
+        inquiry_products_attributes: [:id, :product_id, :sr_no, :quantity, :bp_catalog_name, :bp_catalog_sku, :_destroy]
+    )
+  end
 
-    def edit_suppliers_params
-      if params.has_key?(:inquiry)
-        params.require(:inquiry).permit(
+  def edit_suppliers_params
+    if params.has_key?(:inquiry)
+      params.require(:inquiry).permit(
           inquiry_products_attributes: [
               :id,
               inquiry_product_suppliers_attributes: [
@@ -281,66 +282,66 @@ class Overseers::InquiriesController < Overseers::BaseController
                   :_destroy
               ]
           ]
-        )
-      else
-        {}
-      end
+      )
+    else
+      {}
     end
+  end
 
-    def new_purchase_orders_requests_params
-      if params.has_key?(:inquiry)
-        params.require(:inquiry).permit(
+  def new_purchase_orders_requests_params
+    if params.has_key?(:inquiry)
+      params.require(:inquiry).permit(
           :id,
-            po_requests_attributes: [
-                :id,
-                :supplier_id,
-                :inquiry_id,
-                :company_id,
-                :reason_to_stock,
-                :estimated_date_to_unstock,
-                :requested_by_id,
-                :approved_by_id,
-                :_destroy,
-                :logistics_owner_id,
-                :address_id,
-                :contact_id,
-                :payment_option_id,
-                :stock_status,
-                :supplier_committed_date,
-                :blobs,
-                :supplier_po_type,
-                :contact_email,
-                :contact_phone,
-                :bill_from_id,
-                :ship_from_id,
-                :bill_to_id,
-                :ship_to_id,
-                attachments: [],
-                rows_attributes: [
-                    :id,
-                    :_destroy,
-                    :status,
-                    :quantity,
-                    :sales_order_row_id,
-                    :product_id,
-                    :brand,
-                    :tax_code_id,
-                    :tax_rate_id,
-                    :measurement_unit_id,
-                    :unit_price,
-                    :conversion,
-                    :lead_time,
-                    :discount_percentage
-                ],
-                comments_attributes: [
-                    :created_by_id,
-                    :updated_by_id,
-                    :message
-                ]
-            ]
-        )
-      else
-        {}
-      end
+          po_requests_attributes: [
+              :id,
+              :supplier_id,
+              :inquiry_id,
+              :company_id,
+              :reason_to_stock,
+              :estimated_date_to_unstock,
+              :requested_by_id,
+              :approved_by_id,
+              :_destroy,
+              :logistics_owner_id,
+              :address_id,
+              :contact_id,
+              :payment_option_id,
+              :stock_status,
+              :supplier_committed_date,
+              :blobs,
+              :supplier_po_type,
+              :contact_email,
+              :contact_phone,
+              :bill_from_id,
+              :ship_from_id,
+              :bill_to_id,
+              :ship_to_id,
+              attachments: [],
+              rows_attributes: [
+                  :id,
+                  :_destroy,
+                  :status,
+                  :quantity,
+                  :sales_order_row_id,
+                  :product_id,
+                  :brand,
+                  :tax_code_id,
+                  :tax_rate_id,
+                  :measurement_unit_id,
+                  :unit_price,
+                  :conversion,
+                  :lead_time,
+                  :discount_percentage
+              ],
+              comments_attributes: [
+                  :created_by_id,
+                  :updated_by_id,
+                  :message
+              ]
+          ]
+      )
+    else
+      {}
     end
+  end
 end
