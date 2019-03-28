5a54eca10973749d24cbf38b2882575ff7239eba
rucha parab <rucha.parab@bulkmro.com>
2019-03-28 12:09:29 +0530

    Pagination removed for kra report [branch ch1594]

--- app/assets/javascripts/packs/views/inquiries/kraReport.js
+++ app/assets/javascripts/packs/views/inquiries/kraReport.js
@@ -3,7 +3,6 @@ const kraReport = () => {
     var dataTable = $('.datatable').dataTable();
     dataTable.api().page.len(-1).draw()
     dataTable.on('draw.dt', function () {
-        console.log($('.dataTables_paginate'))
         $('.dataTables_paginate').hide()
     })
 }
