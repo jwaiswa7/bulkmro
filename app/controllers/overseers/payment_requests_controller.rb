1e921f24c3b9a5bb7f9d0fbb1b7e0107fec49f2d
rucha parab <rucha.parab@bulkmro.com>
2019-03-28 18:08:38 +0530

    comment added on payment_request [ch1255]

--- app/controllers/overseers/payment_requests_controller.rb
+++ app/controllers/overseers/payment_requests_controller.rb
@@ -21,6 +21,10 @@ class Overseers::PaymentRequestsController < Overseers::BaseController
     @payment_requests = PaymentRequest.where(id: params[:payment_requests])
     authorize @payment_requests
     @payment_requests.update_all(status: params[:status_id].to_i)
+    @payment_requests.each do |payment_request|
+      payment_comment = PaymentRequestComment.new(message: "Status Changed:#{PaymentRequest.statuses.invert[params[:status_id].to_i]}" , payment_request: payment_request, overseer: current_overseer)
+      payment_comment.save
+    end
   end
 
   def show
