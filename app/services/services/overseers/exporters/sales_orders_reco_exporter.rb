1ba20aafaf08cdfb394d4ce34b98204307510ed1
Ruta kambli <ruta.kambli@bulkmro.com>
2019-03-29 10:39:28 +0530

    rubocop applied

--- app/services/services/overseers/exporters/sales_orders_reco_exporter.rb
+++ app/services/services/overseers/exporters/sales_orders_reco_exporter.rb
@@ -8,15 +8,14 @@ class Services::Overseers::Exporters::SalesOrdersRecoExporter < Services::Overse
   end
 
   def call
-    perform_export_later('SalesOrdersRecoExporter',@arguments)
+    perform_export_later('SalesOrdersRecoExporter', @arguments)
   end
 
   def build_csv
-    model.remote_approved.where.not(sales_quote_id: nil).where(mis_date: start_at..end_at).order(mis_date: :desc).includes(:inquiry,:po_requests).each_with_index do |sales_order, index|
+    model.remote_approved.where.not(sales_quote_id: nil).where(mis_date: start_at..end_at).order(mis_date: :desc).includes(:inquiry, :po_requests).each_with_index do |sales_order, index|
       inquiry = sales_order.inquiry
       purchase_order_ids = sales_order.po_requests.pluck(:purchase_order_id).compact
       purchase_orders = PurchaseOrder.where(id: purchase_order_ids).where.not(status: 'cancelled')
-
       rows.push(
         serial: index,
         inquiry: inquiry.inquiry_number,
@@ -27,7 +26,7 @@ class Services::Overseers::Exporters::SalesOrdersRecoExporter < Services::Overse
         sales_order_total: sales_order.inside_sales_owner.try(:full_name),
         po_count: purchase_orders.count,
         product_cost: purchase_orders.sum(&:calculated_total_without_tax)
-                )
+      )
     end
     export = Export.create!(export_type: 65, filtered: true, created_by_id: @overseer.id, updated_by_id: @overseer.id)
     generate_csv(export)
