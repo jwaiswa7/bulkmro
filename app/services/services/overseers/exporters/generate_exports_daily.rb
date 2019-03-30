bc4a9e27e90b29ae632f5d8e638f4f0b3acac1ae
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2019-03-30 11:42:18 +0530

    Try to Fix clock file

--- app/services/services/overseers/exporters/generate_exports_daily.rb
+++ app/services/services/overseers/exporters/generate_exports_daily.rb
@@ -1,15 +1,3 @@
-class Services::Overseers::Exporters::GenerateExportsHourly < Services::Shared::BaseService
-  def initialize
-    export_arr = [
-        'InquiriesExporter',
-        'SalesOrdersExporter',
-    ]
-    export_arr.each do |value|
-      ['Services', 'Overseers', 'Exporters', value].join('::').constantize.new.call
-    end
-  end
-end
-
 class Services::Overseers::Exporters::GenerateExportsDaily < Services::Shared::BaseService
   def initialize
     export_arr = [
