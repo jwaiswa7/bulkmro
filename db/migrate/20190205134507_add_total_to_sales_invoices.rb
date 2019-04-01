a89321a7134bbf046d1c53c23724b8f4ceeb0108
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-04-01 18:21:21 +0530

    add history logs for grpo requests

--- db/migrate/20190205134507_add_total_to_sales_invoices.rb
+++ db/migrate/20190205134507_add_total_to_sales_invoices.rb
@@ -1,5 +1,5 @@
 class AddTotalToSalesInvoices < ActiveRecord::Migration[5.2]
   def change
-    add_column :sales_invoices, :calculated_total, :decimal, default: 0.0
+    add_column :sales_invoices, :calculated_total, :decimal, default: 0.0 unless column_exists? :sales_invoices, :calculated_total
   end
 end
--- db/migrate/20190206074752_add_account_id_field_in_sales_receipt.rb
+++ db/migrate/20190206074752_add_account_id_field_in_sales_receipt.rb
@@ -1,5 +1,5 @@
 class AddAccountIdFieldInSalesReceipt < ActiveRecord::Migration[5.2]
   def change
-    add_reference :sales_receipts,:account,foreign_key: true
+    add_reference :sales_receipts,:account,foreign_key: true unless column_exists? :sales_receipts,:account_id
   end
 end
