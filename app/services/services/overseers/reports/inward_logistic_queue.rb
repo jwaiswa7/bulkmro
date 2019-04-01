a9d255c0a1335a982b208339bbc8cabe5826ffa3
Lopesh <lopesh.durugkar@bulkmro.com>
2019-04-01 15:15:25 +0530

    - Make logistics owner as ascending order

--- app/services/services/overseers/reports/inward_logistic_queue.rb
+++ app/services/services/overseers/reports/inward_logistic_queue.rb
@@ -1,20 +1,17 @@
 class Services::Overseers::Reports::InwardLogisticQueue < Services::Overseers::Reports::BaseReport
   def call
     purchase_orders = PurchaseOrder.joins(:inward_dispatches).joins(:po_request)
-
     @data = OpenStruct.new(
       entries: {}
     )
     with_material_not_ready = PurchaseOrder.where.not(id: PurchaseOrder.joins(:inward_dispatches)).joins(:po_request).where(po_requests: {status: 'Supplier PO Sent'}).group('logistics_owner_id').count
-    pickups_pending = purchase_orders.where.not(po_requests: {id: nil}).where(material_status: ['Material Pickedup','Material Partially Pickedup','Material Partially Delivered']).group(:logistics_owner_id).count
-    inward_pending = purchase_orders.where.not(po_requests: {id: nil}).where(:material_status => ['Material Delivered', 'Material Partially Delivered']).group(:logistics_owner_id).count
-
-    logistic_overseers = Overseer.where(role: 'logistics')
-    @data.entries[:nil] = {name: "Not Assigned" , with_material_not_ready: 0, material_pickups_pending: 0, with_inward_pending: 0, with_status_partial_grpo_done: 0}
-
+    pickups_pending = purchase_orders.where.not(po_requests: {id: nil}).where(material_status: ['Material Pickedup', 'Material Partially Pickedup', 'Material Partially Delivered']).group(:logistics_owner_id).count
+    inward_pending = purchase_orders.where.not(po_requests: {id: nil}).where(material_status: ['Material Delivered', 'Material Partially Delivered']).group(:logistics_owner_id).count
+    logistic_overseers = Overseer.where(role: 'logistics').order(:first_name)
+    @data.entries[:nil] = {name: 'Not Assigned', with_material_not_ready: 0, material_pickups_pending: 0, with_inward_pending: 0, with_status_partial_grpo_done: 0}
     logistic_overseers.each do |logistic_overseer|
       @data.entries[logistic_overseer.id.to_s] = { name: logistic_overseer.to_s, with_material_not_ready: 0, material_pickups_pending: 0, with_inward_pending: 0, with_status_partial_grpo_done: 0 }
-      get_key_value([with_material_not_ready, pickups_pending, inward_pending], ["with_material_not_ready", "material_pickups_pending", "with_inward_pending"], logistic_overseer.id)
+      get_key_value([with_material_not_ready, pickups_pending, inward_pending], ['with_material_not_ready', 'material_pickups_pending', 'with_inward_pending'], logistic_overseer.id)
     end
     data
   end
