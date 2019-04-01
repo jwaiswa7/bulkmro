952b7883053019b834dd4160bcfedaa409c4e35b
Lopesh <lopesh.durugkar@bulkmro.com>
2019-04-01 12:20:25 +0530

    - Add the table format for inward logistics queue in view file. - changes in inward logistics queue service file.

--- app/services/services/overseers/reports/inward_logistic_queue.rb
+++ app/services/services/overseers/reports/inward_logistic_queue.rb
@@ -5,11 +5,18 @@ class Services::Overseers::Reports::InwardLogisticQueue < Services::Overseers::R
     @data = OpenStruct.new(
       entries: {}
     )
+    with_material_not_ready = PurchaseOrder.where.not(id: PurchaseOrder.joins(:inward_dispatches)).joins(:po_request).where(po_requests: {status: 'Supplier PO Sent'}).group('logistics_owner_id').count
 
     logistic_overseers = Overseer.where(role: 'logistics')
+    @data.entries[:nil] = {name: "Not Assigned" , with_material_not_ready: 0, material_pickups_pending: 0, with_inward_pending: 0, with_status_partial_grpo_done: 0}
     logistic_overseers.each do |logistic_overseer|
-      data.entries[logistic_overseer.id] = { name: logistic_overseer.to_s}
+      @data.entries[logistic_overseer.id.to_s] = {name: logistic_overseer.to_s, with_material_not_ready: 0, material_pickups_pending: 0, with_inward_pending: 0, with_status_partial_grpo_done: 0}
+      if with_material_not_ready.present? && with_material_not_ready.key?(logistic_overseer.id)
+        @data.entries[logistic_overseer.id.to_s][:with_material_not_ready] = with_material_not_ready[logistic_overseer.id]
+      elsif with_material_not_ready.present? && with_material_not_ready.key?(nil)
+        @data.entries[:nil][:with_material_not_ready] = with_material_not_ready[nil]
+      end
     end
-    attr_accessor :data
+    data
   end
 end
