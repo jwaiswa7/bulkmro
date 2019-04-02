3dbe8006b6852341ada74b990ddd1fa4763c6d69
sourabhraje <47655913+sourabhraje@users.noreply.github.com>
2019-04-02 10:48:52 +0530

    Revert "Feature contact creation from activity"

--- db/migrate/20190319114650_add_phone_number_field_to_company_creation_request.rb
+++
@@ -1,8 +0,0 @@
-class AddPhoneNumberFieldToCompanyCreationRequest < ActiveRecord::Migration[5.2]
-  def change
-    add_column :company_creation_requests, :telephone, :string
-    add_column :company_creation_requests, :mobile_number, :string
-    add_column :company_creation_requests, :create_new_contact, :boolean
-    add_column :company_creation_requests, :contact_creation_request_id, :integer
-  end
-end
