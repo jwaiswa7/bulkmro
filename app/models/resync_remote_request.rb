6d452e5bc6cffad044c8fa09a72c8c77c7c4fe30
pradeep <pradeep.ketkale@bulkmro.com>
2019-04-05 17:42:00 +0530

    Merge branch 'master' of github.com:bulkmro/sprint into Feature-supplier-index-improvement
    # Conflicts:
    #	app/services/services/shared/migrations/migrations.rb

---
+++ app/models/resync_remote_request.rb
@@ -0,0 +1,7 @@
+class ResyncRemoteRequest < ApplicationRecord
+  include Mixins::CanBeStamped
+  include Mixins::IsARequest
+
+  pg_search_scope :locate, :against => [:url], :associated_against => {}, :using => { :tsearch => { :prefix => true } }
+
+end
