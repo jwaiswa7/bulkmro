e02618488133d9f5f3534cf39c21b46b6d550eac
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-04-08 12:12:33 +0530

    [WIP] Filters

--- app/services/services/overseers/finders/pipeline_reports.rb
+++ app/services/services/overseers/finders/pipeline_reports.rb
@@ -10,49 +10,49 @@ class Services::Overseers::Finders::PipelineReports < Services::Overseers::Finde
       super
     end
 
-    if @status.present?
-      indexed_records = indexed_records.filter(filter_by_value(:status, @status))
-    end
-
-    if search_filters.present?
-      indexed_records = filter_query(indexed_records)
-    end
-
-    if range_filters.present?
-      indexed_records = range_query(indexed_records)
-    end
+    # if @status.present?
+    #   indexed_records = indexed_records.filter(filter_by_value(:status, @status))
+    # end
+    #
+    # if search_filters.present?
+    #   indexed_records = filter_query(indexed_records)
+    # end
+    #
+    # if range_filters.present?
+    #   indexed_records = range_query(indexed_records)
+    # end
 
     indexed_records = aggregation_pipeline_report(indexed_records)
     indexed_records
   end
 
-  def perform_query(query_string)
-    indexed_records = index_klass.query(
-      multi_match: {
-          query: query_string,
-          operator: 'and',
-          fields: %w[inside_sales_owner]
-      }
-    ).order(sort_definition)
+  # def perform_query(query_string)
+  #   indexed_records = index_klass.query(
+  #     multi_match: {
+  #         query: query_string,
+  #         operator: 'and',
+  #         fields: %w[inside_sales_owner]
+  #     }
+  #   ).order(sort_definition)
+  #
+  #   if current_overseer.present? && !current_overseer.allow_inquiries?
+  #     indexed_records = indexed_records.filter(filter_by_owner(current_overseer.self_and_descendant_ids))
+  #   end
 
-    if current_overseer.present? && !current_overseer.allow_inquiries?
-      indexed_records = indexed_records.filter(filter_by_owner(current_overseer.self_and_descendant_ids))
-    end
-
-    if @status.present?
-      indexed_records = indexed_records.filter(filter_by_value(:status, @status))
-    end
-
-    if search_filters.present?
-      indexed_records = filter_query(indexed_records)
-    end
-
-    if range_filters.present?
-      indexed_records = range_query(indexed_records)
-    end
-    indexed_records = aggregation_pipeline_report(indexed_records)
-    indexed_records
-  end
+    # if @status.present?
+    #   indexed_records = indexed_records.filter(filter_by_value(:status, @status))
+    # end
+    #
+    # if search_filters.present?
+    #   indexed_records = filter_query(indexed_records)
+    # end
+    #
+    # if range_filters.present?
+    #   indexed_records = range_query(indexed_records)
+    # end
+  #   indexed_records = aggregation_pipeline_report(indexed_records)
+  #   indexed_records
+  # end
 
   def aggregation_pipeline_report(indexed_records)
     if @pipeline_report_params.present?
@@ -62,7 +62,50 @@ class Services::Overseers::Finders::PipelineReports < Services::Overseers::Finde
     else
       date_range = {from: Date.new(2019, 01, 01).strftime('%Y-%m-%d'), to: Date.today.strftime('%Y-%m-%d'), key: 'custom-range'}
     end
-    # raise
+
+    pipeline_query = {
+        'inquiries_over_time': {
+            'date_histogram': {
+                'field': 'created_at',
+                'interval': 'month',
+                keyed: true,
+                order: {_key: 'desc'}
+            },
+            aggs: {
+                'pipeline': {
+                    'terms': {'field': 'status_key'},
+                    aggs: {
+                        inquiry_value: {
+                            sum: {
+                                field: 'calculated_total'
+                            }
+                        }
+                    }
+                },
+                'sum_monthly_sales': {
+                    'sum_bucket': {
+                        'buckets_path': 'pipeline>inquiry_value'
+                    }
+                }
+            }
+        },
+        'summary_row': {
+            'terms': {'field': 'status_key'},
+            aggs: {
+                statuswise_inquiry_summary: {
+                    sum: {
+                        field: 'calculated_total'
+                    }
+                }
+            }
+        },
+        'summary_row_total': {
+            'sum_bucket': {
+                'buckets_path': 'summary_row>statuswise_inquiry_summary'
+            }
+        }
+    }
+
     indexed_records = indexed_records.aggregations(
       'pipeline_filter': {
           'date_range': {
@@ -73,48 +116,7 @@ class Services::Overseers::Finders::PipelineReports < Services::Overseers::Finde
               ],
               'keyed': 'true'
           },
-          'aggs': {
-              'inquiries_over_time': {
-                  'date_histogram': {
-                      'field': 'created_at',
-                      'interval': 'month',
-                      keyed: true,
-                      order: {_key: 'desc'}
-                  },
-                  aggs: {
-                      'pipeline': {
-                          'terms': {'field': 'status_key'},
-                          aggs: {
-                              inquiry_value: {
-                                  sum: {
-                                      field: 'calculated_total'
-                                  }
-                              }
-                          }
-                      },
-                      'sum_monthly_sales': {
-                          'sum_bucket': {
-                              'buckets_path': 'pipeline>inquiry_value'
-                          }
-                      }
-                  }
-              },
-              'summary_row': {
-                  'terms': {'field': 'status_key'},
-                  aggs: {
-                      statuswise_inquiry_summary: {
-                          sum: {
-                              field: 'calculated_total'
-                          }
-                      }
-                  }
-              },
-              'summary_row_total': {
-            'sum_bucket': {
-                'buckets_path': 'summary_row>statuswise_inquiry_summary'
-            }
-        }
-          }
+          'aggs': pipeline_query
       }
     )
     indexed_records
