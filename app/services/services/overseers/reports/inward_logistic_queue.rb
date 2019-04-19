c0fd29fe0fa304ff1008ea81f28dafc20c4911d4
Lopesh <lopesh.durugkar@bulkmro.com>
2019-04-19 17:11:03 +0530

    Fix the logistics reports count in summary boxes.

--- app/services/services/overseers/reports/inward_logistic_queue.rb
+++ app/services/services/overseers/reports/inward_logistic_queue.rb
@@ -6,8 +6,11 @@ class Services::Overseers::Reports::InwardLogisticQueue < Services::Overseers::R
       summary_box_data: { with_material_not_ready: 0, material_pickups_pending: 0, with_inward_pending: 0, with_status_partial_grpo_done: 0 }
     )
     with_material_not_ready = PurchaseOrder.where.not(id: PurchaseOrder.joins(:inward_dispatches)).joins(:po_request).where(po_requests: {status: 'Supplier PO Sent'}).group('logistics_owner_id').count
+    @data.summary_box_data[:with_material_not_ready] = with_material_not_ready.values.sum
     pickups_pending = purchase_orders.where.not(po_requests: {id: nil}).where(material_status: ['Inward Dispatch', 'Inward Dispatch: Partial', 'Material Partially Delivered']).group(:logistics_owner_id).count
+    @data.summary_box_data[:material_pickups_pending] = pickups_pending.values.sum
     inward_pending = purchase_orders.where.not(po_requests: {id: nil}).where(material_status: ['Material Delivered', 'Material Partially Delivered']).group(:logistics_owner_id).count
+    @data.summary_box_data[:with_inward_pending] = inward_pending.values.sum
     logistic_overseers = Overseer.where(role: 'logistics').order(:first_name)
     @data.entries[:nil] = {name: 'Not Assigned', with_material_not_ready: 0, material_pickups_pending: 0, with_inward_pending: 0, with_status_partial_grpo_done: 0}
     logistic_overseers.each do |logistic_overseer|
@@ -21,10 +24,8 @@ class Services::Overseers::Reports::InwardLogisticQueue < Services::Overseers::R
     objects.each_with_index do |object, index|
       if object.present? && object.key?(overseer_id)
         @data.entries[overseer_id.to_s][keys[index].to_sym] = object[overseer_id]
-        @data.summary_box_data[keys[index].to_sym] = @data.summary_box_data[keys[index].to_sym] + object[overseer_id]
       elsif object.present? && object.key?(nil)
         @data.entries[:nil][keys[index].to_sym] = object[nil]
-        @data.summary_box_data[keys[index].to_sym] = @data.summary_box_data[keys[index].to_sym] + object[nil]
       end
     end
   end
