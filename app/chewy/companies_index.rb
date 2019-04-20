928bd40e0fe6e009b65de7b901939315e6998996
pradeep <pradeep.ketkale@bulkmro.com>
2019-04-20 13:09:57 +0530

    Company index fix check

--- app/chewy/companies_index.rb
+++ app/chewy/companies_index.rb
@@ -1,6 +1,6 @@
 class CompaniesIndex < BaseIndex
   businesses = Company.nature_of_businesses
-  define_type Company.where('id < 100').with_includes do
+  define_type Company.with_includes do
     field :id, type: 'integer'
     field :account_id, value: -> (record) {record.account_id}
     field :name, value: -> (record) {record.name}, analyzer: 'fuzzy_substring'
@@ -26,21 +26,21 @@ class CompaniesIndex < BaseIndex
 
     field :created_at, value: -> (record) {record.created_at}, type: 'date'
     field :updated_at, value: -> (record) {record.updated_at}, type: 'date'
-    field :inquiries_size, value: -> (record) {record.inquiry_size}, type: 'integer'
-    field :inquiries, value: -> (record) {record.inquiries.count}, type: 'integer'
-    field :invoices_count, value: -> (record) {record.inquiries.map{|i| i.invoices.pluck(:id)}.flatten.compact.count}, type: 'integer'
-    field :sales_quote_count, value: -> (record) {record.inquiries.map{|i| i.final_sales_quote}.flatten.compact.count}, type: 'integer'
-    field :sales_order_count, value: -> (record) {record.inquiries.map{|i| i.final_sales_orders.pluck(:id)}.flatten.compact.count}, type: 'integer'
-    field :expected_order, value: -> (record) {record.inquiries.where('inquiries.status = ?', 7).size}, type: 'integer'
-    field :expected_value, value: -> (record) { record.inquiries.where('inquiries.status = ?', 7).map { |ip| ip.final_sales_quote.calculated_total if ip.final_sales_quote.present?}.compact.flatten.sum }, type: 'double'
-    field :order_won, value: -> (record) {record.inquiries.where('inquiries.status = ?', 18).size}, type: 'integer'
-    field :company_key, value: -> (record) { record.id }, type: 'integer'
-    field :total_quote_value, value: -> (record) { record.inquiries.where.not('inquiries.status = ? OR inquiries.status = ?', 10,9).map { |ip| ip.final_sales_quote.calculated_total if ip.final_sales_quote.present?}.compact.flatten.sum }, type: 'double'
-    field :total_order_value, value: -> (record) {record.inquiries.map{|i| i.final_sales_orders.map{|s| s.calculated_total}}.compact.flatten.sum}, type: 'double'
-    field :total_margin, value: -> (record) { record.inquiries.map{|i| i.final_sales_orders.map{|s| s.calculated_total_margin}}.compact.flatten.sum }, type: 'double'
-    field :margin_percentage, value: -> (record) { record.order_margin.sum.to_f / record.order_margin.count }, type: 'double'
-    field :amount_invoiced, value: -> (record) { record.inquiries.map{|i| i.invoices.map{|s| s.calculated_total}}.compact.flatten.sum }, type: 'double'
-    field :cancelled_invoiced, value: -> (record) { record.inquiries.map{|i| i.invoices.where(status: 'Cancelled').pluck(:id)}.flatten.compact.count }, type: 'integer'
-    field :invoice_margin_percentage, value: -> (record) {record.invoice_margin.sum.to_f / record.invoice_margin.count }, type: 'double'
+    # field :inquiries_size, value: -> (record) {record.inquiry_size}, type: 'integer'
+    # field :inquiries, value: -> (record) {record.inquiries.count}, type: 'integer'
+    # field :invoices_count, value: -> (record) {record.inquiries.map{|i| i.invoices.pluck(:id)}.flatten.compact.count}, type: 'integer'
+    # field :sales_quote_count, value: -> (record) {record.inquiries.map{|i| i.final_sales_quote}.flatten.compact.count}, type: 'integer'
+    # field :sales_order_count, value: -> (record) {record.inquiries.map{|i| i.final_sales_orders.pluck(:id)}.flatten.compact.count}, type: 'integer'
+    # field :expected_order, value: -> (record) {record.inquiries.where('inquiries.status = ?', 7).size}, type: 'integer'
+    # field :expected_value, value: -> (record) { record.inquiries.where('inquiries.status = ?', 7).map { |ip| ip.final_sales_quote.calculated_total if ip.final_sales_quote.present?}.compact.flatten.sum }, type: 'double'
+    # field :order_won, value: -> (record) {record.inquiries.where('inquiries.status = ?', 18).size}, type: 'integer'
+    # field :company_key, value: -> (record) { record.id }, type: 'integer'
+    # field :total_quote_value, value: -> (record) { record.inquiries.where.not('inquiries.status = ? OR inquiries.status = ?', 10,9).map { |ip| ip.final_sales_quote.calculated_total if ip.final_sales_quote.present?}.compact.flatten.sum }, type: 'double'
+    # field :total_order_value, value: -> (record) {record.inquiries.map{|i| i.final_sales_orders.map{|s| s.calculated_total}}.compact.flatten.sum}, type: 'double'
+    # field :total_margin, value: -> (record) { record.inquiries.map{|i| i.final_sales_orders.map{|s| s.calculated_total_margin}}.compact.flatten.sum }, type: 'double'
+    # field :margin_percentage, value: -> (record) { record.order_margin.sum.to_f / record.order_margin.count }, type: 'double'
+    # field :amount_invoiced, value: -> (record) { record.inquiries.map{|i| i.invoices.map{|s| s.calculated_total}}.compact.flatten.sum }, type: 'double'
+    # field :cancelled_invoiced, value: -> (record) { record.inquiries.map{|i| i.invoices.where(status: 'Cancelled').pluck(:id)}.flatten.compact.count }, type: 'integer'
+    # field :invoice_margin_percentage, value: -> (record) {record.invoice_margin.sum.to_f / record.invoice_margin.count }, type: 'double'
   end
 end
