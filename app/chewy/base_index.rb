bca2ecd613804a0d82fd53d781a2e07d5b709d57
rucha parab <rucha.parab@bulkmro.com>
2019-04-20 13:04:37 +0530

    changed number of shards from  1 to 3

--- app/chewy/base_index.rb
+++ app/chewy/base_index.rb
@@ -84,7 +84,7 @@ class BaseIndex < Chewy::Index
     },
     max_result_window: 5000000,
     'number_of_replicas': '0',
-    'number_of_shards': '1'
+    'number_of_shards': '3'
   )
 
   def self.fields
