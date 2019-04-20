8a8c1094da6eb79c2949f6164b3682548de58b92
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-04-20 13:46:07 +0530

    update po's and inward dispatches on updating logistics owner in company

--- app/controllers/overseers/accounts/companies_controller.rb
+++ app/controllers/overseers/accounts/companies_controller.rb
@@ -90,6 +90,11 @@ class Overseers::Accounts::CompaniesController < Overseers::Accounts::BaseContro
     @company.assign_attributes(company_params.merge(overseer: current_overseer))
     authorize @company
 
+    if @company.logistics_owner_id_changed?
+      debugger
+      Services::Overseers::PurchaseOrders::UpdateLogisticsOwner(@company).call
+    end
+
     options = @company.name_changed? ? { name: @company.name_change[0] } : false
 
     if @company.save_and_sync(options)
