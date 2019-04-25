d0e0c1e91a3f498621301a2e4fc4784ce284912d
Ruta kambli <ruta.kambli@bulkmro.com>
2019-04-25 13:32:51 +0530

    Ar invoice index page working

--- app/assets/javascripts/packs/views/purchaseOrders/inwardDispatchDeliveredQueue.js
+++ app/assets/javascripts/packs/views/purchaseOrders/inwardDispatchDeliveredQueue.js
@@ -5,6 +5,10 @@ const inwardDispatchDeliveredQueue = () => {
         createInvoiceRequest();
     });
 
+    $('#create_ar_invoice').unbind('click').bind('click', function() {
+        console.log('kkkkkkkkkkkkkkk')
+    })
+
 };
 
 let toggleCheckboxes = () => {
@@ -16,12 +20,12 @@ let toggleCheckboxes = () => {
 let createInvoiceRequest = () => {
     let inward_dispatches = {};
     $('input[type=checkbox][name="the_inward_dispatches[]"]:checked').each((index, element) => {
-        inward_dispatches[$(element).val()] = $(element).data("po-id");
+        inward_dispatches[$(element).val()] = $(element).data("so-id");
     });
 
     if (checkValues(inward_dispatches) == true) {
         let data = {ids: Object.keys(inward_dispatches), purchase_order_id: Object.values(inward_dispatches)[0]};
-        window.open(Routes.new_overseers_invoice_request_path(data));
+        window.open(Routes.new_overseers_ar_invoice_path(data));
     } else {
         $.notify({
             message: 'Selected Inward Dispatches Requests should be of the same Purchase Order'
@@ -38,7 +42,7 @@ let checkValues = (obj) => {
 
 let showOrHideActions = () => {
     var hide = true;
-
+    console.log('showOrHideActions')
     $('input[type=checkbox][name="the_inward_dispatches[]"]').each((index, element) => {
         if ($(element).is(':checked')) {
             hide = false;
