bbca5abab643a2040ab360fd9bfd276a4f58304d
rucha parab <rucha.parab@bulkmro.com>
2019-04-27 10:16:12 +0530

    ifsc autocomplete logic is changed by using autocomplete filter rather than completion suggester

--- app/services/services/overseers/finders/ifsc_codes.rb
+++ app/services/services/overseers/finders/ifsc_codes.rb
@@ -1,10 +1,17 @@
 class Services::Overseers::Finders::IfscCodes < Services::Overseers::Finders::BaseFinder
   def call
-    call_base
+    non_paginated_records = if query_string.present?
+                              perform_query(query_string)
+                            else
+                              all_records
+                            end
+
+    @indexed_records = non_paginated_records.page(page).per(per) if non_paginated_records.present?
+    @indexed_records = non_paginated_records if !paginate
   end
 
   def all_records
-    indexed_records = super
+    indexed_records = index_klass.all
 
     if @base_filter.present?
       indexed_records = indexed_records.filter(@base_filter)
@@ -19,7 +26,7 @@ class Services::Overseers::Finders::IfscCodes < Services::Overseers::Finders::Ba
     end
 
     if @prefix.present?
-      indexed_records = suggestion(indexed_records, @prefix)
+      indexed_records = search_as_to_type(indexed_records, @prefix)
     end
 
     indexed_records
@@ -50,7 +57,7 @@ class Services::Overseers::Finders::IfscCodes < Services::Overseers::Finders::Ba
     end
 
     if @prefix.present?
-      indexed_records = suggestion(indexed_records, @prefix)
+      indexed_records = search_as_to_type(indexed_records, @prefix)
     end
 
     indexed_records
@@ -66,6 +73,15 @@ class Services::Overseers::Finders::IfscCodes < Services::Overseers::Finders::Ba
               skip_duplicates: true
           }
       }
+    )
+    indexed_records
+  end
+
+  def search_as_to_type(indexed_records, prefix)
+    indexed_records = indexed_records.query(
+            "match": {
+                "ifsc_complete.autocomplete": prefix
+            }
     )
     indexed_records
   end
