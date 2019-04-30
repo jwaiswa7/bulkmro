5e98401bf83b5a6f1b8fbab4e7740148e3c38283
pradeep <pradeep.ketkale@bulkmro.com>
2019-04-30 19:15:45 +0530

    Removed proc opr field

--- db/migrate/20190427060113_add_procurement_operations_to_inquiries.rb
+++
@@ -1,7 +0,0 @@
-class AddProcurementOperationsToInquiries < ActiveRecord::Migration[5.2]
-  def change
-    add_column :inquiries, :procurement_operations_id, :integer, index: true
-
-    add_foreign_key :inquiries, :overseers, column: :procurement_operations_id
-  end
-end
