70bcf579923a4529210bb8a4f1b30a49c660178f
Ruta kambli <ruta.kambli@bulkmro.com>
2019-04-30 19:23:05 +0530

    comment message adding for rejection reason

--- app/controllers/overseers/ar_invoice_requests_controller.rb
+++ app/controllers/overseers/ar_invoice_requests_controller.rb
@@ -146,7 +146,8 @@ class Overseers::ArInvoiceRequestsController < Overseers::BaseController
           :other_cancellation_reason,
           :ar_invoice_number,
           :e_way,
-          rows_attributes: [ :id, :inward_dispatch_row_id, :sales_order_id, :quantity ]
-        )
+          rows_attributes: [ :id, :inward_dispatch_row_id, :sales_order_id, :quantity ],
+          comments_attributes: [:id, :message, :created_by_id, :updated_by_id],
+          )
     end
 end
