9cf15e44f60fa9ba4e82f3ef740265e527105984
rucha parab <rucha.parab@bulkmro.com>
2019-05-02 11:36:24 +0530

    summary boxes fix

--- app/assets/javascripts/packs/views/common/updateSummaryBox.js
+++ app/assets/javascripts/packs/views/common/updateSummaryBox.js
@@ -6,7 +6,7 @@ const updateSummaryBox = () => {
         let json = table.ajax.json() ? table.ajax.json() : {};
 
         $.each(json.recordsSummary, function (index, summary) {
-            let statusSize = parseInt(summary["size"].toLocaleString(undefined, { minimumFractionDigits: 0 })) || 0;
+            let statusSize = parseInt(summary["size"]) || 0;
             let statusId = ".status-" + summary["status_id"];
             $(statusId).find('.status-count-' + summary["status_id"]).html(new Intl.NumberFormat('en-US',{maximumFractionDigits: 0}).format(statusSize));
         });
