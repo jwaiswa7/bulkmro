5c904a37b4d4fcd005140f44ce8e44ef996c29b6
MeenakshiNaik <meenu688naik@gmail.com>
2019-05-02 12:17:03 +0530

    Adding inquiry status changes in InquiryStatusMapping table

--- app/models/inquiry_mapping_tat.rb
+++ app/models/inquiry_mapping_tat.rb
@@ -11,6 +11,26 @@ class InquiryMappingTat < ApplicationRecord
 
   scope :with_includes, -> { includes(:inquiry, inquiry: [:inquiry_status_records]) }
 
+  def self.save_record(inquiry, subject)
+    if subject.class.name == "SalesOrder"
+      record =  InquiryMappingTat.where(inquiry_id: inquiry.id, sales_quote_id: subject.sales_quote.id, sales_order_id: nil).first
+      if record.present?
+        record.update_attributes(sales_order_id: subject.id)
+      else
+        InquiryMappingTat.where(inquiry_id: inquiry.id, sales_order_id: subject.id, sales_quote_id: subject.sales_quote.id, inquiry_created_at: inquiry.created_at).create
+      end
+    elsif subject.class.name == "SalesQuote"
+      record =  InquiryMappingTat.where(inquiry_id: inquiry.id, sales_quote_id: nil).first
+      if record.present?
+        record.update_attributes(sales_quote_id: subject.id)
+      else
+        InquiryMappingTat.where(inquiry_id: inquiry.id, sales_quote_id: subject.id, inquiry_created_at: inquiry.created_at).create
+      end
+    else
+      InquiryMappingTat.where(inquiry_id: inquiry.id, inquiry_created_at: inquiry.created_at).first_or_create
+    end
+  end
+
   def calculate_turn_around_time(current_status)
     current_record = self.inquiry.inquiry_status_records.find_by(status: current_status)
     prev_status = current_record.previous_status_record
