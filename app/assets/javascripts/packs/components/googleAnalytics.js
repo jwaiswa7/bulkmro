2179dc8bbfcd1d27e40a964293d4274b6b1ee933
rucha parab <rucha.parab@bulkmro.com>
2019-05-06 15:05:30 +0530

    global search Google analytics event tracking added

--- app/assets/javascripts/packs/components/googleAnalytics.js
+++ app/assets/javascripts/packs/components/googleAnalytics.js
@@ -4,6 +4,17 @@ const googleAnalytics = () => {
         ga('set', 'location', event.data.url);
         return ga('send', 'pageview');
     }
+
+    $('.global-search').on('click', function(){
+        if (typeof ga === 'function') {
+            ga('send', 'event', {
+                eventCategory: 'search',
+                eventAction: 'click',
+                eventLabel: 'Global search'
+            });
+        }
+
+    })
 };
 
 export default googleAnalytics
