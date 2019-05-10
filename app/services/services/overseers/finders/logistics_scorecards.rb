d204cf463049ba82b4b02925102fa2cacfabf090
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-05-10 12:44:32 +0530

    add logistics scoreboard finder

---
+++ app/services/services/overseers/finders/logistics_scorecards.rb
@@ -0,0 +1,64 @@
+class Services::Overseers::Finders::LogisticsScorecards < Services::Overseers::Finders::BaseFinder
+  def call
+    call_base
+  end
+
+  def all_records
+    indexed_records = if current_overseer.present? && !current_overseer.allow_inquiries?
+                        super.filter(filter_by_owner(current_overseer.self_and_descendant_ids))
+                      else
+                        super
+                      end
+
+    if @status.present?
+      indexed_records = indexed_records.filter(filter_by_value(:status, @status))
+    end
+
+    if search_filters.present?
+      indexed_records = filter_query(indexed_records)
+    end
+
+    if range_filters.present?
+      indexed_records = range_query(indexed_records)
+    end
+
+    indexed_records = aggregation_kra_report(indexed_records)
+    indexed_records
+  end
+
+  def perform_query(query_string)
+    indexed_records = index_klass.query(
+        multi_match: {
+            query: query_string,
+            operator: 'and',
+            fields: %w[inside_sales_owner]
+        }
+    ).order(sort_definition)
+
+    if current_overseer.present? && !current_overseer.allow_inquiries?
+      indexed_records = indexed_records.filter(filter_by_owner(current_overseer.self_and_descendant_ids))
+    end
+
+    if @status.present?
+      indexed_records = indexed_records.filter(filter_by_value(:status, @status))
+    end
+
+    if search_filters.present?
+      indexed_records = filter_query(indexed_records)
+    end
+
+    if range_filters.present?
+      indexed_records = range_query(indexed_records)
+    end
+
+    indexed_records
+  end
+
+  def model_klass
+    SalesOrder
+  end
+
+  def index_klass
+    'LogisticsScorecardsIndex'.constantize
+  end
+end
