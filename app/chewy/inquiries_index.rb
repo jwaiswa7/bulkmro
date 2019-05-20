309580e2d1d4d3ae7d66ecbcfff1b77e825e6297
sakshiyadav95 <sakshi.yadav@bulkmro.com>
2019-05-20 15:21:09 +0530

    Lead by os' hidden form each page

--- app/chewy/inquiries_index.rb
+++ app/chewy/inquiries_index.rb
@@ -1,5 +1,5 @@
   class InquiriesIndex < BaseIndex
-    statuses = Inquiry.statuses
+    statuses = Inquiry.statuses.except("Lead by O/S")
 
     define_type Inquiry.all.with_includes do
       field :id, type: 'integer'
