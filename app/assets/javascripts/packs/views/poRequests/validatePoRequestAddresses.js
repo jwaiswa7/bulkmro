68a4c25d5c369d074ff5f9482339b49aaf00d2e3
MeenakshiNaik <meenu688naik@gmail.com>
2019-05-28 16:18:09 +0530

    Po Request - Validation removed for selecting another city when supplier PO type is Drop Ship

--- app/assets/javascripts/packs/views/poRequests/validatePoRequestAddresses.js
+++ app/assets/javascripts/packs/views/poRequests/validatePoRequestAddresses.js
@@ -16,17 +16,19 @@ const validatePoRequestAddresses = () => {
 
             var warehouseStates = $(parsleyInstance.$element[0]).data('warehouse-list').split(',');
 
-            if (selectedBillingSupplierState && selectedShippingSupplierState && !warehouseStates.includes(selectedBillingSupplierState) && !warehouseStates.includes(selectedShippingSupplierState)) {
-                return selectedWarehouseState == locations[2];
+            if (selectedBillingSupplierState && selectedShippingSupplierState && !warehouseStates.includes(selectedBillingSupplierState) && !warehouseStates.includes(selectedShippingSupplierState) && supplier_po_type != "Drop Ship") {
+                return selectedWarehouseState == parseInt(locations[2]);
             }
 
             if (supplier_po_type == "Regular" && selectedBillToCity == selectedShipToCity) {
                 return true;
             }
 
+            if ((supplier_po_type == "Route Through") && selectedWarehouse == locations[0]) {
+                return true;
+            }
 
-
-            if ((supplier_po_type == "Route Through" || supplier_po_type == "Drop Ship") && selectedWarehouse == locations[0]) {
+            if (supplier_po_type == "Drop Ship") {
                 return true;
             }
 
