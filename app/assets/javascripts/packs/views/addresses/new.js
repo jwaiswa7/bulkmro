dfc8b5218b7dd10dd580096e0a41bc6a45170468
sufiyan-bmro <sufiyan.siddique@bulkmro.com>
2019-06-18 18:03:20 +0530

    Bug fixed - during length validation all the string joins even if the length is below 15

--- app/assets/javascripts/packs/views/addresses/new.js
+++ app/assets/javascripts/packs/views/addresses/new.js
@@ -18,23 +18,31 @@ const newAction = () => {
     $(document).ready(function() {
         if ($('#address_gst').val() != ''){
             var full_gst_code = $('#address_gst').val()
+            console.log('full_gst_cd.length'+full_gst_code.length)
             var gst_cd1 = full_gst_code.substring(0, 2);
-            var gst_cd2 = full_gst_code.substring(2, 12);
-            var gst_cd3 = full_gst_code.substring(12, 13);
-            var gst_cd4 = full_gst_code.substring(13, 14);
-            var gst_cd5 = full_gst_code.substring(14, 15);
+            $('#gst_code_1').val(gst_cd1);
+            if (full_gst_code.length == 15)
+            {
+                var gst_cd2 = full_gst_code.substring(2, 12);
+                var gst_cd3 = full_gst_code.substring(12, 13);
+                var gst_cd4 = full_gst_code.substring(13, 14);
+                var gst_cd5 = full_gst_code.substring(14, 15);
 
-            $('#gst_code_1').val(gst_cd1)
-            $('#gst_code_2').val(gst_cd2)
-            $('#gst_code_3').val(gst_cd3)
-            $('#gst_code_4').val(gst_cd4)
-            $('#gst_code_5').val(gst_cd5)
+                $('#gst_code_1').val(gst_cd1)
+                $('#gst_code_2').val(gst_cd2)
+                $('#gst_code_3').val(gst_cd3)
+                $('#gst_code_4').val(gst_cd4)
+                $('#gst_code_5').val(gst_cd5)
+            }else{
+                $('#gst_code_2,#gst_code_3,#gst_code_4,#gst_code_5').val('')
+            }
         }
     })
 
 
     $('.gst-change').on('input', function() {
-        $('#address_gst').val($('#gst_code_1').val()+$('#gst_code_2').val()+$('#gst_code_3').val()+$('#gst_code_4').val()+$('#gst_code_5').val());
+        var full_gst_cd = $('#gst_code_1').val()+$('#gst_code_2').val()+$('#gst_code_3').val()+$('#gst_code_4').val()+$('#gst_code_5').val()
+        $('#address_gst').val(full_gst_cd);
     });
 }
 
