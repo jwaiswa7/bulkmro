89b43c84ebce67bd548626e8d56eac30585b4c63
pradeep <pradeep.ketkale@bulkmro.com>
2019-06-24 19:21:11 +0530

    Removed acl for notifications

--- app/controllers/overseers/notifications_controller.rb
+++ app/controllers/overseers/notifications_controller.rb
@@ -3,18 +3,18 @@
 class Overseers::NotificationsController < Overseers::BaseController
   def index
     @notifications = ApplyDatatableParams.to(Notification.where(recipient: current_overseer), params)
-    authorize_acl @notifications
+    authorize @notifications
   end
 
   def queue
     @notifications = Notification.where(recipient: current_overseer).recent
-    authorize_acl @notifications
+    authorize @notifications
   end
 
   def mark_as_read
     @notifications = Notification.where(recipient: current_overseer).unread
     @notifications.update_all(read_at: Time.zone.now)
-    authorize_acl @notifications
+    authorize @notifications
     render json: { success: true }
   end
 end
