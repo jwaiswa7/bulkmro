4af557ef3df842264ff36ee8c71b9922ccdd9945
Ruta kambli <ruta.kambli@bulkmro.com>
2019-07-08 13:16:21 +0530

    Moq set to 0 for henkel

--- app/controllers/customers/customer_products_controller.rb
+++ app/controllers/customers/customer_products_controller.rb
@@ -10,12 +10,16 @@ class Customers::CustomerProductsController < Customers::BaseController
       params[:page] = 1 unless params[:page].present?
       params[:per] = 24
     end
-    @henkel_company = Account.find(7).companies.pluck(:id)
+    account = Account.find(7)
     service = Services::Customers::Finders::CustomerProducts.new(params, current_contact, current_company)
     service.call
     @indexed_customer_products = service.indexed_records
     @customer_products = service.records.try(:reverse)
-
+    @is_henkel = (current_company.account == account)
+    @default_quantity = nil
+    if @is_henkel
+      @default_quantity = 0
+    end
 
     @tags = CustomerProduct.all.map(&:tags).flatten.uniq.collect{ |t| [t.id, t.name] }
     @checked_tags = (params['custom_filters']['tags'].nil? ? [] : params['custom_filters']['tags'].map(&:to_i)) if params['custom_filters'].present?
@@ -35,6 +39,11 @@ class Customers::CustomerProductsController < Customers::BaseController
   def show
     authorize @customer_product
     @account = Account.find(7)
+    @default_quantity = @customer_product.moq
+    @is_henkel = (current_company.account == @account)
+    if @is_henkel
+      @default_quantity = 0
+    end
   end
 
   def most_ordered_products
