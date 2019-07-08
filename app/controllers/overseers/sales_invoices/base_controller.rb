3426f825da4faed3a9f61f0494d93e0d8808b180
sufiyan-bmro <sufiyan.siddique@bulkmro.com>
2019-07-08 14:00:34 +0530

    generic email functionality on sales invoice

--- app/controllers/overseers/sales_invoices/base_controller.rb
+++ app/controllers/overseers/sales_invoices/base_controller.rb
@@ -1,8 +1,8 @@
-class Overseers::SalesInvoices::BaseController < Overseers::BaseController
-  before_action :set_sales_invoice
-
-  private
-    def set_sales_invoice
-      @sales_invoice = SalesInvoice.find(params[:sales_invoice_id])
-    end
-end
+# class Overseers::SalesInvoices::BaseController < Overseers::BaseController
+#   before_action :set_sales_invoice
+#
+#   private
+#     def set_sales_invoice
+#       @sales_invoice = SalesInvoice.find(params[:sales_invoice_id])
+#     end
+# end
--- app/mailers/sales_invoice_mailer.rb
+++ app/mailers/sales_invoice_mailer.rb
@@ -18,4 +18,22 @@ class SalesInvoiceMailer < ApplicationMailer
     email = htmlized_email(email_message)
     email.delivery_method.settings.merge!(user_name: @overseer.email, password: @overseer.smtp_password)
   end
+
+  def dispatch_mail(email_message)
+    @overseer = email_message.overseer
+    @contact = email_message.contact
+    @inquiry = email_message.inquiry
+
+    standard_email(email_message)
+  end
+
+  def send_dispatch_mail(email_message)
+    @overseer = email_message.overseer
+    @contact = email_message.contact
+    @inquiry = email_message.inquiry
+
+    attach_files(email_message.files)
+    email = htmlized_email(email_message)
+    email.delivery_method.settings.merge!(user_name: @overseer.email, password: @overseer.smtp_password)
+  end
 end
