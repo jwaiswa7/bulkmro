134f3f152152c4b0191348de2071a080ded954ae
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-07-12 10:50:52 +0530

    Added service for Bible Order creation

--- app/services/services/overseers/bible_sales_orders/create.rb
+++ app/services/services/overseers/bible_sales_orders/create.rb
@@ -5,32 +5,32 @@ class Services::Overseers::BibleSalesOrders::Create < Services::Shared::BaseServ
   def call
   service = Services::Shared::Spreadsheets::CsvImporter.new('bible_till_june.csv', 'seed_files_3')
   service.loop(nil) do |x|
+    bible_total = x.get_column('Total Selling Price').to_f
     bible_total_with_tax = x.get_column('Total Selling Price').to_f + x.get_column('Tax Amount').to_f
 
-    if bible_total_with_tax.negative?
-      bible_order = BibleSalesOrder.where(inquiry_number: x.get_column('Inquiry Number').to_i,
-                                          order_number: x.get_column('So #'),
-                                          client_order_date: x.get_column('Client Order Date'),
-                                          is_adjustment_entry: true).first_or_create! do |bible_order|
-        bible_order.inside_sales_owner = x.get_column('Inside Sales Name')
-        bible_order.company_name = x.get_column('Magento Company Name')
-        bible_order.account_name = x.get_column('Company Alias')
-        bible_order.currency = x.get_column('Price Currency')
-        bible_order.document_rate = x.get_column('Document Rate')
-        bible_order.metadata = []
-      end
+    if bible_total.negative?
+      # bible_order = BibleSalesOrder.where(inquiry_number: x.get_column('Inquiry Number').to_i,
+      #                                     order_number: x.get_column('So #'),
+      #                                     client_order_date: x.get_column('Client Order Date'),
+      #                                     is_adjustment_entry: true).first_or_create! do |bible_order|
+      #   bible_order.inside_sales_owner = x.get_column('Inside Sales Name')
+      #   bible_order.company_name = x.get_column('Magento Company Name')
+      #   bible_order.account_name = x.get_column('Company Alias')
+      #   bible_order.currency = x.get_column('Price Currency')
+      #   bible_order.document_rate = x.get_column('Document Rate')
+      #   bible_order.metadata = []
+      # end
     else
       bible_order = BibleSalesOrder.where(inquiry_number: x.get_column('Inquiry Number').to_i,
                                           order_number: x.get_column('So #'),
-                                          client_order_date: x.get_column('Client Order Date')).first_or_create! do |bible_order|
+                                          mis_date: x.get_column('Order Date')).first_or_create! do |bible_order|
         bible_order.inside_sales_owner = x.get_column('Inside Sales Name')
         bible_order.company_name = x.get_column('Magento Company Name')
         bible_order.account_name = x.get_column('Company Alias')
-        bible_order.mis_date = x.get_column('Order Date')
+        bible_order.client_order_date = x.get_column('Client Order Date')
         bible_order.currency = x.get_column('Price Currency')
         bible_order.document_rate = x.get_column('Document Rate')
         bible_order.metadata = []
-        bible_order.is_adjustment_entry = false
       end
     end
 
@@ -50,6 +50,7 @@ class Services::Overseers::BibleSalesOrders::Create < Services::Shared::BaseServ
           'margin_percentage': x.get_column('Margin (In %)'),
           'total_selling_price': x.get_column('Total Selling Price').to_f,
           'tax_rate': x.get_column('Tax Rate'),
+          'total_selling_price_with_tax': x.get_column('Total Selling Price').to_f + x.get_column('Tax Amount').to_f,
           'tax_amount': x.get_column('Tax Amount').to_f,
           'unit_selling_price': x.get_column('Unit selling Price').to_f,
           'order_date': x.get_column('Order Date')
@@ -60,18 +61,24 @@ class Services::Overseers::BibleSalesOrders::Create < Services::Shared::BaseServ
     end
   end
 
-  bible_order_total = 0
-  bible_order_tax = 0
-  bible_order_total_with_tax = 0
-
-  # BibleSalesOrder.all.each do |bible_order|
-  #   bible_order.metadata.each do |line_item|
-  #     bible_order_total = bible_order_total + line_item['total_selling_price']
-  #     bible_order_tax = bible_order_tax + line_item['tax_amount']
-  #     bible_order_total_with_tax = bible_order_total_with_tax + bible_order_total + bible_order_tax
-  #     bible_order.update_attributes(order_total: bible_order_total, order_tax: bible_order_tax, order_total_with_tax: bible_order_total_with_tax)
-  #   end
-  # end
+  calculate_totals
   puts 'BibleSO', BibleSalesOrder.count
+  end
+
+  def calculate_totals
+    BibleSalesOrder.all.each do |bible_order|
+      bible_order_total = 0
+      bible_order_tax = 0
+      bible_order_total_with_tax = 0
+      order_margin = 0
+
+      bible_order.metadata.each do |line_item|
+        bible_order_total = bible_order_total + line_item['total_selling_price']
+        bible_order_tax = bible_order_tax + line_item['tax_amount']
+        bible_order_total_with_tax = bible_order_total_with_tax + line_item['total_selling_price_with_tax']
+        bible_order_total_margin = order_margin + line_item['margin_amount']
+        bible_order.update_attributes(order_total: bible_order_total, order_tax: bible_order_tax, order_total_with_tax: bible_order_total_with_tax, total_margin: bible_order_total_margin)
+      end
+    end
   end
 end
