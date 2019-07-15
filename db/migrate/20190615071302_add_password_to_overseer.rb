cdce72d832855dc1d8666092fbd82d38aaf50ad2
MeenakshiNaik <meenu688naik@gmail.com>
2019-07-15 17:11:36 +0530

    Purchase Order Address mismatch issue fixed

--- db/migrate/20190615071302_add_password_to_overseer.rb
+++
@@ -1,6 +0,0 @@
-class AddPasswordToOverseer < ActiveRecord::Migration[5.2]
-  def change
-    add_column :overseers, :changed_by, :string
-    add_column :overseers, :changed_at, :datetime
-  end
-end
