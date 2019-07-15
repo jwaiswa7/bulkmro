5e2c38d34c3b450791e432fa211339ad5c278343
prikesh-bulkmro <prikesh.savla@bulkmro.com>
2019-07-16 00:27:23 +0530

    Temporary: commented addoverallmarginpercentage** migration, uncomment before live

--- db/migrate/20190715085625_add_overall_margin_percentgae_to_bible_order_and_invoice.rb
+++ db/migrate/20190715085625_add_overall_margin_percentgae_to_bible_order_and_invoice.rb
@@ -1,6 +1,6 @@
 class AddOverallMarginPercentgaeToBibleOrderAndInvoice < ActiveRecord::Migration[5.2]
   def change
-    add_column :bible_sales_orders, :overall_margin_percentage, :float, :default => 0
-    add_column :bible_invoices, :overall_margin_percentage, :float, :default => 0
+    # add_column :bible_sales_orders, :overall_margin_percentage, :float, :default => 0
+    # add_column :bible_invoices, :overall_margin_percentage, :float, :default => 0
   end
 end
