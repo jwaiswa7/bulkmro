abad96d93877378af87f5dc8670e1fb4f1992ef2
pradeep <pradeep.ketkale@bulkmro.com>
2019-07-17 15:10:35 +0530

    Fix sales shipment rows

--- app/services/services/shared/migrations/fix_shipment_rows.rb
+++ app/services/services/shared/migrations/fix_shipment_rows.rb
@@ -1,9 +1,10 @@
 class Services::Shared::Migrations::FixShipmentRows < Services::Shared::Migrations::Migrations
   def add_sku_rows
-    SalesShipment.all.order(id: :desc).each do |ss|
+    SalesShipment.where('created_at >= ?', '2018-10-17').order(id: :desc).each do |ss|
       if ss.metadata.present?
         item_lines =  ss.metadata['ItemLine'] if ss.metadata['ItemLine'].present?
         if item_lines.present?
+          ss.rows.delete_all
           item_lines.each do |line_item|
             puts line_item['sku']
             ss.rows.where(sku: line_item['sku']).first_or_create! do |row|
@@ -18,4 +19,3 @@ class Services::Shared::Migrations::FixShipmentRows < Services::Shared::Migratio
     end
   end
 end
-
