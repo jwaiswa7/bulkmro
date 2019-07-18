de52a4f78a7175fbb427d174744c684d68703141
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-07-18 11:21:50 +0530

    fix migration issue

--- db/migrate/20190717140630_rename_box_detail_to_dimension_packing_slips.rb
+++ db/migrate/20190717140630_rename_box_detail_to_dimension_packing_slips.rb
@@ -1,5 +1,5 @@
 class RenameBoxDetailToDimensionPackingSlips < ActiveRecord::Migration[5.2]
   def change
-    rename_column :packing_slips, :box_detail, :box_dimension
+    # rename_column :packing_slips, :box_detail, :box_dimension
   end
 end
