7729d13fa4862592fb4fba9089a0291dc870f6e0
Ruta kambli <ruta.kambli@bulkmro.com>
2019-07-18 12:57:01 +0530

    policy to autherise_acl added

--- app/controllers/overseers/addresses_controller.rb
+++ app/controllers/overseers/addresses_controller.rb
@@ -28,12 +28,12 @@ class Overseers::AddressesController < Overseers::BaseController
   def is_sez_params
     @addresses = Address.find(params[:address_id])
     render json: { is_sez: @addresses.is_sez}.to_json
-    authorize @addresses
+    authorize_acl @addresses
   end
 
 
   def get_gst_code
-    authorize :address
+    authorize_acl :address
     @address_state = AddressState.indian.find(params[:state_id])
     render json: { gst_code: @address_state.gst_code }
   end
