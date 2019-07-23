7b05445bcf607e31521db2f3f1be3de6c29c8d0c
MeenakshiNaik <meenu688naik@gmail.com>
2019-07-23 14:46:22 +0530

    TAT report query optimization code

--- app/services/services/shared/migrations/add_inquiry_mapping_tat.rb
+++ app/services/services/shared/migrations/add_inquiry_mapping_tat.rb
@@ -2,7 +2,7 @@ class Services::Shared::Migrations::AddInquiryMappingTat < Services::Shared::Mig
   def add_inquiry_mapping_tats
     start_date = Date.parse('01-01-2019')
     end_date = Date.today
-    inquiries = Inquiry.where(created_at: start_date..end_date).where.not(status: 'Expected Order').includes(:sales_orders)
+    inquiries = Inquiry.includes(:sales_orders).where(created_at: start_date..end_date).where.not(status: 'Expected Order')
     Chewy.strategy(:bypass) do
       inquiries.each do |inquiry|
         if inquiry.sales_orders.present?
@@ -24,8 +24,8 @@ class Services::Shared::Migrations::AddInquiryMappingTat < Services::Shared::Mig
   end
 
   def add_missing_inquiries_in_inquiry_mapping_tat
-    start_date = Date.parse('18-04-2019')
-    end_date = Date.parse('03-05-2019')
+    start_date = Date.parse('01-01-2019')
+    end_date = Date.today
     inquiries = Inquiry.where(created_at: start_date..end_date).where(status: ['New Inquiry', 'Acknowledgement Mail'])
     inquiries.each do |inquiry|
       if inquiry.status == 'New Inquiry'
@@ -35,6 +35,31 @@ class Services::Shared::Migrations::AddInquiryMappingTat < Services::Shared::Mig
         InquiryStatusRecord.where(status: 'Acknowledgement Mail', inquiry: inquiry, subject_type: 'Inquiry', subject_id: inquiry.id, created_at: inquiry.updated_at).first_or_create
         InquiryStatusRecord.where(status: 'New Inquiry', inquiry: inquiry, subject_type: 'Inquiry', subject_id: inquiry.id, created_at: inquiry.created_at).first_or_create
         InquiryMappingTat.where(inquiry_id: inquiry.id, sales_quote_id: nil, sales_order_id: nil, inquiry_created_at: inquiry.created_at).first_or_create
+      end
+    end
+  end
+
+  def add_new_inquiry_status
+    start_date = Date.parse('01-01-2019')
+    end_date = Date.today
+    inquiries = Inquiry.where(created_at: start_date..end_date)
+    inquiries.each do |inquiry|
+      isr = InquiryStatusRecord.where(inquiry_id: inquiry).where(status: 'New Inquiry')
+      unless isr.present?
+        InquiryStatusRecord.create(status: 'New Inquiry', inquiry_id: inquiry.id, subject_type: 'Inquiry', subject_id: inquiry.id, created_at: inquiry.created_at)
+      end
+    end
+  end
+
+  def add_parent_id_for_existing_records
+    start_date = Date.parse('01-01-2019')
+    end_date = Date.today
+    inquiries = Inquiry.where(status: ['Order Won', 'Order Lost']).where(created_at: start_date..end_date)
+
+    inquiries.each do |inquiry|
+      inquiry.inquiry_status_records.where.not(status: ['New Inquiry']).each do |isr|
+        previous_status_record = isr.fetch_previous_status_record
+        isr.update_attributes(previous_status_record_id: previous_status_record.id) if previous_status_record.present?
       end
     end
   end
