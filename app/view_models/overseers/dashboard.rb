b0b2ee5b7241881bce5cfdae213996746775b7c7
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-08-01 15:27:58 +0530

    Minor change

--- app/view_models/overseers/dashboard.rb
+++ app/view_models/overseers/dashboard.rb
@@ -4,8 +4,9 @@ class Overseers::Dashboard
   end
 
   def inquiries
+    # inquiry.quotation_followup_date.blank? &&
     inquiries_in_range = Inquiry.with_includes.where(inside_sales_owner_id: overseer.id).where('updated_at > ? OR quotation_followup_date > ?', Date.new(2018, 04, 01), Date.new(2018, 04, 01)).order(updated_at: :desc)
-    inquiries_in_range.map{ |inquiry| inquiry if inquiry.quotation_followup_date == Date.today || inquiry.updated_at.to_date + 2.day == Date.today }
+    inquiries_in_range.map{ |inquiry| inquiry if (inquiry.quotation_followup_date.present? && inquiry.quotation_followup_date == Date.today) || inquiry.updated_at.to_date + 2.day == Date.today }.compact
   end
 
   def sales_orders
