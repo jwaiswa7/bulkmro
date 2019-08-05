ab2bb8510831cfafa234a17b63e679692aca4de0
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-08-05 16:06:42 +0530

    Resolved rubocop offenses

--- app/view_models/overseers/dashboard.rb
+++ app/view_models/overseers/dashboard.rb
@@ -5,7 +5,7 @@ class Overseers::Dashboard
 
   def inquiries
     inquiries_in_range = Inquiry.with_includes.where(inside_sales_owner_id: overseer.id).where('updated_at > ? OR quotation_followup_date > ?', Date.new(2018, 04, 01), Date.new(2018, 04, 01)).order(updated_at: :desc)
-    inquiries_in_range.map{ |inquiry| inquiry if (inquiry.quotation_followup_date.present? && inquiry.quotation_followup_date == Date.today) || inquiry.updated_at.to_date + 2.day == Date.today }.compact
+    inquiries_in_range.map { |inquiry| inquiry if (inquiry.quotation_followup_date.present? && inquiry.quotation_followup_date == Date.today) || inquiry.updated_at.to_date + 2.day == Date.today }.compact
   end
 
   def sales_orders
