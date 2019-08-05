2802516460fa3b7f21e1a42daba268da0dba55dc
Lopesh <lopesh.durugkar@bulkmro.com>
2019-08-05 17:28:28 +0530

    - check po_number is present or not.

--- app/services/services/shared/migrations/purchase_order_fields_approvals.rb
+++ app/services/services/shared/migrations/purchase_order_fields_approvals.rb
@@ -5,7 +5,7 @@ class Services::Shared::Migrations::PurchaseOrderFieldsApprovals < Services::Sha
 
   def call
     purchase_orders.each do |po|
-      if po.remote_uid.present?
+      if po.remote_uid.present? && po.po_number.present?
         ::Resources::PurchaseOrder.create_approval_purchase_order_fields(po.remote_uid)
       end
     end
