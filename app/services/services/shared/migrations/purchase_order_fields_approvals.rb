5460707de304fe8823997f277c42c689faae55da
Lopesh <lopesh.durugkar@bulkmro.com>
2019-08-05 17:27:45 +0530

    - Purchase Order Approvals fields through API. - Add migration to Approved fields in SAP as well.

---
+++ app/services/services/shared/migrations/purchase_order_fields_approvals.rb
@@ -0,0 +1,15 @@
+class Services::Shared::Migrations::PurchaseOrderFieldsApprovals < Services::Shared::BaseService
+  def initialize
+    @purchase_orders = PurchaseOrder.where("created_at >= '2019-07-18'")
+  end
+
+  def call
+    purchase_orders.each do |po|
+      if po.remote_uid.present?
+        ::Resources::PurchaseOrder.create_approval_purchase_order_fields(po.remote_uid)
+      end
+    end
+  end
+
+  attr_accessor :purchase_orders
+end
