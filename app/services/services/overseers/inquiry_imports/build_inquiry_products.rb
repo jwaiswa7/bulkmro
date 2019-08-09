921462de045f3363d49c09b5c1c8d2b2de43e595
Lopesh <lopesh.durugkar@bulkmro.com>
2019-08-09 16:16:16 +0530

    - Add Category id number on index page. - Add new columns tax_code, is_service, category_id in template as well as set the value in dropdown list in manage_failed_skus.

--- app/services/services/overseers/inquiry_imports/build_inquiry_products.rb
+++ app/services/services/overseers/inquiry_imports/build_inquiry_products.rb
@@ -9,7 +9,9 @@ class Services::Overseers::InquiryImports::BuildInquiryProducts < Services::Shar
 
     excel_import.rows.reverse.each do |row|
       if row.failed?
-
+        tax_code = row.metadata['tax_code'].present? ? row.metadata['tax_code'].to_s.gsub('.0','') : nil
+        is_service = row.metadata['is_service'].present? && row.metadata['is_service'].downcase == 'yes' ? true : false
+        category = row.metadata['category_id'].present? && row.metadata['category_id'].to_i.to_s.match?(/^[1-9]*/) ? row.metadata['category_id'].to_i : nil
         if Product.find_by_sku(row['sku']).present?
           row.sku = Services::Resources::Shared::UidGenerator.product_sku(excel_import.rows.map { |r| r['sku'] })
         end
@@ -23,6 +25,9 @@ class Services::Overseers::InquiryImports::BuildInquiryProducts < Services::Shar
             sku: row.sku,
             mpn: row.metadata['mpn'],
             brand: Brand.find_by_name(row.metadata['brand']),
+            tax_code: tax_code.present? ? TaxCode.where(code: tax_code, is_service: is_service, is_active: true).last : nil,
+            category: category.present? ? Category.where(id: category, is_service: is_service, is_active: true).last : nil,
+            is_service: is_service
           ),
           quantity: row.metadata['quantity'].to_i
         )
