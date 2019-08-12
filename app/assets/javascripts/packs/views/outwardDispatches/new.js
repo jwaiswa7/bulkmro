9027d693efd521651bedb75184ddbf61507c5f72
sakshiyadav95 <sakshi.yadav@bulkmro.com>
2019-08-12 11:26:12 +0530

    js written for box dimension validation

--- app/assets/javascripts/packs/views/outwardDispatches/new.js
+++ app/assets/javascripts/packs/views/outwardDispatches/new.js
@@ -18,7 +18,7 @@ const outwardNew = () => {
         $('[name="outward_dispatch[material_delivery_date]').val('')
     })
     $('.packing_slip_wrapper').on('change',"#packing_slips input[name*='box_number'], #packing_slips input[name*='box_dimension'] ",function () {
-        console.log($("#packing_slips input[name*='box_number']"))
+        // console.log($("#packing_slips input[name*='box_number']"))
 
         var ar = $("#packing_slips input[name*='box_number']").map(function() {
             if ($(this).val() != '') return $(this).val()
@@ -39,27 +39,12 @@ const outwardNew = () => {
             $(".submit-form").attr("disabled", true);
 
         }
-        $("#packing_slips input[name*='box_dimension'] ").val();
-        var isValid = false;
-        var regex = /^[0-9-+()]*$/;
-        isValid = regex.test($("input[name*='box_dimension']").val());
-        alert(isValid)
-        $(".error").css("display", !isValid ? "block" : "none");
-        return isValid;
+        var VAL = $("#packing_slips input[name*='box_dimension'] ").val();
+        var email = new RegExp('^[\\d ()*]+$');
+        email.test(VAL) ? $('.box_dimension_error').text('') : $('.box_dimension_error').text('Enter box dimension in the format of 20*30*455.') ;
 
     })
-        // var VAL = $("#packing_slips input[name*='box_dimension'] ").val();
-        // alert($("#packing_slips input[name*='box_dimension'] ").val())
-        // var email = new RegExp('^[0-9-!@#$%*?]');
-        // if (email.test(VAL)) {
-        //     // $('.error').text('Please check you have entered duplicate box number.')
-        //     // (email.test(VAL) != undefined) ? $('.cc').text('pppp.'): $('.cc').text('');
-        //     alert('write')
-        // }
-        // else {
-        //     // $('.error').text('');
-        //     alert('wrong')
-        // }
+
 
 }
 export default outwardNew
