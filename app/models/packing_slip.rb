e78c28f1ee49fa0c120df7a39c3f5eff49c5d011
sakshiyadav95 <sakshi.yadav@bulkmro.com>
2019-08-22 18:39:03 +0530

    packingslip updat eflow changed and issue fixed

--- app/models/packing_slip.rb
+++ app/models/packing_slip.rb
@@ -3,11 +3,19 @@ class PackingSlip < ApplicationRecord
   has_many :rows, class_name: 'PackingSlipRow', inverse_of: :packing_slip, dependent: :destroy
   include Mixins::CanBeStamped
   accepts_nested_attributes_for :rows, reject_if: lambda { |attributes| attributes['ar_invoice_request_row_id'].blank? && attributes['id'].blank? }, allow_destroy: true
+  validate :box_number_uniq?
 
 
 
   def dispatched_quantity
     self.rows.sum(:delivery_quantity)
+  end
+
+  def box_number_uniq?
+    box_uniq = PackingSlip.where(outward_dispatch_id: self.outward_dispatch_id).pluck(:box_number)
+    if box_uniq.include?(box_number)
+      errors.add(:box_number, "#{box_number} already exists in this outward dispatch.")
+    end
   end
 
   def filename(include_extension: false)
