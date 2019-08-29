8815b721efe8c2d0ff613b51f0cd3323bee6e223
MeenakshiNaik <meenu688naik@gmail.com>
2019-08-29 13:19:30 +0530

    TAT report - hound fixes

--- app/services/services/overseers/finders/tat_reports.rb
+++ app/services/services/overseers/finders/tat_reports.rb
@@ -32,14 +32,11 @@ class Services::Overseers::Finders::TatReports < Services::Overseers::Finders::B
   def perform_query(query)
     query = query[0, 35]
 
-    indexed_records = index_klass.query(
-                        multi_match: {
-                            query: query,
-                            operator: 'and',
-                            fields: %w[inquiry_number_string inside_sales_owner],
-                            minimum_should_match: '100%'
-                        }
-                      ).order(sort_definition)
+    indexed_records = index_klass.query(multi_match: {
+          query: query,
+          operator: 'and',
+          fields: %w[inquiry_number_string inside_sales_owner],
+          minimum_should_match: '100%'}).order(sort_definition)
 
     if @base_filter.present?
       indexed_records = indexed_records.filter(@base_filter)
