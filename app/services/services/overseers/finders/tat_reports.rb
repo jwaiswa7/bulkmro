97decd755e600f60f92e4e0fe9873e3b6ca6e34a
MeenakshiNaik <meenu688naik@gmail.com>
2019-08-29 12:40:15 +0530

    TAT report - indentation + hound fixed

--- app/services/services/overseers/finders/tat_reports.rb
+++ app/services/services/overseers/finders/tat_reports.rb
@@ -9,10 +9,10 @@ class Services::Overseers::Finders::TatReports < Services::Overseers::Finders::B
 
   def all_records
     indexed_records = if current_overseer.present? && !current_overseer.allow_inquiries?
-      index_klass.limit(model_klass.count).order(sort_definition).filter(filter_by_owner(current_overseer.self_and_descendant_ids))
-    else
-      index_klass.limit(model_klass.count).order(sort_definition)
-    end
+                        index_klass.limit(model_klass.count).order(sort_definition).filter(filter_by_owner(current_overseer.self_and_descendant_ids))
+                      else
+                        index_klass.limit(model_klass.count).order(sort_definition)
+                      end
     if search_filters.present?
       indexed_records = filter_query(indexed_records)
     end
@@ -33,12 +33,12 @@ class Services::Overseers::Finders::TatReports < Services::Overseers::Finders::B
     query = query[0, 35]
 
     indexed_records = index_klass.query(
-      multi_match: {
-          query: query,
-          operator: 'and',
-          fields: %w[inquiry_number_string inside_sales_owner],
-          minimum_should_match: '100%'
-      }
+        multi_match: {
+            query: query,
+            operator: 'and',
+            fields: %w[inquiry_number_string inside_sales_owner],
+            minimum_should_match: '100%'
+        }
     ).order(sort_definition)
 
     if @base_filter.present?
@@ -62,89 +62,89 @@ class Services::Overseers::Finders::TatReports < Services::Overseers::Finders::B
       dates = @tat_report_params['date_range'].split('~')
       from = ((dates[0].strip).to_time).strftime('%Y-%m-%d')
       to = ((dates[1].strip).to_time).strftime('%Y-%m-%d')
-      date_range = { from: from, to: to, key: 'custom-range' }
+      date_range = {from: from, to: to, key: 'custom-range'}
     else
-      date_range = { from: '2019-01-01', to: Date.today.strftime('%Y-%m-%d'), key: 'custom-range' }
+      date_range = {from: '2019-01-01', to: Date.today.strftime('%Y-%m-%d'), key: 'custom-range'}
     end
     indexed_records = indexed_records.aggregations(
-      'tat_by_sales_owner': {
-          date_range: {
-              field: 'created_at',
-              format: 'yyyy-MM-dd',
-              ranges: [
-                  date_range
-              ],
-              keyed: true
-          },
+        'tat_by_sales_owner': {
+            date_range: {
+                field: 'created_at',
+                format: 'yyyy-MM-dd',
+                ranges: [
+                    date_range
+                ],
+                keyed: true
+            },
 
-          aggs: {
-              'inquiry_mapping_tats': {
-                  'terms': {'field': 'inside_sales_owner_id', size: 500},
-                  aggs: {
-                      acknowledgment_mail: {
-                          sum: {
-                              field: 'acknowledgment_mail_mins'
-                          }
-                      },
-                      cross_reference: {
-                          sum: {
-                              field: 'cross_reference_mins'
-                          }
-                      },
-                      preparing_quotation: {
-                          sum: {
-                              field: 'preparing_quotation_mins'
-                          }
-                      },
-                      quotation_sent: {
-                          sum: {
-                              field: 'quotation_sent_mins'
-                          }
-                      },
-                      draft_so_appr_by_sales_manager: {
-                          sum: {
-                              field: 'draft_so_appr_by_sales_manager_mins'
-                          }
-                      },
-                      so_reject_by_sales_manager: {
-                          sum: {
-                              field: 'so_reject_by_sales_manager_mins'
-                          }
-                      },
-                      so_draft_pending_acct_approval: {
-                          sum: {
-                              field: 'so_draft_pending_acct_approval_mins'
-                          }
-                      },
-                      rejected_by_accounts: {
-                          sum: {
-                              field: 'rejected_by_accounts_mins'
-                          }
-                      },
-                      hold_by_accounts: {
-                          sum: {
-                              field: 'hold_by_accounts_mins'
-                          }
-                      },
-                      order_won: {
-                          sum: {
-                              field: 'order_won_mins'
-                          }
-                      },
-                      order_lost: {
-                          sum: {
-                              field: 'order_lost_mins'
-                          }
-                      },
-                      regret: {
-                          sum: {
-                              field: 'regret_mins'
-                          }
-                      }
-                  }
-              }
-          }
-      }
+            aggs: {
+                'inquiry_mapping_tats': {
+                    'terms': {'field': 'inside_sales_owner_id', size: 500},
+                    aggs: {
+                        acknowledgment_mail: {
+                            sum: {
+                                field: 'acknowledgment_mail_mins'
+                            }
+                        },
+                        cross_reference: {
+                            sum: {
+                                field: 'cross_reference_mins'
+                            }
+                        },
+                        preparing_quotation: {
+                            sum: {
+                                field: 'preparing_quotation_mins'
+                            }
+                        },
+                        quotation_sent: {
+                            sum: {
+                                field: 'quotation_sent_mins'
+                            }
+                        },
+                        draft_so_appr_by_sales_manager: {
+                            sum: {
+                                field: 'draft_so_appr_by_sales_manager_mins'
+                            }
+                        },
+                        so_reject_by_sales_manager: {
+                            sum: {
+                                field: 'so_reject_by_sales_manager_mins'
+                            }
+                        },
+                        so_draft_pending_acct_approval: {
+                            sum: {
+                                field: 'so_draft_pending_acct_approval_mins'
+                            }
+                        },
+                        rejected_by_accounts: {
+                            sum: {
+                                field: 'rejected_by_accounts_mins'
+                            }
+                        },
+                        hold_by_accounts: {
+                            sum: {
+                                field: 'hold_by_accounts_mins'
+                            }
+                        },
+                        order_won: {
+                            sum: {
+                                field: 'order_won_mins'
+                            }
+                        },
+                        order_lost: {
+                            sum: {
+                                field: 'order_lost_mins'
+                            }
+                        },
+                        regret: {
+                            sum: {
+                                field: 'regret_mins'
+                            }
+                        }
+                    }
+                }
+            }
+        }
     )
     indexed_records
   end
