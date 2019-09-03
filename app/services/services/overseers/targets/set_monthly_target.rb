93b89a8fad8656f27d25f002e1148d5a5bedd03b
MeenakshiNaik <meenu688naik@gmail.com>
2019-09-03 17:01:12 +0530

    KRA report - current month issue

--- app/services/services/overseers/targets/set_monthly_target.rb
+++ app/services/services/overseers/targets/set_monthly_target.rb
@@ -4,9 +4,9 @@ class Services::Overseers::Targets::SetMonthlyTarget < Services::Shared::BaseSer
   def set_overseer_monthly_target
     overseer_ids = AnnualTarget.pluck(:overseer_id).uniq
     overseers = Overseer.where(id: overseer_ids)
-    last_month = Date.today.at_beginning_of_month - 1.month
+    current_month = Date.today.at_beginning_of_month
     last_target_period = TargetPeriod.order(period_month: :asc).last.period_month
-    month_diff = (last_month.month - last_target_period.month)
+    month_diff = (current_month.month - last_target_period.month)
     month_diff.times.each do
       target_period = TargetPeriod.where(period_month: last_target_period + 1.month).first_or_create
       last_target_period = target_period.period_month
