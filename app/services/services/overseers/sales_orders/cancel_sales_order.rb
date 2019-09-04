9abe5cab3603c8aa672923a779d92649579a04a0
Lopesh <lopesh.durugkar@bulkmro.com>
2019-09-04 11:18:57 +0530

    - Fix the issue when remote_uid not present in sales_order.

--- app/services/services/overseers/sales_orders/cancel_sales_order.rb
+++ app/services/services/overseers/sales_orders/cancel_sales_order.rb
@@ -9,21 +9,24 @@ class Services::Overseers::SalesOrders::CancelSalesOrder < Services::Shared::Bas
       if sales_order.remote_uid.present?
         remote_status = ::Resources::Order.cancel_document(sales_order)
         if remote_status.key?('status') && remote_status['status'] == 'success'
-          sales_order.assign_attributes(sales_order_params)
-          sales_order.save
-          @status = { status: 'success', message: 'Sales Order Cancelled Successfully' }
+          @status = sales_order_cancel(sales_order, sales_order_params)
         elsif remote_status.key?('status') && remote_status['status'] == 'failed'
           @status = { status: 'failed', message: remote_status['message'] }
         end
       elsif sales_order.remote_uid.blank?
-        sales_order.assign_attributes(sales_order_params)
-        sales_order.save
-        @status = { status: 'success', message: 'Sales Order Cancelled Successfully' }
+        @status = sales_order_cancel(sales_order, sales_order_params)
       end
     else
       @status = { empty_message: 'true', message: 'Comment message is required' }
     end
     @status
+  end
+
+
+  def sales_order_cancel(sales_order, sales_order_params)
+    sales_order.assign_attributes(sales_order_params)
+    sales_order.save
+    return { status: 'success', message: 'Sales Order Cancelled Successfully' }
   end
 
   attr_accessor :sales_order, :sales_order_params
