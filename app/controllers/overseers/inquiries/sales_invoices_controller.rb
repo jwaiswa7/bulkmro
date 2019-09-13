d055aa3fa6b00eef8e3bff4c91fab0aa638ec251
sakshiyadav95 <sakshi.yadav@bulkmro.com>
2019-09-13 11:14:35 +0530

    sales invoice pdf issue.

--- app/controllers/overseers/inquiries/sales_invoices_controller.rb
+++ app/controllers/overseers/inquiries/sales_invoices_controller.rb
@@ -10,7 +10,7 @@ class Overseers::Inquiries::SalesInvoicesController < Overseers::Inquiries::Base
   def show
     authorize_acl @sales_invoice
 
-    @bill_from_warehouse = get_bill_from_warehouse(@sales_invoice)
+    @bill_from_warehouse = SalesInvoice.get_bill_from_warehouse(@sales_invoice)
     respond_to do |format|
       format.html { render 'show' }
       format.pdf do
@@ -22,7 +22,7 @@ class Overseers::Inquiries::SalesInvoicesController < Overseers::Inquiries::Base
   def duplicate
     authorize_acl @sales_invoice, 'show'
     @metadata = @sales_invoice.metadata.deep_symbolize_keys
-    @bill_from_warehouse = get_bill_from_warehouse(@sales_invoice)
+    @bill_from_warehouse = SalesInvoice.get_bill_from_warehouse(@sales_invoice)
     locals.merge!(duplicate: true)
     respond_to do |format|
       format.html { }
@@ -35,7 +35,8 @@ class Overseers::Inquiries::SalesInvoicesController < Overseers::Inquiries::Base
   def triplicate
     authorize_acl @sales_invoice, 'show'
     @metadata = @sales_invoice.metadata.deep_symbolize_keys
-    @bill_from_warehouse = get_bill_from_warehouse(@sales_invoice)
+    # @bill_from_warehouse = get_bill_from_warehouse(@sales_invoice)
+     @bill_from_warehouse = SalesInvoice.get_bill_from_warehouse(@sales_invoice)
     locals.merge!(triplicate: true)
     respond_to do |format|
       format.html { }
@@ -48,7 +49,7 @@ class Overseers::Inquiries::SalesInvoicesController < Overseers::Inquiries::Base
   def make_zip
     authorize_acl @sales_invoice
 
-    service = Services::Overseers::SalesInvoices::Zipped.new(@sales_invoice, locals.merge(bill_from_warehouse: get_bill_from_warehouse(@sales_invoice)))
+    service = Services::Overseers::SalesInvoices::Zipped.new(@sales_invoice, locals.merge(bill_from_warehouse: @bill_from_warehouse = SalesInvoice.get_bill_from_warehouse(@sales_invoice)))
     zip = service.call
 
     send_data(zip, type: 'application/zip', filename: @sales_invoice.zipped_filename(include_extension: true))
@@ -97,17 +98,17 @@ class Overseers::Inquiries::SalesInvoicesController < Overseers::Inquiries::Base
       end
     end
 
-    def get_bill_from_warehouse(sales_invoice)
-      metadata = sales_invoice.metadata.deep_symbolize_keys
-      if metadata[:bill_from].present?
-        bill_from_warehouse = Warehouse.find_by_remote_uid(metadata[:bill_from])
-      else
-        inquiry = sales_invoice.inquiry
-        bill_from_warehouse = inquiry.bill_from
-      end
-
-      bill_from_warehouse
-    end
+    # def get_bill_from_warehouse(sales_invoice)
+    #   metadata = sales_invoice.metadata.deep_symbolize_keys
+    #   if metadata[:bill_from].present?
+    #     bill_from_warehouse = Warehouse.find_by_remote_uid(metadata[:bill_from])
+    #   else
+    #     inquiry = sales_invoice.inquiry
+    #     bill_from_warehouse = inquiry.bill_from
+    #   end
+    #
+    #   bill_from_warehouse
+    # end
 
     def set_invoice_items
       Resources::SalesInvoice.set_multiple_items([@sales_invoice.invoice_number])
--- app/controllers/overseers/sales_invoices_controller.rb
+++ app/controllers/overseers/sales_invoices_controller.rb
@@ -130,6 +130,8 @@ class Overseers::SalesInvoicesController < Overseers::BaseController
 
   def dispatch_mail_to_customer
     @outward_dispatch = @invoice.ar_invoice_request.outward_dispatches.last if @invoice.ar_invoice_request.present?
+    @bill_from_warehouse = SalesInvoice.get_bill_from_warehouse(@invoice)
+    # @invoice.metadata['bill_from'] = @bill_from_warehouse
     @email_message = @invoice.email_messages.build(overseer: current_overseer, contact: @invoice.inquiry.contact, inquiry: @invoice.inquiry, sales_invoice: @invoice, outward_dispatch: @outward_dispatch)
     subject = "Ref# #{@invoice.inquiry.inquiry_number}- Your Order #{@invoice.inquiry.customer_po_number} - Dispatch Notification"
     @action = 'dispatch_mail_to_customer_notification'
