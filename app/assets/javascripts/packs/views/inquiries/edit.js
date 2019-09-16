e4c2d379952fca4708c5cdf2aa697e043bd7cbc9
asmitpatil <ashmit@aspiringwebsolutions.com>
2019-09-16 20:33:26 +0530

    test
    test

--- app/assets/javascripts/packs/views/inquiries/edit.js
+++ app/assets/javascripts/packs/views/inquiries/edit.js
@@ -56,41 +56,33 @@ function slideTo(page) {
     $(this).removeClass('bmro-active-li');
 });
 
-$(function() {
-  var top = $('.bmro-card-header').offset().top - parseFloat($('.bmro-card-header').css('marginTop').replace(/auto/, 0));
-  var footTop = $('.bmro-product-bottom').offset().top - parseFloat($('.bmro-product-bottom').css('marginTop').replace(/auto/, 0));
+// $(function() {
+//   var top = $('.bmro-card-header').offset().top - parseFloat($('.bmro-card-header').css('marginTop').replace(/auto/, 0));
+//   var footTop = $('.bmro-product-bottom').offset().top - parseFloat($('.bmro-product-bottom').css('marginTop').replace(/auto/, 0));
 
-  var maxY = footTop - $('.bmro-card-header').outerHeight();
+//   var maxY = footTop - $('.bmro-card-header').outerHeight();
 
-  $(window).scroll(function(evt) {
-      var y = $(this).scrollTop();
-      if (y > top) {
+//   $(window).scroll(function(evt) {
+//       var y = $(this).scrollTop();
+//       if (y > top) {
   
-          if (y < maxY) {
-              $('.bmro-card-header').addClass('fixed').removeAttr('style');
-          } else {
+//           if (y < maxY) {
+//               $('.bmro-card-header').addClass('fixed').removeAttr('style');
+//           } else {
               
-              $('.bmro-card-header').removeClass('fixed').css({
-                  position: 'relative',
-                  // top: (maxY - top) + 'px'
-                  top:'1940px'
-              });
-          }
-      } else {
-          $('.bmro-card-header').removeClass('fixed');
-      }
-  });
-});      
+//               $('.bmro-card-header').removeClass('fixed').css({
+//                   position: 'relative',
+//                   // top: (maxY - top) + 'px'
+//                   top:'1940px'
+//               });
+//           }
+//       } else {
+//           $('.bmro-card-header').removeClass('fixed');
+//       }
+//   });
+// });      
 
-// function truncateText(selector, maxLength) {
-//     var element = document.querySelector(selector),
-//         truncated = element.innerText;
+// 
 
-//     if (truncated.length > maxLength) {
-//         truncated = truncated.substr(0,maxLength) + '...';
-//     }
-//     return truncated;
-// }
-// document.querySelector('bmro-product-input').innerText = truncateText('bmro-product-input', 107);
 
 export default edit
