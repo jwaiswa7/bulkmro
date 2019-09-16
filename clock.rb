aef3322ccab75b1b5f0616e5838beca051194d04
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-09-16 17:02:15 +0530

    Adjusted time for crons in clock

--- clock.rb
+++ clock.rb
@@ -9,19 +9,9 @@ handler do |job, time|
   puts "Running #{job}, at #{time}"
 end
 
-every(1.day, 'log_currency_rates', at: '06:00') do
-  service = Services::Overseers::Currencies::LogCurrencyRates.new
-  service.call
-end
-
-every(1.day, 'refresh_calculated_totals', at: '03:00') do
-  service = Services::Overseers::Inquiries::RefreshCalculatedTotals.new
-  service.call
-end
-
-every(20.minutes, 'refresh_smart_queue') do
-  Chewy.strategy(:atomic) do
-    service = Services::Overseers::Inquiries::RefreshSmartQueue.new
+every(10.minutes, 'resync_remote_requests') do
+  ResyncRemoteRequest.where('hits < 5').each do | resync_request |
+    service = Services::Resources::Shared::ResyncFailedRequests.new(resync_request)
     service.call
   end
 end
@@ -31,80 +21,9 @@ every(30.minutes, 'update_admin_dashboard_cache') do
   service.call
 end
 
-every(1.day, 'flush_unavailable_images', at: '03:30') do
+every(35.minutes, 'refresh_smart_queue') do
   Chewy.strategy(:atomic) do
-    service = Services::Customers::CustomerProducts::FlushUnavailableImages.new
-    service.call
-  end
-end
-
-every(1.hour, 'generate_exports_hourly') do
-  Chewy.strategy(:atomic) do
-    Services::Overseers::Exporters::GenerateExportsHourly.new
-  end
-end
-
-every(1.day, 'generate_exports_daily', at: '05:00') do
-  Chewy.strategy(:atomic) do
-    Services::Overseers::Exporters::GenerateExportsDaily.new
-  end
-end
-
-every(1.day, 'refresh_indices', at: '06:00') do
-  Services::Shared::Chewy::RefreshIndices.new
-end
-
-every(1.hour, 'adjust_dynos') do
-  Services::Shared::Heroku::DynoAdjuster.new
-end if Rails.env.production?
-
-every(1.day, 'set_slack_ids', at: '07:00') do
-  Chewy.strategy(:atomic) do
-    service = Services::Overseers::Slack::SetSlackIds.new
-    service.call
-  end
-end
-
-every(1.day, 'gcloud_run_backups', at: '23:00') do
-  service = Services::Shared::Gcloud::RunBackups.new
-  service.call
-end if Rails.env.production?
-
-every(1.day, 'gcloud_run_backups_alt', at: '23:30') do
-  service = Services::Shared::Gcloud::RunBackups.new(send_chat_message: false)
-  service.call
-end if Rails.env.production?
-
-# every(1.month, 'product_inventory_update', :at => '05:00') do
-#   service = Services::Resources::Products::UpdateInventory.new
-#   service.call
-# end if Rails.env.production?
-
-every(1.day, 'inquiry_product_inventory_update', at: '02:00') do
-  service = Services::Resources::Products::UpdateRecentInquiryProductInventory.new
-  service.call
-end if Rails.env.production?
-
-=begin
-every(1.day, 'remote_unwanted_requests', at: '22:00') do
-  service = Services::Overseers::RequestCronJobs::RemoveRequestCronJob.new
-  service.call
-end if Rails.env.production?
-=end
-
-every(1.day, 'resync_failed_requests', at: '03:00') do
-  service = Services::Overseers::FailedRemoteRequests::Resync.new
-  service.call
-end if Rails.env.production?
-
-every(1.day, 'resync_requests_status', at: '06:00') do
-  service = Services::Overseers::FailedRemoteRequests::Resync.new
-  service.verify
-end if Rails.env.production?
-
-every(10.minutes, 'resync_remote_requests') do
-  ResyncRemoteRequest.where('hits < 5').each do | resync_request |
-    service = Services::Resources::Shared::ResyncFailedRequests.new(resync_request)
+    service = Services::Overseers::Inquiries::RefreshSmartQueue.new
     service.call
   end
 end
@@ -119,8 +38,89 @@ every(50.minutes, 'bible_upload') do
   end
 end
 
+every(1.hour, 'adjust_dynos') do
+  Services::Shared::Heroku::DynoAdjuster.new
+end if Rails.env.production?
+
+every(4.hour, 'generate_exports_hourly') do
+  Chewy.strategy(:atomic) do
+    Services::Overseers::Exporters::GenerateExportsHourly.new
+  end
+end
+
+every(1.day, 'refresh_indices', at: '00:00') do
+  Services::Shared::Chewy::RefreshIndices.new
+end
+
+every(1.day, 'generate_exports_daily', at: '03:00') do
+  Chewy.strategy(:atomic) do
+    Services::Overseers::Exporters::GenerateExportsDaily.new
+  end
+end
+
+every(1.day, 'refresh_calculated_totals', at: '06:00') do
+  service = Services::Overseers::Inquiries::RefreshCalculatedTotals.new
+  service.call
+end
+
+every(1.day, 'flush_unavailable_images', at: '06:30') do
+  Chewy.strategy(:atomic) do
+    service = Services::Customers::CustomerProducts::FlushUnavailableImages.new
+    service.call
+  end
+end
+
+every(1.day, 'resync_failed_requests', at: '07:00') do
+  service = Services::Overseers::FailedRemoteRequests::Resync.new
+  service.call
+end if Rails.env.production?
+
+every(1.day, 'inquiry_product_inventory_update', at: '07:30') do
+  service = Services::Resources::Products::UpdateRecentInquiryProductInventory.new
+  service.call
+end if Rails.env.production?
+
+every(1.day, 'resync_requests_status', at: '08:30') do
+  service = Services::Overseers::FailedRemoteRequests::Resync.new
+  service.verify
+end if Rails.env.production?
+
+every(1.day, 'log_currency_rates', at: '09:00') do
+  service = Services::Overseers::Currencies::LogCurrencyRates.new
+  service.call
+end
+
+every(1.day, 'set_slack_ids', at: '10:00') do
+  Chewy.strategy(:atomic) do
+    service = Services::Overseers::Slack::SetSlackIds.new
+    service.call
+  end
+end
+
+every(1.day, 'gcloud_run_backups', at: '22:00') do
+  service = Services::Shared::Gcloud::RunBackups.new
+  service.call
+end if Rails.env.production?
+
+every(1.day, 'gcloud_run_backups_alt', at: '22:00') do
+  service = Services::Shared::Gcloud::RunBackups.new(send_chat_message: false)
+  service.call
+end if Rails.env.production?
+
 every(1.day, 'set_overseer_monthly_target', if: lambda { |t| t.day == 1 }) do
   puts 'For setting Monthly Targets'
   service = Services::Overseers::Targets::SetMonthlyTarget.new
   service.set_overseer_monthly_target
 end
+
+# every(1.month, 'product_inventory_update', :at => '05:00') do
+#   service = Services::Resources::Products::UpdateInventory.new
+#   service.call
+# end if Rails.env.production?
+
+=begin
+every(1.day, 'remote_unwanted_requests', at: '22:00') do
+  service = Services::Overseers::RequestCronJobs::RemoveRequestCronJob.new
+  service.call
+end if Rails.env.production?
+=end
