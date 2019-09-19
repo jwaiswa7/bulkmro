ec8254e051f51038700f67b329baa5e26ec6a36a
asmitpatil <ashmit@aspiringwebsolutions.com>
2019-09-19 16:48:13 +0530

    test
    test

--- app/assets/javascripts/packs/views/inquiries/edit.js
+++ app/assets/javascripts/packs/views/inquiries/edit.js
@@ -74,7 +74,7 @@ let onProductChange = (container) => {
 
 function slideTo(page) {
     let element = document.getElementById(page);
-    element.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    element.scrollIntoView({behavior: "smooth", block: "start"});
 }
 $(".bmro-li-right").click(function(){
     $('.bmro-li-right').addClass('bmro-active-li',1000);
