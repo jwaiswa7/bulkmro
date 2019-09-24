4d32c615fc8dff2c40055c892cf9dbaad10401a3
asmitpatil <ashmit@aspiringwebsolutions.com>
2019-09-24 18:25:18 +0530

    test
    test

--- app/assets/javascripts/packs/views/purchaseOrders/materialReadinessQueue.js
+++ app/assets/javascripts/packs/views/purchaseOrders/materialReadinessQueue.js
@@ -23,13 +23,16 @@ let bindSummaryBox = (classname) => {
 
 $(window).scroll(function(){
         console.log($(window).scrollTop());
+        console.log('asassa');
     if ($(window).scrollTop() >= 500) {
         $('.bmro-datatable-style').addClass('bmro-datatable-style-position');
         // $('nav div').addClass('visible-title');
+        console.log('true');
     }
     else {
         $('bmro-datatable-style').removeClass('bmro-datatable-style-position');
         // $('nav div').removeClass('visible-title');
+        console.log('false');
     }
 });
 
