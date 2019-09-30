cfd29d983f1dcbce19efe609b3b5d771ded5d221
SOURABH RAJE <sourabh.raje@bulkmro.com>
2019-09-30 19:08:20 +0530

    fix calculation in edit rfq and fix breadcrumbs

--- app/assets/javascripts/packs/views/rfq/edit.js
+++ app/assets/javascripts/packs/views/rfq/edit.js
@@ -1,15 +1,32 @@
 const edit = () => {
-    calculate_total_price($('#inquiry_product_supplier_final_unit_price').val(), $('#inquiry_product_supplier_quantity').val());
+    let gst = $('#inquiry_product_supplier_gst').val();
+    let basic_unit_price = $('#inquiry_product_supplier_unit_cost_price').val();
+    let final_unit_price = $('#inquiry_product_supplier_final_unit_price').val();
+    let quantity = $('#inquiry_product_supplier_quantity').val();
+
+    calculate_gst(gst, basic_unit_price);
+    calculate_total_price(final_unit_price, quantity);
+
     $("#rfq_edit :input").on('input', function(){
+        calculate_gst($('#inquiry_product_supplier_gst').val(), $('#inquiry_product_supplier_unit_cost_price').val());
         calculate_total_price($('#inquiry_product_supplier_final_unit_price').val(), $('#inquiry_product_supplier_quantity').val());
     });
 };
 
 let calculate_total_price = (final_unit_price, quantity) => {
-    if (final_unit_price != null && quantity != null) {
-        $('#inquiry_product_supplier_total_price').val(parseFloat(final_unit_price) * parseFloat(quantity))
+    if (!(!final_unit_price && !quantity)) {
+        $('#inquiry_product_supplier_total_price').val(parseFloat(final_unit_price) * parseFloat(quantity));
     }else{
-        $('#inquiry_product_supplier_total_price').val(0)
+        $('#inquiry_product_supplier_total_price').val(0);
+    }
+};
+
+let calculate_gst = (gst, basic_unit_price) => {
+    if ((gst && gst != 0) && basic_unit_price) {
+        let gst_value = parseFloat(basic_unit_price) * parseFloat(gst) / 100
+        $('#inquiry_product_supplier_final_unit_price').val(parseFloat(basic_unit_price) + parseFloat(gst_value));
+    }else if(basic_unit_price) {
+        $('#inquiry_product_supplier_final_unit_price').val(parseFloat(basic_unit_price));
     }
 };
 
