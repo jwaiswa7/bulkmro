245be073c0df020f4541c848f3998cb3cf4a8c08
MeenakshiNaik <meenu688naik@gmail.com>
2019-10-04 13:47:11 +0530

    RFQ - auto calculations on draft rfq page

--- app/assets/javascripts/packs/views/supplierRfqs/editSupplierRfqs.js
+++ app/assets/javascripts/packs/views/supplierRfqs/editSupplierRfqs.js
@@ -8,6 +8,24 @@ const editSupplierRfqs = () => {
         });
     });
 
+    $(".rfq_edit :input").on('input', function(){
+        let active_element_number = document.activeElement.id.match(/\d+/)[0];
+        let basic_unit_price = $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_unit_cost_price").val();
+        let gst = $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_gst").val();
+        let unit_freight = $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_unit_freight").val();
+        let final_unit_price = $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_final_unit_price").val();
+        let quantity = $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_quantity").val();
+        calculate_gst(gst, basic_unit_price, active_element_number);
+        calculate_freight(unit_freight, final_unit_price, active_element_number);
+        calculate_total_price($("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_final_unit_price").val(), quantity, active_element_number);
+        if (!(basic_unit_price) || basic_unit_price == "0") {
+            $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_final_unit_price").val('');
+            $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_gst").val('');
+            $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_unit_freight").val('');
+            $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_total_price").val('');
+        }
+    });
+
     rfqReview();
     updateAllInquiryProductSuppliers();
 
@@ -39,6 +57,29 @@ let updateAllInquiryProductSuppliers = () => {
             form.submit();
         });
     });
+};
+
+let calculate_gst = (gst, basic_unit_price, active_element_number) => {
+    if ((gst && gst != 0) && basic_unit_price) {
+        let gst_value = parseFloat(basic_unit_price) * parseFloat(gst) / 100;
+        $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_final_unit_price").val(parseFloat(basic_unit_price) + parseFloat(gst_value));
+    }else if(basic_unit_price) {
+        $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_final_unit_price").val(parseFloat(basic_unit_price));
+    }
+};
+
+let calculate_freight = (unit_freight, final_unit_price, active_element_number) => {
+    if (unit_freight) {
+        $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_final_unit_price").val(parseFloat(unit_freight) + parseFloat(final_unit_price));
+    }
+};
+
+let calculate_total_price = (final_unit_price, quantity, active_element_number) => {
+    if (final_unit_price != null && quantity != null) {
+        $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_total_price").val(parseFloat(final_unit_price) * parseFloat(quantity));
+    }else{
+        $("#supplier_rfq_inquiry_product_suppliers_attributes_" + active_element_number + "_total_price").val('');
+    }
 };
 
 export default editSupplierRfqs
