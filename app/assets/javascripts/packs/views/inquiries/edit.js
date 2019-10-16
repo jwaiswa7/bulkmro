263b803ff3b64cbdef42bf971ec1ac723e468850
pk5210 <prathamesh@aspiringwebsolutions.com>
2019-10-16 14:10:00 +0530

    changes in silder
    add css

--- app/assets/javascripts/packs/views/inquiries/edit.js
+++ app/assets/javascripts/packs/views/inquiries/edit.js
@@ -45,7 +45,7 @@ const edit = () => {
             $('.bmro-card-header').css({
                 position: 'relative',
                 // top: (maxY - top) + 'px'
-                top:'1360px'
+                top:'1370px'
             });
         }
     });
