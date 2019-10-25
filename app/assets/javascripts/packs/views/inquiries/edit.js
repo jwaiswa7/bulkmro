36ce6a6da769518a046e169808ee385e40b23be3
ashmitpatil <ashmit@aspiringwebsolutions.com>
2019-10-25 11:11:47 +0530

    bug fix
    add js

--- app/assets/javascripts/packs/views/inquiries/edit.js
+++ app/assets/javascripts/packs/views/inquiries/edit.js
@@ -141,8 +141,8 @@ $('.bmro-invoice-one').click(function(){
 
 // table display remove
 $(document).ready(function() {
-    $('.bmro-remove-style th,td').removeAttr("style")
-    console.log('asssa')
+    $('.bmro-remove-style th').removeAttr("style");
+    $('td').removeAttr('style');
     // $('.bmro-remove-style table tr td').removeAttr("style");
 });
 
