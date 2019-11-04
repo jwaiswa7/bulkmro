747e9e5152a9e76d7396ba52744c2d00e0d815b8
Lopesh <lopesh.durugkar@bulkmro.com>
2019-11-04 14:18:30 +0530

    - Change condition is_active.

--- app/services/services/shared/migrations/categories_activate.rb
+++ app/services/services/shared/migrations/categories_activate.rb
@@ -8,8 +8,11 @@ class Services::Shared::Migrations::CategoriesActivate < Services::Shared::Migra
       is_service = row.get_column('product_service').downcase == "product" ? false : true
       parent_category = Category.find_by_name(parent_category_name)
       if parent_category.present?
+        if parent_category.is_active == false
+          parent_category.update_attributes(is_active: true)
+        end
         if parent_category.is_service != is_service
-          parent_category.update_attributes(is_service: is_service, is_active: true)
+          parent_category.update_attributes(is_service: is_service)
         end
         middle_category = Category.where(parent_id: parent_category.id, name: middle_category_name).last
         category_activation(middle_category, is_active: true, is_service: is_service)
