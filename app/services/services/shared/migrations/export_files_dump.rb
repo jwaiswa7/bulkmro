a1aa04acad2215382c20df75d55f01f745f878b1
Lopesh <lopesh.durugkar@bulkmro.com>
2019-11-05 17:06:14 +0530

    - Rubocop Fixes.

--- app/services/services/shared/migrations/export_files_dump.rb
+++ app/services/services/shared/migrations/export_files_dump.rb
@@ -1,10 +1,10 @@
 class Services::Shared::Migrations::ExportFilesDump < Services::Shared::BaseService
   def export_dump
     export_dump = "#{Rails.root}/tmp/export_files.csv"
-    headers = ['ID','Export Name', 'Downloaded By', 'Created At']
+    headers = ['ID', 'Export Name', 'Downloaded By', 'Created At']
     csv_data = CSV.generate(write_headers: true, headers: headers) do |writer|
       Export.all.each do |export|
-        writer << [export.id, export.export_type.titlecase,export.created_by.to_s, export.created_at]
+        writer << [export.id, export.export_type.titlecase, export.created_by.to_s, export.created_at]
       end
     end
     temp_file = File.open(export_dump, 'wb')
