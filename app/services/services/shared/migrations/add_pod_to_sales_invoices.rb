8bd65a4d16f2aa440196a8b09c20b13fd4fe6caf
Lopesh <lopesh.durugkar@bulkmro.com>
2019-11-20 15:10:42 +0530

    - Maintain array for Sales Invoices.

--- app/services/services/shared/migrations/add_pod_to_sales_invoices.rb
+++ app/services/services/shared/migrations/add_pod_to_sales_invoices.rb
@@ -2,7 +2,9 @@ class Services::Shared::Migrations::AddPodToSalesInvoices < Services::Shared::Mi
   def pod_attachment_to_sales_invoices
     service = Services::Shared::Spreadsheets::CsvImporter.new('payment_received.csv', 'seed_files')
     headers = ['Branch', 'Invoice Number', 'Customer Name', 'Date']
-    csv_data = CSV.generate(write_headers: true, headers: headers) do |writer|
+    sales_invoice_array = []
+    Chewy.strategy(:bypass) do
+      csv_data = CSV.generate(write_headers: true, headers: headers) do |writer|
       service.loop(nil) do |x|
         invoice_number = x.get_column('Invoice No.')
         delivery_date = x.get_column('Date')
@@ -13,13 +15,14 @@ class Services::Shared::Migrations::AddPodToSalesInvoices < Services::Shared::Mi
           pod_row = sales_invoice.pod_rows.build(delivery_date: delivery_date)
           pod_row.attachments.attach(io: File.open("#{Rails.root}/lib/assets/pod_attachments/dummy_pod.pdf"), filename: 'dummy_pod.pdf')
           pod_row.save!
-          sales_invoice.update_attributes(is_manual_closed: true)
+          sales_invoice_array << sales_invoice.invoice_number
         else
           writer << [ branch, invoice_number, customer_name, delivery_date ]
         end
-        puts invoice_number
       end
+      SalesInvoice.where(invoice_number: sales_invoice_array).update_attributes(is_manual_closed: true)
     end
-    fetch_csv('missing_sales_invoices.csv', csv_data)
+      fetch_csv('missing_sales_invoices.csv', csv_data)
+    end
   end
 end
