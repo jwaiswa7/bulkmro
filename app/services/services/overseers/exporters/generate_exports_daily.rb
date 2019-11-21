233357e70c1e93e4d790fd9ec6fbf9ba992ae3ca
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-11-21 15:17:03 +0530

    Added export update stages in SI, PO, suppliers export

--- app/services/services/overseers/exporters/generate_exports_daily.rb
+++ app/services/services/overseers/exporters/generate_exports_daily.rb
@@ -1,11 +1,11 @@
 class Services::Overseers::Exporters::GenerateExportsDaily < Services::Shared::BaseService
   def initialize
     export_arr = [
+        'SalesInvoicesExporter',
+        'ProductsExporter',
         'SalesOrderRowsExporter',
         'SalesInvoiceRowsExporter',
-        'ProductsExporter',
         'CompaniesExporter',
-        'SalesInvoicesExporter',
         'SalesInvoicesLogisticsExporter',
         'SalesOrdersLogisticsExporter',
         'PurchaseOrdersExporter'
