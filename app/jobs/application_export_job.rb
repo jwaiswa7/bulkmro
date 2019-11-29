e79c84be7199b7c4203b18b9600365a0545fb681
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-11-29 18:12:30 +0530

    Added garbage collection for freeing the thread memory

--- app/jobs/application_export_job.rb
+++ app/jobs/application_export_job.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationExportJob < ActiveJob::Base
-  queue_as :default
+  #include Sidekiq::Worker
+  queue_as :exports
+  #sidekiq_options retry: 1, queue: 'high_priority'
 
   def perform(*args)
     service_class = ['Services', 'Overseers', 'Exporters', args[0]].join('::').constantize
     args.shift
     service = service_class.send(:new, *args[0])
     service.build_csv
+    GC.start
   end
 end
