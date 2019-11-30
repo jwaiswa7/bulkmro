5c21889315145181eb81d8c96b19ce8085f0f751
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-11-30 11:49:19 +0530

    Restricted sidekiq access to admin users only and called GC before export service call

--- app/jobs/application_export_job.rb
+++ app/jobs/application_export_job.rb
@@ -6,10 +6,10 @@ class ApplicationExportJob < ActiveJob::Base
   #sidekiq_options retry: 1, queue: 'high_priority'
 
   def perform(*args)
+    GC.start
     service_class = ['Services', 'Overseers', 'Exporters', args[0]].join('::').constantize
     args.shift
     service = service_class.send(:new, *args[0])
     service.build_csv
-    GC.start
   end
 end
