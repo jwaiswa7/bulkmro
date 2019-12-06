272ce64649a78732c41e9b93d00d90b5e71ac225
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-12-06 15:59:43 +0530

    Fixed pdf and email issues

--- app/controllers/overseers/inquiries/base_controller.rb
+++ app/controllers/overseers/inquiries/base_controller.rb
@@ -7,24 +7,24 @@ class Overseers::Inquiries::BaseController < Overseers::BaseController
       @inquiry = Inquiry.find(params[:inquiry_id])
     end
 
-    def render_pdf_for(record, locals_variables)
-      if locals_variables[:locals][:is_pages_visible]
-        footer = {
-            center: '[page] of [topage]'
-        }
-      else
-        footer = {}
-      end
+    def render_pdf_for(record, locals = {pagination: true})
+      #if locals_variables[:locals][:is_pages_visible]
+      #  footer = {
+      #      center: '[page] of [topage]'
+      #  }
+      #else
+      #  footer = {}
+      #end
       render(
         pdf: record.filename,
         template: ['shared', 'layouts', 'pdf_templates', record.class.name.pluralize.underscore, 'show'].join('/'),
         layout: 'shared/layouts/pdf_templates/show',
         page_size: 'A4',
-        footer: footer,
+        footer: locals[:pagination] ? { center: '[page] of [topage]' } : {},
         # show_as_html: true,
         locals: {
             record: record
-        }.merge(locals_variables)
+        }.merge(locals)
              )
     end
 end
