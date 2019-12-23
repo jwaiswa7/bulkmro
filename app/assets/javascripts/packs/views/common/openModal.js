519c30a08e83eb5ef1724f24e6de2b9fd6b73abe
MeenakshiNaik <meenu688naik@gmail.com>
2019-12-23 18:51:40 +0530

    Delivery date modal issue fixed from sales order index

--- app/assets/javascripts/packs/views/common/openModal.js
+++ app/assets/javascripts/packs/views/common/openModal.js
@@ -7,7 +7,7 @@ const openModal = () => {
         var json = {
             url: Routes.render_committed_date_revision_form_overseers_sales_order_path(id),
             modalId: '#revise-committed-delivery-date',
-            className: '.revise-committed-delivery-date',
+            className: '.revise-delivery-date',
             buttonClassName: '.confirm',
             this: $(this),
             title: ''
