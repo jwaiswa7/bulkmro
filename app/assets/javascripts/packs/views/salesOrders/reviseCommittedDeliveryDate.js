43c03766d13cb1b6e1786bcc0e787c310757b8d3
Saurabh Bhosale <saurabh.bhosale@bulkmro.com>
2019-12-30 15:43:17 +0530

    Minor changes in js for disabling backdate selection

--- app/assets/javascripts/packs/views/salesOrders/reviseCommittedDeliveryDate.js
+++ app/assets/javascripts/packs/views/salesOrders/reviseCommittedDeliveryDate.js
@@ -1,7 +1,7 @@
 import disableBackdateOption from "../common/disableBackdateOption";
 
 const reviseCommittedDeliveryDate = () => {
-    disableBackdateOption($('.sales_order_revised_committed_delivery_date'));
+    disableBackdateOption($('input[name*=revised_committed_delivery_date]'));
 };
 
 export default reviseCommittedDeliveryDate
