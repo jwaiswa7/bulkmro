a8833d77e884b9715dedceab48abe13ecebe7785
MeenakshiNaik <meenu688naik@gmail.com>
2020-01-14 16:17:51 +0530

    Export all dropdown button fixes + sales invoices exports added

--- app/assets/javascripts/packs/components/navigationMenu.js
+++ app/assets/javascripts/packs/components/navigationMenu.js
@@ -24,21 +24,21 @@ const navigationMenu = () => {
     $('.dropdown-toggle').click(function() {
         $('.dropdown-toggle').parent().removeClass('dropdown-show');
         $(this).parent().addClass('dropdown-show',2000);
-    })
+    });
 
     function toggleFirstLevelHeading (classname, containerName) {
 
         $("#" + classname).click(() => {
-            let toggle = $('.bmro-main-drop-col-level-one-container .bmro-select-level-one-to-see-second-and-third').hasClass('active')
-            let toggleClass = $('.bmro-second-and-third-level-headings .bmro-second-and-third-level-headings-container').hasClass('show')
-            console.log(toggle)
+            let toggle = $('.bmro-main-drop-col-level-one-container .bmro-select-level-one-to-see-second-and-third').hasClass('active');
+            let toggleClass = $('.bmro-second-and-third-level-headings .bmro-second-and-third-level-headings-container').hasClass('show');
+
             if(toggle || toggleClass) {
-                $('.bmro-main-drop-col-level-one-container .bmro-select-level-one-to-see-second-and-third').toggleClass('active', false)
-                $('.bmro-second-and-third-level-headings .bmro-second-and-third-level-headings-container').toggleClass('show', false)
+                $('.bmro-main-drop-col-level-one-container .bmro-select-level-one-to-see-second-and-third').toggleClass('active', false);
+                $('.bmro-second-and-third-level-headings .bmro-second-and-third-level-headings-container').toggleClass('show', false);
             }
-            console.log(this)
-            $("#" + classname).toggleClass('active', true)
-            $("#" + containerName).toggleClass('show', true)
+
+            $("#" + classname).toggleClass('active', true);
+            $("#" + containerName).toggleClass('show', true);
         })
 
     }
@@ -46,25 +46,25 @@ const navigationMenu = () => {
     $(".bmro-menu-click").click(function () {
         $('.bmro-dash-main-menu').toggleClass('bmro-dash-main-menu-show');
         $(this).toggleClass('bmro-menu-roted');
-    })
+    });
 
-    toggleFirstLevelHeading('nav-bpartners-heading', 'bpartners_heading_container')
+    toggleFirstLevelHeading('nav-bpartners-heading', 'bpartners_heading_container');
 
-    toggleFirstLevelHeading('nav-catalog-heading', 'catalog_heading_container')
+    toggleFirstLevelHeading('nav-catalog-heading', 'catalog_heading_container');
 
-    toggleFirstLevelHeading('nav-sales-heading', 'sales_heading_container')
+    toggleFirstLevelHeading('nav-sales-heading', 'sales_heading_container');
 
-    toggleFirstLevelHeading('nav-logistics-heading', 'logistics_heading_container')
+    toggleFirstLevelHeading('nav-logistics-heading', 'logistics_heading_container');
 
-    toggleFirstLevelHeading('nav-inventory-heading', 'inventory_heading_container')
+    toggleFirstLevelHeading('nav-inventory-heading', 'inventory_heading_container');
 
-    toggleFirstLevelHeading('nav-reports-heading', 'reports_heading_container')
+    toggleFirstLevelHeading('nav-reports-heading', 'reports_heading_container');
 
-    toggleFirstLevelHeading('nav-admin-heading', 'admin_heading_container')
+    toggleFirstLevelHeading('nav-admin-heading', 'admin_heading_container');
 
-    toggleFirstLevelHeading('nav-docs-heading', 'docs_heading_container')
+    toggleFirstLevelHeading('nav-docs-heading', 'docs_heading_container');
 
-    toggleFirstLevelHeading('nav-favourites-heading', 'favourites_heading_container')
+    toggleFirstLevelHeading('nav-favourites-heading', 'favourites_heading_container');
 
 
 
