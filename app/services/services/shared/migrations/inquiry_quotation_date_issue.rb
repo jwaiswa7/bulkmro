ecbcfcf69a9d9f2bc7fe647be20a2b0233719f4f
Lopesh <lopesh.durugkar@bulkmro.com>
2020-02-03 16:17:56 +0530

    Added Migration File for Wrong quotation date in inquiry.

---
+++ app/services/services/shared/migrations/inquiry_quotation_date_issue.rb
@@ -0,0 +1,17 @@
+class Services::Shared::Migrations::InquiryQuotationDateIssue < Services::Shared::Migrations::Migrations
+  def call
+  	inq = Inquiry.where('quotation_date < Date(created_at)')
+  	if inq.present?
+  	    inq.each_with_index do |val, index|
+  		    quotation_date = 
+  	        sales_quotes = val.sales_quotes
+  			if sales_quotes.present?
+  			    val.quotation_date = sales_quotes.last.created_at.to_date
+  			else
+  			    val.quotation_date = ""	 
+  			end
+  		    val.save(validate: false)
+  		end		
+  	end	
+  end
+end	
