f0605fdd660cddc522b90ab2c96eb9978e7b4717
sakshiyadav95 <sakshi.yadav@bulkmro.com>
2020-02-03 10:48:28 +0530

    rubocop resolved

--- app/controllers/overseers/companies_controller.rb
+++ app/controllers/overseers/companies_controller.rb
@@ -8,7 +8,6 @@ class Overseers::CompaniesController < Overseers::BaseController
     @indexed_companies = service.indexed_records
     @companies = service.records
     authorize_acl @companies
-
   end
 
   def autocomplete
