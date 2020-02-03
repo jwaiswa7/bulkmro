4670b987bf2786f7fbb3d366ebbc07f8bfa1a3d1
Lopesh <lopesh.durugkar@bulkmro.com>
2020-02-03 16:29:45 +0530

    Rubocop Fixes

--- app/services/services/shared/migrations/inquiry_quotation_date_issue.rb
+++ app/services/services/shared/migrations/inquiry_quotation_date_issue.rb
@@ -1,17 +1,16 @@
 class Services::Shared::Migrations::InquiryQuotationDateIssue < Services::Shared::Migrations::Migrations
   def call
-  	inq = Inquiry.where('quotation_date < Date(created_at)')
-  	if inq.present?
-  	    inq.each_with_index do |val, index|
-  		    quotation_date = 
-  	        sales_quotes = val.sales_quotes
-  			if sales_quotes.present?
-  			    val.quotation_date = sales_quotes.last.created_at.to_date
-  			else
-  			    val.quotation_date = ""	 
-  			end
-  		    val.save(validate: false)
-  		end		
-  	end	
+    inq = Inquiry.where('quotation_date < Date(created_at)')
+    if inq.present?
+      inq.each_with_index do |val, index|
+      sales_quotes = val.sales_quotes
+      if sales_quotes.present?
+        val.quotation_date = sales_quotes.last.created_at.to_date
+      else
+        val.quotation_date = ''
+      end
+      val.save(validate: false)
+    end
+    end
   end
-end	
+end
