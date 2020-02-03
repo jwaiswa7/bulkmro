8e2d835878f069117bee9561bb601edab54476c0
MeenakshiNaik <meenu688naik@gmail.com>
2020-02-03 19:25:33 +0530

    Supplier RFQ - index updation on rfq update

--- app/controllers/overseers/inquiries/supplier_rfqs_controller.rb
+++ app/controllers/overseers/inquiries/supplier_rfqs_controller.rb
@@ -78,6 +78,7 @@ class Overseers::Inquiries::SupplierRfqsController < Overseers::Inquiries::BaseC
         if @email_message.save
           if SupplierRfqMailer.send_request_for_quote_email(@email_message).deliver_now
             @supplier_rfq.update_attributes(email_sent_at: Time.now, status: 'Email Sent: Supplier Response Pending')
+            SupplierRfqsIndex::SupplierRfq.import([@supplier_rfq.id])
           end
         end
         flash[:notice] = 'Record updated successfully and Emails has been sent to suppliers!'
@@ -101,6 +102,7 @@ class Overseers::Inquiries::SupplierRfqsController < Overseers::Inquiries::BaseC
     if @email_message.save
       SupplierRfqMailer.send_request_for_quote_email(@email_message).deliver_now
       @supplier_rfq.update_attributes(email_sent_at: Time.now, status: 'Email Sent: Supplier Response Pending')
+      SupplierRfqsIndex::SupplierRfq.import([@supplier_rfq.id])
       redirect_to edit_supplier_rfqs_overseers_inquiry_supplier_rfqs_path(inquiry_id: @inquiry)
       # redirect_to overseers_inquiry_sales_quotes_path(@inquiry)
     else
--- app/services/services/overseers/purchase_orders/create_purchase_order.rb
+++ app/services/services/overseers/purchase_orders/create_purchase_order.rb
@@ -35,7 +35,10 @@ class Services::Overseers::PurchaseOrders::CreatePurchaseOrder < Services::Share
             updated_by_id: params[:overseer].id,
             po_request_row_id: row.id
           )
-          po_row.po_request_row.inquiry_product_supplier.supplier_rfq.update_column(:status, 'PO Issued') if po_row.po_request_row.inquiry_product_supplier.present? && po_row.po_request_row.inquiry_product_supplier.supplier_rfq.present?
+          if po_row.po_request_row.inquiry_product_supplier.present? && po_row.po_request_row.inquiry_product_supplier.supplier_rfq.present?
+            po_row.po_request_row.inquiry_product_supplier.supplier_rfq.update_column(:status, 'PO Issued')
+            SupplierRfqsIndex::SupplierRfq.import([po_row.po_request_row.inquiry_product_supplier.supplier_rfq.id])
+          end
         end
       end
       if @purchase_order.save_and_sync(po_request)
