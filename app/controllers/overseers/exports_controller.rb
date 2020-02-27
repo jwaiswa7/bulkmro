2faf42303859f1bf9c17afc17ca8083e1d476f09
sufiyan-bmro <sufiyan.siddique@bulkmro.com>
2020-02-27 18:51:58 +0530

    code refactored in export controller

--- app/controllers/overseers/exports_controller.rb
+++ app/controllers/overseers/exports_controller.rb
@@ -1,8 +1,22 @@
 class Overseers::ExportsController < Overseers::BaseController
   def index
     authorize_acl :export
-
-    @exports_all = Export.order('created_at asc').group_by(&:export_type).except(nil)
+    @export_filtered = {}
+    @exports_all = Export.where(status: ['Completed','Processing']).order('created_at asc').group_by(&:export_type).except(nil)
+    @exports_all.each do |export_type,export_val|
+      export_val_filtered = {}
+      export_val.reverse.each do |exp|
+        if !(export_val_filtered.key?(exp.status))
+          if exp.status.include?('Completed') && exp.report.attached?
+            export_val_filtered['Completed'] = exp
+          elsif exp.status.include?('Processing')
+            export_val_filtered['Processing'] = exp
+          end
+          break if (export_val_filtered.keys.include?("Completed") && export_val_filtered.keys.include?("Processing"))
+        end
+      end
+      @export_filtered[export_type] = export_val_filtered
+    end
   end
 
 
