35f161977a231385819b4467374c5fba9f0c23da
sufiyan-bmro <sufiyan.siddique@bulkmro.com>
2020-03-17 20:01:20 +0530

    clock timing set for generate_daily_rate

--- clock.rb
+++ clock.rb
@@ -127,7 +127,7 @@ end
 #   end
 # end
 
-every(1.day, 'generate_exports_daily', at: '16:25') do
+every(1.day, 'generate_exports_daily', at: '22:00') do
   # to-do check for memory leaks on heroku
   Chewy.strategy(:atomic) do
     service = Services::Overseers::Exporters::GenerateExportsDaily.new
