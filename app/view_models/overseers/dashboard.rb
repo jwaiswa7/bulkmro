414d5b559bd44cb68303f254956d65ff620f7eee
sufiyan-bmro <sufiyan.siddique@bulkmro.com>
2020-03-20 19:05:29 +0530

    initial commit for setup

--- app/view_models/overseers/dashboard.rb
+++ app/view_models/overseers/dashboard.rb
@@ -7,6 +7,14 @@ class Overseers::Dashboard
     Inquiry.with_includes.where(inside_sales_owner_id: overseer.id).where('updated_at > ? OR quotation_followup_date > ?', Date.new(2018, 04, 01), Date.new(2018, 04, 01)).where.not(status: ['Order Won', 'Order Lost', 'Regret', 'Regret Request']).order(updated_at: :desc).compact
   end
 
+  def invoice_requests
+    InvoiceRequest.where('updated_at > ?', Date.new(2018, 04, 01)).where.not(status: ['Completed AR Invoice Request', 'Cancelled AR Invoice', 'Cancelled', 'AP Invoice Request Rejected','GRPO Request Rejected','Inward Completed','Cancelled AP Invoice','Cancelled GRPO']).order(updated_at: :desc).compact
+  end
+
+  def inquiries_with_so_approval_pending
+    Inquiry.where('updated_at > ?', Date.new(2018, 04, 01)).where(status: 'SO Draft: Pending Accounts Approval').order(updated_at: :desc).compact
+  end
+
   def inquiries_with_followup
     inquiries_in_range = Inquiry.with_includes.where(inside_sales_owner_id: overseer.id).where('updated_at > ? OR quotation_followup_date > ?', Date.new(2018, 04, 01), Date.new(2018, 04, 01)).where.not(status: ['Order Won', 'Order Lost', 'Regret', 'Regret Request']).order(updated_at: :desc)
     inquiries_in_range.map {
@@ -47,14 +55,34 @@ class Overseers::Dashboard
   end
 
   def main_statuses
-    ['New Inquiry', 'Preparing Quotation', 'Quotation Sent', 'Follow Up on Quotation', 'Expected Order']
+    if self.overseer.inside_sales_executive?
+      ['New Inquiry', 'Preparing Quotation', 'Quotation Sent', 'Follow Up on Quotation', 'Expected Order']
+    elsif self.overseer.accounts?
+      ['GRPO Pending', 'Pending AP Invoice', 'Pending AR Invoice', 'SO Draft: Pending Accounts Approval']
+    end
   end
 
   def get_status_metrics(status)
+    if self.overseer.inside_sales_executive?
+      count_parameter = recent_inquiries.pluck(:status)
+      value_parameter = inquiry_calculated_total(nil,status)
+    elsif self.overseer.accounts?
+      count_parameter = invoice_requests.pluck(:status)
+      count_parameter = count_parameter.concat (inquiries_with_so_approval_pending.pluck(:status))
+      value_parameter = inquiry_calculated_total(nil,invoice_requests)
+    end
     {
-        count: recent_inquiries.pluck(:status).count(status),
-        value: recent_inquiries.map { |inquiry| inquiry.calculated_total if inquiry.status == status }.compact.sum
+        count: count_parameter.count(status),
+        value: value_parameter
     }
+  end
+
+  def inquiry_calculated_total(documents,status)
+    if documents
+      documents.map { |doc| doc.inquiry.calculated_total if doc.status == status }.compact.sum
+    else
+      recent_inquiries.map { |inquiry| inquiry.calculated_total if inquiry.status == status }.compact.sum
+    end
   end
 
   def recent_sales_orders
