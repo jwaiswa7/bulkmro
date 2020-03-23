73ff8c0cabc33c22e0ad4bbd8c9b367189d88ef5
sakshi-yadav <sakshi.yadav@bulkmro.com>
2020-03-23 17:25:26 +0530

    regret inquiry request list displayed and two and functionality of code is fixed.

--- app/controllers/overseers/inquiries_controller.rb
+++ app/controllers/overseers/inquiries_controller.rb
@@ -39,6 +39,7 @@ class Overseers::InquiriesController < Overseers::BaseController
 
   def regret_request_action
     @inquiry = Inquiry.find(params['inquiry'])
+    @inquiry = Inquiry.find(params['inquiry'])
     if params['action_name'] == 'Approve'
       if @inquiry.update_attributes(status: 'Regret')
         message = "Inquiry Regreted by #{current_overseer}"
@@ -48,12 +49,14 @@ class Overseers::InquiriesController < Overseers::BaseController
     else
       inquiry_status = "inquiry_status_#{@inquiry.inquiry_number}"
       if Rails.cache.exist?(inquiry_status)
-        @prev_inquiry = Rails.cache.read(inquiry_status)
-        message = "Inquiry Regret Request Rejected by #{current_overseer}"
-        inq_com = InquiryComment.new(inquiry_id: @inquiry.id, message: message, created_by: current_overseer, updated_by: current_overseer)
-        inq_com.save
-        @inquiry.update_attributes(status: @prev_inquiry.status)
+        Rails.cache.delete(inquiry_status)
       end
+      Rails.cache.write(inquiry_status, @inquiry)
+      @prev_inquiry = Rails.cache.read(inquiry_status)
+      message = "Inquiry Regret Request Rejected by #{current_overseer}"
+      inq_com = InquiryComment.new(inquiry_id: @inquiry.id, message: message, created_by: current_overseer, updated_by: current_overseer)
+      inq_com.save
+      @inquiry.update_attributes(status: @prev_inquiry.status)
     end
 
     redirect_to regret_inquiry_request_queue_overseers_inquiries_path
