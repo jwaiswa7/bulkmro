3cab21cfc4b1ce6a794e47d4ff2facb77d83bc4b
sufiyan-bmro <sufiyan.siddique@bulkmro.com>
2020-03-31 17:04:16 +0530

    Invoice request summary box values fix

--- app/view_models/overseers/dashboard.rb
+++ app/view_models/overseers/dashboard.rb
@@ -31,7 +31,7 @@ class Overseers::Dashboard
   def inq_for_dash
     if self.overseer.inside_sales_executive?
       Inquiry.with_includes.where(inside_sales_owner_id: overseer.id).where('updated_at > ? OR quotation_followup_date > ?', Date.new(2018, 04, 01), Date.new(2018, 04, 01)).where.not(status: ['Order Won', 'Order Lost', 'Regret', 'Rejected by Accounts']).order(updated_at: :desc)
-    elsif self.overseer.accounts?
+    elsif self.overseer.acl_role.role_name == 'Accounts'
       inq_for_account_dash
     end
   end
@@ -63,7 +63,7 @@ class Overseers::Dashboard
     if self.overseer.inside_sales_executive?
       recent_inquiry_ids = recent_inquiries.pluck(:id)
       InquiryComment.where(inquiry_id: recent_inquiry_ids).order(created_at: :desc).limit(10).group_by { |c| c.created_at.to_date }
-    elsif self.overseer.accounts?
+    elsif self.overseer.acl_role.role_name == 'Accounts'
       invoice_request_ids = invoice_requests.pluck(:id)
       InvoiceRequestComment.where(invoice_request_id: invoice_request_ids).order(created_at: :desc).limit(8).group_by { |c| c.created_at.to_date }
     end
@@ -82,14 +82,14 @@ class Overseers::Dashboard
     if self.overseer.inside_sales_executive?
       count_parameter = recent_inquiries.pluck(:status)
       value_parameter = document_calculated_total(recent_inquiries,status)
-    elsif self.overseer.accounts?
+    elsif self.overseer.acl_role.role_name == 'Accounts'
       inv_req_status_array = invoice_requests.pluck(:status)
       inq_status_array = inq_for_dash.pluck(:status)
       count_parameter = inq_status_array.concat inv_req_status_array
       if inquiry_statuses.include? status
-        value_parameter = document_calculated_total(inq_for_dash,status)
+        value_parameter = inquiries_calculated_total(inq_for_dash, status)
       else
-        value_parameter = document_calculated_total(invoice_requests,status)
+        value_parameter = get_calculated_invoice_request(invoice_requests,status)
       end
     end
     {
@@ -98,12 +98,23 @@ class Overseers::Dashboard
     }
   end
 
-  def document_calculated_total(doc,status)
-    if inquiry_statuses.include? status
-      doc.map {  |inquiry| inquiry.calculated_total if inquiry.status == status }.compact.sum
-    else
-      doc.map {  |inv_req| inv_req.inquiry.calculated_total if inv_req.status == status }.compact.sum
+  def inquiries_calculated_total(doc, status)
+    doc.map {  |inquiry| inquiry.calculated_total if inquiry.status == status }.compact.sum
+  end
+
+  def get_calculated_invoice_request(invoice_requests_arr,status)
+    total_price_arr = []
+    invoice_requests_arr.each do |invoice_req|
+      if invoice_req.status == status
+        service = Services::Overseers::InvoiceRequests::FormProductsList.new(invoice_req.inward_dispatches.ids, false)
+        products_list = service.call
+
+        products_list.each do |product|
+          total_price_arr.push(product[:purchase_order_row].unit_selling_price.to_f * product[:total_quantity].to_f)
+        end
+      end
     end
+    total_price_arr.sum
   end
 
   def inquiry_statuses
@@ -128,21 +139,6 @@ class Overseers::Dashboard
      'Order Lost',
      'Regret',
      'Regret Request']
-  end
-
-  def invoice_request_statuses
-    ['GRPO Pending',
-     'Pending AP Invoice',
-     'Pending AR Invoice',
-     'In stock',
-     'Completed AR Invoice Request',
-     'Cancelled AR Invoice',
-     'Cancelled',
-     'AP Invoice Request Rejected',
-     'GRPO Request Rejected',
-     'Inward Completed',
-     'Cancelled AP Invoice',
-     'Cancelled GRPO']
   end
 
   def recent_sales_orders
