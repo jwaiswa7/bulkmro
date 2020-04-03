703ed0d4eed176dd46cd6e57977a2c2605f25f90
MeenakshiNaik <meenu688naik@gmail.com>
2020-04-03 13:32:11 +0530

    space removed

--- app/policies/suppliers/supplier_rfq_policy.rb
+++ app/policies/suppliers/supplier_rfq_policy.rb
@@ -3,7 +3,7 @@
 class Suppliers::SupplierRfqPolicy < Suppliers::ApplicationPolicy
 
   def edit_supplier_rfqs?
-    !(record.inquiry.sales_orders.approved.map { |so| so.sales_quote.supplier_rfq_ids.include?(record.id) }.include?(true))
+    !(record.inquiry.sales_orders.approved.map{ |so| so.sales_quote.supplier_rfq_ids.include?(record.id) }.include?(true))
   end
 
   def edit_rfq?
