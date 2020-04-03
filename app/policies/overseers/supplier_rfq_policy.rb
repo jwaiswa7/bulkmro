a28115ca0224a722f4effad82f94a8c79fd8aac4
MeenakshiNaik <meenu688naik@gmail.com>
2020-04-03 15:23:37 +0530

    hound fixes

--- app/policies/overseers/supplier_rfq_policy.rb
+++ app/policies/overseers/supplier_rfq_policy.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Suppliers::SupplierRfqPolicy < Suppliers::ApplicationPolicy
-
   def edit_supplier_rfqs?
     !(record.inquiry.sales_orders.approved.map { |so| so.sales_quote.supplier_rfq_ids.include?(record.id) }.include?(true))
   end
--- app/policies/suppliers/supplier_rfq_policy.rb
+++ app/policies/suppliers/supplier_rfq_policy.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 class Suppliers::SupplierRfqPolicy < Suppliers::ApplicationPolicy
-
   def edit_supplier_rfqs?
-    !(record.inquiry.sales_orders.approved.map{ |so| so.sales_quote.supplier_rfq_ids.include?(record.id) }.include?(true))
+    !(record.inquiry.sales_orders.approved.map { |so| so.sales_quote.supplier_rfq_ids.include?(record.id) }.include?(true))
   end
 
   def edit_rfq?
