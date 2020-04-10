4b8e7c067f55590147e4fb3d39f2966245bc9229
sufiyan-bmro <sufiyan.siddique@bulkmro.com>
2020-04-10 13:31:16 +0530

    temporary push

--- app/assets/javascripts/packs/views/common/statusBox.js
+++ app/assets/javascripts/packs/views/common/statusBox.js
@@ -21,6 +21,27 @@ const statusBox = function () {
             },
         });
     });
+
+
+    $(".status-box-sales-manager").click(function (e) {
+        let inquiry_status = $(this).data('status');
+        $('.bmro-same-box').removeClass('bmro-same-box-active');
+        $(this).addClass('bmro-same-box-active', 1000);
+        e.preventDefault();
+        $.ajax({
+            url: Routes.get_filtered_inquiries_overseers_dashboard_path({format: "html"}),
+            type: "GET",
+            data: {
+                status: inquiry_status
+            },
+            success: function (data) {
+                $('.inquiries-card').empty();
+                $('.inquiries-card').append(data);
+                getInquiryTasks();
+                getStatusRecords();
+            },
+        });
+    });
 }
 
 export default statusBox;
