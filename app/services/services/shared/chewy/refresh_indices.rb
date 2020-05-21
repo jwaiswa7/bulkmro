dc78349c8e48343219cc444a81ec102f4a4155e6
MeenakshiNaik <meenu688naik@gmail.com>
2020-05-21 19:58:12 +0530

    Script added to remove outdated indices + modified cron

--- app/services/services/shared/chewy/refresh_indices.rb
+++ app/services/services/shared/chewy/refresh_indices.rb
@@ -3,6 +3,8 @@ require 'rake'
 class Services::Shared::Chewy::RefreshIndices < Services::Shared::BaseService
   def initialize
     Rails.application.class.load_tasks
+    service = Services::Shared::Chewy::RemoveOutdatedIndices.new
+    service.delete_outdated_indices
     Rake::Task['chewy:reset'].invoke
   end
 end
