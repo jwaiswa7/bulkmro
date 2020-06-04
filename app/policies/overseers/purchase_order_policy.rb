5488bfbf77e22ea6025907d9766a6d95fbce01a7
sakshi-yadav <sakshi.yadav@bulkmro.com>
2020-06-04 17:23:38 +0530

    policy condition modified

--- app/policies/overseers/purchase_order_policy.rb
+++ app/policies/overseers/purchase_order_policy.rb
@@ -104,6 +104,6 @@ class Overseers::PurchaseOrderPolicy < Overseers::ApplicationPolicy
     true
   end
   def manually_close?
-    developer? && !(record.material_status == 'Manually Closed')
+    developer? && (record.material_status != 'Manually Closed')
   end
 end
