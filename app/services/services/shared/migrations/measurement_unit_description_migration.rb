da2ea5e32a4b8ca3a13276a479bbf669c24f813f
sakshi-yadav <sakshi.yadav@bulkmro.com>
2020-06-08 12:23:00 +0530

    migration script written to description for existing UOM and field also added to form

---
+++ app/services/services/shared/migrations/measurement_unit_description_migration.rb
@@ -0,0 +1,23 @@
+class Services::Shared::Migrations::MeasurementUnitDescriptionMigration < Services::Shared::Migrations::Migrations
+  def update_measurement_unit
+    service = Services::Shared::Spreadsheets::CsvImporter.new('measurement_unit.csv', 'seed_files_3')
+    MeasurementUnit.all.each do |measurement_unit|
+      service.loop(nil) do |x|
+        if measurement_unit.name == (x.get_column('name'))
+          bi = MeasurementUnit.where(name: x.get_column('name')).last
+          bi.description = x.get_column('description')
+          bi.save
+        end
+      end
+    end
+  end
+
+  def missing_update
+    missing_uom_names =   MeasurementUnit.where(description: nil).pluck(:name)
+    missing_uom_names.each do |name|
+      bi = MeasurementUnit.where(name: name).last
+      bi.description = name
+      bi.save
+    end
+  end
+end
