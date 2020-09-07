3fb853ce9d76faf4bb48d113d0e748062b710bd9
Sourabh Raje <sourabh.raje@bulkmro.com>
2020-09-07 14:23:14 +0530

    association changes in model and few changes in migrations

--- app/models/dc_row.rb
+++ app/models/dc_row.rb
@@ -1,12 +1,7 @@
 class DcRow < ApplicationRecord
   belongs_to :delivery_challan, class_name: 'DeliveryChallan'
-  belongs_to :sales_order_row, class_name: 'SalesOrderRow'
+  belongs_to :inquiry_product
+  belongs_to :product
 
   validates_numericality_of :quantity, greater_than: 0, allow_nil: true
-  delegate :converted_unit_selling_price, to: :sales_order_row, allow_nil: true
-  delegate :best_tax_code, to: :sales_order_row, allow_nil: true
-  delegate :best_tax_rate, to: :sales_order_row, allow_nil: true
-  delegate :unit_selling_price, to: :sales_order_row, allow_nil: true
-  delegate :unit_selling_price_with_tax, to: :sales_order_row, allow_nil: true
-
 end
--- db/migrate/20200616113911_create_dc_rows.rb
+++ db/migrate/20200616113911_create_dc_rows.rb
@@ -1,10 +1,14 @@
 class CreateDcRows < ActiveRecord::Migration[5.2]
   def change
     create_table :dc_rows do |t|
-      t.references :sales_order_row, foreign_key: true
+      t.references :inquiry_product, foreign_key: true
+      t.references :product, foreign_key: true
 
+      t.integer :sr_no
       t.integer :quantity
-      t.integer :product_id
+
+      t.timestamps
+      t.userstamps
     end
   end
 end
