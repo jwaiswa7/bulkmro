23837b3a75651941b060fd0242fa9b6d2062021e
Sourabh Raje <sourabh.raje@bulkmro.com>
2020-09-07 13:18:38 +0530

    reset indentation

--- app/models/delivery_challan.rb
+++ app/models/delivery_challan.rb
@@ -1,17 +1,17 @@
 class DeliveryChallan < ApplicationRecord
-    include Mixins::CanBeStamped
+  include Mixins::CanBeStamped
 
-    belongs_to :inquiry
-    belongs_to :sales_order
-    has_many :rows, class_name: 'DcRows'
+  belongs_to :inquiry
+  belongs_to :sales_order
+  has_many :rows, class_name: 'DcRows'
   
-    has_one_attached :customer_request_attachment
+  has_one_attached :customer_request_attachment
 
-    enum reason: { 
-        'Urgent Delivery of Goods and AR invoice not ready': 10,
-        'Multiple delivery of goods against single AR Invoice': 20,
-        'Urgent Delivery of Goods and SO not ready': 30,
-        'Free Samples to be delivered': 40,
-        'Partial Delivery of missed out goods on previous delivery': 50
-    }
+  enum reason: { 
+    'Urgent Delivery of Goods and AR invoice not ready': 10,
+    'Multiple delivery of goods against single AR Invoice': 20,
+    'Urgent Delivery of Goods and SO not ready': 30,
+    'Free Samples to be delivered': 40,
+    'Partial Delivery of missed out goods on previous delivery': 50
+  }
 end
