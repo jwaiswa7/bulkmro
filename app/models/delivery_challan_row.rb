44489543f6afb1488d9a21ff383d4138405fdde7
sakshi-yadav <sakshi.yadav@bulkmro.com>
2020-09-11 13:48:00 +0530

    preview page dynamic

--- app/models/delivery_challan_row.rb
+++ app/models/delivery_challan_row.rb
@@ -5,6 +5,7 @@ class DeliveryChallanRow < ApplicationRecord
   belongs_to :inquiry_product
   belongs_to :product
   belongs_to :sales_order_row
+  delegate :sales_quote_row, to: :sales_order_row
 
   validates_numericality_of :quantity, greater_than: 0, allow_nil: true
 end
