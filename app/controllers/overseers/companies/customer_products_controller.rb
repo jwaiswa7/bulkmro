efb92f3313e29779b9873ceaf6a72c664c390c9c
Sourabh Raje <sourabh.raje@bulkmro.com>
2020-10-01 17:40:01 +0530

    fix destroy catalog issue and add new fields to the customer product excel template

--- app/controllers/overseers/companies/customer_products_controller.rb
+++ app/controllers/overseers/companies/customer_products_controller.rb
@@ -54,7 +54,11 @@ class Overseers::Companies::CustomerProductsController < Overseers::Companies::B
 
   def destroy_all
     authorize_acl :customer_product
-    @company.customer_products.destroy_all
+    @company.customer_products.each do |customer_product|
+      unless customer_product.customer_order_rows.exists?
+        customer_product.destroy
+      end
+    end
 
     redirect_to overseers_company_path(@company)
   end
