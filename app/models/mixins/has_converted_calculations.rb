ae586bccecc68b896c7ca0cf1e4d49902401b9ef
sakshi-yadav <sakshi.yadav@bulkmro.com>
2020-11-04 11:43:53 +0530

    service written to make tcs_applicable false for given service products

--- app/models/mixins/has_converted_calculations.rb
+++ app/models/mixins/has_converted_calculations.rb
@@ -13,7 +13,9 @@ module Mixins::HasConvertedCalculations
     def calculated_total_with_tax
       rows.map { |row| row.total_selling_price_with_tax }.compact.sum.round(2)
     end
-
+    def calculated_total_tcs_without_service
+      rows.map { |row| row.total_selling_price_with_tax }.compact.sum.round(2)
+    end
     def calculated_total_margin
       rows.map { |row| row.total_margin }.compact.sum.round(2)
     end
