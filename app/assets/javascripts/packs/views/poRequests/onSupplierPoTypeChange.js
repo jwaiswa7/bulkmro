b11834abd4afd75bc72cc86a2e7c88bc2257195e
jigarbmro <jigar.shah@bulkmro.com>
2021-02-10 13:44:57 +0530

    Dropship and Route Through option related bug fixed.

--- app/assets/javascripts/packs/views/poRequests/onSupplierPoTypeChange.js
+++ app/assets/javascripts/packs/views/poRequests/onSupplierPoTypeChange.js
@@ -1,3 +1,5 @@
+import select2s from "../../components/select2s";
+
 const onSupplierPoTypeChange = () => {
     $("[id^='drop-ship-address']").show();
     $("[id^='warehouse-addresses']").find('select').attr('required', false);
@@ -8,7 +10,10 @@ const onSupplierPoTypeChange = () => {
         let supplierPoType = $(this).val();
         let warehouseAddressElement = $('#warehouse-addresses-'+supplierId).find('select');
         let dropshipAddressElement = $('#drop-ship-address-'+supplierId).find('select');
-
+        let inquiryID = $('.supplier-po-type').data('inquiry-id');
+        let shipToElem = $('form').find('select[name*=ship_to_id]');
+        shipToElem.empty();
+        
         if (supplierPoType == 'Drop Ship') {
             $('#warehouse-addresses-'+supplierId).hide();
             warehouseAddressElement.attr('disabled', true);
@@ -27,7 +32,24 @@ const onSupplierPoTypeChange = () => {
             dropshipAddressElement.attr('required', false);
         }
 
-    });
-
+        if (supplierPoType != '' && ['Drop Ship', 'Route Through'].includes(supplierPoType)) {
+            $.get({
+                url: Routes.dropbox_ship_to_overseers_inquiry_po_requests_path(inquiryID),
+                data: {
+                    drop_ship: true,
+                    inquiry_id: inquiryID
+                },
+                success: function (response) {
+                    shipToElem.attr('data-source', Routes.autocomplete_overseers_company_addresses_path(response.company_id)).select2('destroy');
+                    select2s();
+                    var option = new Option(response.address, response.address_id, true, true);
+                    shipToElem.append(option).trigger('change');
+                }
+            });
+        }else {
+            shipToElem.attr('data-source', Routes.autocomplete_overseers_warehouses_path()).select2('destroy');
+            select2s();
+        }
+    }); 
 };
 export default onSupplierPoTypeChange
