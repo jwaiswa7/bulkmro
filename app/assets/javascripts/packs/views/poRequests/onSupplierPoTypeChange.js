05b2de36edbef1a61e9383001bc33a1ca22d709a
jigarbmro <jigar.shah@bulkmro.com>
2021-02-10 17:27:02 +0530

    purchase order request creation from sales order bug fixed.

--- app/assets/javascripts/packs/views/poRequests/onSupplierPoTypeChange.js
+++ app/assets/javascripts/packs/views/poRequests/onSupplierPoTypeChange.js
@@ -6,32 +6,11 @@ const onSupplierPoTypeChange = () => {
     $("[id^='warehouse-addresses']").find('select').attr('disabled', true);
 
     $('form').on('change', 'select[name*=supplier_po_type]', function (e) {
-        let supplierId = parseInt($(this).data('supplier-id'));
         let supplierPoType = $(this).val();
-        let warehouseAddressElement = $('#warehouse-addresses-'+supplierId).find('select');
-        let dropshipAddressElement = $('#drop-ship-address-'+supplierId).find('select');
         let inquiryID = $('.supplier-po-type').data('inquiry-id');
         let shipToElem = $('form').find('select[name*=ship_to_id]');
         shipToElem.empty();
         
-        if (supplierPoType == 'Drop Ship') {
-            $('#warehouse-addresses-'+supplierId).hide();
-            warehouseAddressElement.attr('disabled', true);
-            warehouseAddressElement.attr('required', false);
-
-            $('#drop-ship-address-'+supplierId).show();
-            dropshipAddressElement.removeAttr('disabled');
-            dropshipAddressElement.attr('required', true);
-        } else {
-            $('#warehouse-addresses-'+supplierId).show();
-            warehouseAddressElement.removeAttr('disabled');
-            warehouseAddressElement.attr('required', true);
-
-            $('#drop-ship-address-'+supplierId).hide();
-            dropshipAddressElement.attr('disabled', true);
-            dropshipAddressElement.attr('required', false);
-        }
-
         if (supplierPoType != '' && ['Drop Ship', 'Route Through'].includes(supplierPoType)) {
             $.get({
                 url: Routes.dropbox_ship_to_overseers_inquiry_po_requests_path(inquiryID),
