fe3da6636b2e207309b687b27b9e5c5a0c155ced
jigarbmro <jigar.shah@bulkmro.com>
2021-03-30 15:52:09 +0530

    CallbackRequest added for CreditNote Creation and Update.

--- app/services/services/callbacks/credit_notes/create.rb
+++ app/services/services/callbacks/credit_notes/create.rb
@@ -4,12 +4,23 @@ class Services::Callbacks::CreditNotes::Create < Services::Callbacks::Shared::Ba
 
     if sales_invoice.present?
       credit_note = sales_invoice.credit_note || sales_invoice.build_credit_note(memo_number: params['memo_number'], memo_date: params['memo_date'].to_date, memo_amount: params['memo_amount'], metadata: params['metadata'])
+      method = credit_note.persisted? ? :patch : :post
+      @callback_request = CallbackRequest.where(resource: 'CreditNote', method: method, subject_id: params['memo_number']).first_or_create do |cr|
+        cr.update(
+          url: '/CreditNote',
+          status: :pending,
+          subject_type: CreditNote
+        )
+      end
+
       if credit_note.persisted?
         credit_note.update_attributes!(memo_date: params['memo_date'].to_date, memo_amount: params['memo_amount'], metadata: params['metadata'])
-        return_response('AR Credit Note updated successfully.')
+        @callback_request.update_attributes(request: credit_note.to_json)
+        return_response('AR Credit Note updated successfully.', 1)
       else
         credit_note.save!
-        return_response('AR Credit Note created successfully.')
+        @callback_request.update_attributes(request: credit_note.to_json)
+        return_response('AR Credit Note created successfully.', 1)
       end
       status = sales_invoice.rows.count == credit_note.metadata['lineItems'].count ? 209 : 208
       sales_invoice.update_attributes(status: status)
