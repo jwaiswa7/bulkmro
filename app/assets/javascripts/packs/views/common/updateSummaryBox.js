4234122ff295ec83005d37d3385ba5936e3f302b
amm-bulk <ammad.tariq@bulkmro.com>
2022-07-05 18:30:29 +0500

    Add total without tax amout

--- app/assets/javascripts/packs/views/common/updateSummaryBox.js
+++ app/assets/javascripts/packs/views/common/updateSummaryBox.js
@@ -22,7 +22,9 @@ const updateSummaryBox = () => {
         $.each(json.recordsTotalValue, function (index, total_value) {
             let statusId = ".status-" + index;
             $(statusId).find('.status-value-' + index).html(currencyName + new Intl.NumberFormat('en-US', {maximumFractionDigits: 0}).format(total_value));
-        })
+        });
+
+        $('.status-value-without-tax').html(currencyName + new Intl.NumberFormat('en-US', {maximumFractionDigits: 0}).format(json.recordsTotalValue_without_tax));
     });
 };
 
