ab7c5885f7a8abd4121ac60191f9213b74e286af
Dev Singh <techiedev1987@gmail.com>
2022-09-01 20:33:27 +0530

    Import invoice and credit notes data

--- app/services/services/shared/migrations/invoice_data_import.rb
+++ app/services/services/shared/migrations/invoice_data_import.rb
@@ -1,6 +1,7 @@
 class Services::Shared::Migrations::InvoiceDataImport < Services::Shared::Migrations::Migrations
 	def import_invoice_data
 		service = Services::Shared::Spreadsheets::CsvImporter.new('invoice_data.csv', 'seed_files_3')
+		@dup_invoice_number = 0
 		service.loop(nil) do |x|
 			invoice_number = x.get_column('Invoice number')
 			state = x.get_column('State')
@@ -19,34 +20,47 @@ class Services::Shared::Migrations::InvoiceDataImport < Services::Shared::Migrat
 			mis_date = x.get_column('MIS date')
 			billing_address_id = x.get_column('Billing address id')
 			shipping_address_id = x.get_column('Shipping address id')
-
-			sales_order = SalesOrder.find_by(order_number: order_id.to_i)
       
-			if order_id != '221110800'
-				invoice = SalesInvoice.new
-				invoice.sales_order_id = sales_order.id if sales_order.present?
-				invoice.invoice_number = invoice_number.to_i
-				invoice.metadata["state"] = state.to_s
-				invoice.metadata["ItemLine"][0]["sku"] = sku.to_s
-				invoice.metadata["ItemLine"][0]["product_id"] = product_id.to_s
-				invoice.metadata["ItemLine"][0]["description"] = description.to_s
-				invoice.metadata["ItemLine"][0]["qty"] = qty.to_f
-				invoice.metadata["ItemLine"][0]["price"] = price.to_f
-				invoice.metadata["ItemLine"][0]["tax_code"] = tax_code.to_i
-				invoice.metadata["ItemLine"][0]["row_total"] = row_total.to_f
-				invoice.metadata["ItemLine"][0]["base_row_total"] = row_total.to_f
-				invoice.metadata["ItemLine"][0]["tax_amount"] = tax_amount.to_f
-				invoice.metadata["ItemLine"][0]["base_tax_amount"] = tax_amount.to_f
-				invoice.metadata["ItemLine"][0]["row_total_incl_tax"] = row_total_incl_tax.to_f
-				invoice.metadata["ItemLine"][0]["base_row_total_incl_tax"] = row_total_incl_tax.to_f
-				invoice.metadata["order_id"] = order_id.to_s
-				invoice.metadata["bill_from"] = bill_from.to_s
-				invoice.metadata["created_at"] = created_at.to_s
-				invoice.created_at = DateTime.parse created_at.to_s
-				invoice.mis_date =  Date.parse mis_date.to_s
-				invoice.metadata["billing_address_id"] = billing_address_id.to_i
-				invoice.metadata["shipping_address_id"] = shipping_address_id.to_i
-				invoice.save(validate: false)
+      sales_invoice = SalesInvoice.find_by(invoice_number: invoice_number.to_i)
+      
+			unless sales_invoice.present?
+				sales_order = SalesOrder.find_by(order_number: order_id.to_i)
+
+				item_hash = Hash.new
+				item_hash["sku"] = sku.to_s
+				item_hash["product_id"] = product_id.to_s
+				item_hash["description"] = description.to_s
+				item_hash["qty"] = qty.to_f
+				item_hash["price"] = price.to_f
+				item_hash["tax_code"] = tax_code.to_i
+				item_hash["row_total"] = row_total.to_f
+				item_hash["base_row_total"] = row_total.to_f
+				item_hash["tax_amount"] = tax_amount.to_f
+				item_hash["base_tax_amount"] = tax_amount.to_f
+				item_hash["row_total_incl_tax"] = row_total_incl_tax.to_f
+				item_hash["base_row_total_incl_tax"] = row_total_incl_tax.to_f
+
+				if @dup_invoice_number == invoice_number
+					sales_invoice.metadata["ItemLine"] << item_hash
+				  sales_invoice.save(validate: false)
+				else
+					invoice = SalesInvoice.new
+					invoice.sales_order_id = sales_order.id if sales_order.present?
+					invoice.invoice_number = invoice_number.to_i
+					invoice.metadata = Hash.new
+					invoice.metadata["state"] = state.to_s
+					invoice.metadata["ItemLine"] = []
+					invoice.metadata["ItemLine"] << item_hash
+					invoice.metadata["order_id"] = order_id.to_s
+					invoice.metadata["bill_from"] = bill_from.to_s
+					invoice.metadata["created_at"] = created_at.to_s
+					invoice.created_at = DateTime.parse created_at.to_s
+					invoice.mis_date =  Date.parse mis_date.to_s
+					invoice.metadata["billing_address_id"] = billing_address_id.to_i
+					invoice.metadata["shipping_address_id"] = shipping_address_id.to_i
+				  invoice.save(validate: false)
+					@dup_invoice_number = invoice_number
+				end
 			end
 		end
 	end
