7f7a4d7d3d90e6f0bd0ca2b784c9bfa8eff90c24
Dev Singh <techiedev1987@gmail.com>
2022-09-01 17:52:40 +0530

    Script run failed fix

--- app/services/services/shared/migrations/invoice_data_import.rb
+++ app/services/services/shared/migrations/invoice_data_import.rb
@@ -22,31 +22,32 @@ class Services::Shared::Migrations::InvoiceDataImport < Services::Shared::Migrat
 
 			sales_order = SalesOrder.find_by(order_number: order_id.to_i)
       
-			invoice = SalesInvoice.new
-			invoice.sales_order_id = sales_order.id if sales_order.present?
-			invoice.invoice_number = invoice_number.to_i
-			invoice.metadata["state"] = state.to_s
-			invoice.metadata["ItemLine"][0]["sku"] = sku.to_s
-			invoice.metadata["ItemLine"][0]["product_id"] = product_id.to_s
-			invoice.metadata["ItemLine"][0]["description"] = description.to_s
-			invoice.metadata["ItemLine"][0]["qty"] = qty.to_f
-			invoice.metadata["ItemLine"][0]["price"] = price.to_f
-			invoice.metadata["ItemLine"][0]["tax_code"] = tax_code.to_i
-			invoice.metadata["ItemLine"][0]["row_total"] = row_total.to_f
-			invoice.metadata["ItemLine"][0]["base_row_total"] = row_total.to_f
-			invoice.metadata["ItemLine"][0]["tax_amount"] = tax_amount.to_f
-			invoice.metadata["ItemLine"][0]["base_tax_amount"] = tax_amount.to_f
-			invoice.metadata["ItemLine"][0]["row_total_incl_tax"] = row_total_incl_tax.to_f
-			invoice.metadata["ItemLine"][0]["base_row_total_incl_tax"] = row_total_incl_tax.to_f
-			invoice.metadata["order_id"] = order_id.to_s
-			invoice.metadata["bill_from"] = bill_from.to_s
-			invoice.metadata["created_at"] = created_at.to_s
-			invoice.created_at = DateTime.parse created_at.to_s
-			invoice.mis_date =  Date.parse mis_date.to_s
-			invoice.metadata["billing_address_id"] = billing_address_id.to_i
-			invoice.metadata["shipping_address_id"] = shipping_address_id.to_i
-			invoice.save(validate: false)
-			
+			if order_id != '221110800'
+				invoice = SalesInvoice.new
+				invoice.sales_order_id = sales_order.id if sales_order.present?
+				invoice.invoice_number = invoice_number.to_i
+				invoice.metadata["state"] = state.to_s
+				invoice.metadata["ItemLine"][0]["sku"] = sku.to_s
+				invoice.metadata["ItemLine"][0]["product_id"] = product_id.to_s
+				invoice.metadata["ItemLine"][0]["description"] = description.to_s
+				invoice.metadata["ItemLine"][0]["qty"] = qty.to_f
+				invoice.metadata["ItemLine"][0]["price"] = price.to_f
+				invoice.metadata["ItemLine"][0]["tax_code"] = tax_code.to_i
+				invoice.metadata["ItemLine"][0]["row_total"] = row_total.to_f
+				invoice.metadata["ItemLine"][0]["base_row_total"] = row_total.to_f
+				invoice.metadata["ItemLine"][0]["tax_amount"] = tax_amount.to_f
+				invoice.metadata["ItemLine"][0]["base_tax_amount"] = tax_amount.to_f
+				invoice.metadata["ItemLine"][0]["row_total_incl_tax"] = row_total_incl_tax.to_f
+				invoice.metadata["ItemLine"][0]["base_row_total_incl_tax"] = row_total_incl_tax.to_f
+				invoice.metadata["order_id"] = order_id.to_s
+				invoice.metadata["bill_from"] = bill_from.to_s
+				invoice.metadata["created_at"] = created_at.to_s
+				invoice.created_at = DateTime.parse created_at.to_s
+				invoice.mis_date =  Date.parse mis_date.to_s
+				invoice.metadata["billing_address_id"] = billing_address_id.to_i
+				invoice.metadata["shipping_address_id"] = shipping_address_id.to_i
+				invoice.save(validate: false)
+			end
 		end
 	end
 
