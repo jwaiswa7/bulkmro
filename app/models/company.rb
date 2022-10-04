6726331180f75d31d39eb12c70f89471b936f9ae
Joshua Waiswa <jwaiswa7@gmail.com>
2022-10-04 11:37:49 +0300

    Create generate catalog worker

--- app/models/company.rb
+++ app/models/company.rb
@@ -216,23 +216,7 @@ class Company < ApplicationRecord
   end
 
   def generate_catalog(overseer)
-    inquiry_products = Inquiry.includes(:inquiry_products, :products).where(company_id: self.id).map { |i| i.inquiry_products }.flatten
-    inquiry_products.each do |inquiry_product|
-      if inquiry_product.product.synced?
-        CustomerProduct.where(company_id: self.id, product_id: inquiry_product.product_id).first_or_create do |customer_product|
-          customer_product.category_id = inquiry_product.product.category_id
-          customer_product.brand_id = inquiry_product.product.brand_id
-          customer_product.name = (inquiry_product.bp_catalog_name == '' ? nil : inquiry_product.bp_catalog_name) || inquiry_product.product.name
-          customer_product.inquiry_product_sku = (inquiry_product.bp_catalog_sku == '' ? nil : inquiry_product.bp_catalog_sku) || inquiry_product.product.sku
-          customer_product.tax_code = inquiry_product.product.best_tax_code
-          customer_product.tax_rate = inquiry_product.best_tax_rate
-          customer_product.measurement_unit = inquiry_product.product.measurement_unit
-          customer_product.moq = 1
-          customer_product.created_by = overseer
-          customer_product.customer_price = (inquiry_product.product.latest_unit_cost_price || 0)
-        end
-      end
-    end
+    GenerateCatalogWorker.perform_async(self.id, overseer.id)
   end
 
   def customer_product_for(product)
