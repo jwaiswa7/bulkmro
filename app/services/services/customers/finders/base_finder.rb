ec0f4dcc15cd07bbb4e5719969888cfe862d430d
jd-bulkmro <jawad.akram@bulkmro.com>
2022-10-12 19:53:48 +0500

    fix sort issues

--- app/services/services/customers/finders/base_finder.rb
+++ app/services/services/customers/finders/base_finder.rb
@@ -48,7 +48,11 @@ class Services::Customers::Finders::BaseFinder < Services::Shared::BaseService
     end
 
     @indexed_records = non_paginated_records.page(page).per(per) if non_paginated_records.present?
-    @records = model_klass.where(id: indexed_records.pluck(:id)).with_includes if indexed_records.present?
+    if @indexed_records.size > 0
+      @records = model_klass.find_ordered(indexed_records.pluck(:id)).with_includes if @indexed_records.present?
+    else
+      @records = model_klass.none
+    end
   end
 
 
