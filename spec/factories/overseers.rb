d46ea46fdd94543694ad88ff76cb068291284d63
Joshua Waiswa <jwaiswa7@gmail.com>
2022-10-25 12:48:33 +0300

    Sign in overseer

--- spec/factories/overseers.rb
+++ spec/factories/overseers.rb
@@ -3,5 +3,10 @@ FactoryBot.define do
     email { "#{Faker::Internet.username}@bulkmro.com" }
     password { "password" }
     password_confirmation { "password" }
+
+    factory :admin_overseer do 
+      role { "admin" }
+      is_super_admin { true }
+    end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # devise sign in 
+  config.include Devise::Test::IntegrationHelpers, type: :request
 end
