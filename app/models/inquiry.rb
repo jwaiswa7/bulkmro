724a2defd2c44443121c68b932e45a6c2622a043
amm-bulk <ammad.tariq@bulkmro.com>
2022-10-31 15:49:32 +0500

    Update Inquiry Terms and Conditions

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -332,8 +332,9 @@ class Inquiry < ApplicationRecord
           '1. Cost does not include any additional certification if required as per Indian regulations.',
           '2. Any errors in quotation including HSN codes, GST Tax rates must be notified before placing order.',
           '3. Order once placed cannot be changed.',
-          '4. BulkMRO does not accept any financial penalties for late deliveries.',
-          '5. Warranties are applicable as per OEM\'s Standard warranty only.'
+          '4. Bulk MRO does not accept any financial penalties for late deliveries.',
+          '5. Warranties are applicable as per OEM\'s Standard warranty only.',
+          '6. Warranty against manufacturing defects only. Damages due to mishandling and wear and tear are not covered. In case of a warranty claim, the manufacturer\'s decision will be considered final.'
       ].join("\r\n") if not_legacy?
       self.stage ||= 1
     end
