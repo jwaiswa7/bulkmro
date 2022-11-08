64d71fc755c35e6bcf30b3041c46ce1e2b51fb1a
jd-bulkmro <jawad.akram@bulkmro.com>
2022-11-08 21:38:37 +0500

    change text field to dropdown on material readiness page

--- app/assets/javascripts/packs/views/purchaseOrders/editMaterialFollowup.js
+++ app/assets/javascripts/packs/views/purchaseOrders/editMaterialFollowup.js
@@ -12,6 +12,27 @@ const editMaterialFollowup = () => {
     $('.followup-date').on("focus", function () {
         disableBackdateOption( $('.followup-date'));
     });
+
+    if ($('#po_message').select2('data')[0].text == "Others") {
+        $("#po_other_message").prop("required", true);
+        $("#po_other_message").prop("disabled", false);
+    }
+    else{
+        $("#po_other_message").val('');
+    }
+
+    $('#po_message').on('select2:select', function (e) {
+        if ($(this).select2('data')[0].text != "Others") {
+          $("#po_other_message").val('');
+          $("#po_other_message").prop("disabled", true);
+          $("#po_other_message").attr("required", false);
+          $("#po_other_message").parent().children().removeClass('is-invalid');
+          $("#po_other_message").parent().find('span .select2-selection').removeClass('is-invalid');
+        } else {
+          $("#po_other_message").prop("required", true);
+          $("#po_other_message").prop("disabled", false);
+        }
+      });
 }
 
 export default editMaterialFollowup
