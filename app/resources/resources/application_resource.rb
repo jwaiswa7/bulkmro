674bed97b711c519a62e8f0136314f28a744d3fa
Dev Singh <techiedev1987@gmail.com>
2023-01-23 20:54:50 +0530

    Use URL instead of IP address in requests to SAP

--- app/resources/resources/application_resource.rb
+++ app/resources/resources/application_resource.rb
@@ -5,7 +5,7 @@ class Resources::ApplicationResource
 
   def self.new_session_id
      response = HTTParty.post(
-       'http://sap-api.bulkmro.com/b1s/v1/Login',
+       'https://sap-api.bulkmro.com:50000/b1s/v1/Login',
          body: {CompanyDB: Settings.sap.DATABASE, UserName: Settings.sap.USERNAME, Password: Settings.sap.PASSWORD}.to_json,
          verify: false,
          debug_output: $stdout,
