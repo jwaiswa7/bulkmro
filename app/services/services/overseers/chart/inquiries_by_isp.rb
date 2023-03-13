5149c11940dee9e4cce5719c95ac6ead86fe1ef1
Joshua Waiswa <jwaiswa7@gmail.com>
2023-03-13 12:28:26 +0300

    Update the bar graph

--- app/services/services/overseers/chart/inquiries_by_isp.rb
+++ app/services/services/overseers/chart/inquiries_by_isp.rb
@@ -1,4 +1,8 @@
 class Services::Overseers::Chart::InquiriesByIsp
+
+  def initialize 
+    @labels = Overseer.first(29).map(&:name)
+  end
   
 
   def call 
@@ -9,57 +13,51 @@ class Services::Overseers::Chart::InquiriesByIsp
   end
 
   private 
+  attr_accessor :labels 
+
+  def data_set 
+    labels.map{|label| rand(10..100)}
+  end
+
+  def first_colors 
+    labels.map {|label| 'rgba(137,219,253,255)'}
+  end
+
+  def second_colors 
+    labels.map {|label| 'rgba(176,122,106,255)'}
+  end
+
   def data 
     {
-      labels: ["Amit Goyal", "Ashwin Goyal", "Creative Bulkmro", "Jeetendra Sharma", "Sarika Tanawade", "Swati Bhosale"],
+      labels: labels,
       datasets: [{
         axis: 'y',
         label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
+        data: data_set,
         fill: false,
-        backgroundColor: [
-          'rgba(137,219,253,255)',
-          'rgba(137,219,253,255)',
-          'rgba(137,219,253,255)',
-          'rgba(137,219,253,255)',
-          'rgba(137,219,253,255)',
-          'rgba(137,219,253,255)',
-          'rgba(137,219,253,255)'
-
-        ],
-        borderWidth: 1
+        backgroundColor: first_colors,
       },
       {
         axis: 'y',
         label: 'My second Dataset',
-        data: [83, 83, 72, 85, 43, 60, 86],
+        data:  data_set,
         fill: false,
-        backgroundColor: [
-          'rgba(176,122,106,255)',
-          'rgba(176,122,106,255)',
-          'rgba(176,122,106,255)',
-          'rgba(176,122,106,255)',
-          'rgba(176,122,106,255)',
-          'rgba(176,122,106,255)',
-          'rgba(176,122,106,255)'
-
-        ],
-        borderWidth: 1
+        backgroundColor: second_colors,
       }]
     };
   end
 
   def options 
     {
-      indexAxis: 'y',
+      height: 700,
       scales: {
-        x: {
-          stacked: true,
-        },
-        y: {
-          stacked: true
-        }
-      }
+        xAxes: [{
+            stacked: true
+        }],
+        yAxes: [{
+            stacked: true
+        }]
+    }
     }
   end
 end
