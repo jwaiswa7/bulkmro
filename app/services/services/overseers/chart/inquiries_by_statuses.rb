40296a63d1ac3a0459fe496615103de6275606a4
Joshua Waiswa <jwaiswa7@gmail.com>
2023-03-14 11:57:54 +0300

    Data for inquiries by status chart

--- app/services/services/overseers/chart/inquiries_by_statuses.rb
+++ app/services/services/overseers/chart/inquiries_by_statuses.rb
@@ -1,26 +1,57 @@
 class Services::Overseers::Chart::InquiriesByStatuses
   def initialize
-    @statuses = ["Order Won", "Quotation Sent", "Order Lost", "Acknowledgement Mail", "New Inquiry", "Regret Request", "Cross Reference", "Preparing Quotation", "SO Draft: Pending", "Rejected by Accounts", "Expected Order"]
-    @values =  [34, 48, 31, 35, 50, 31, 23, 35, 20, 44, 25]
+    @inquiries = Inquiry.select(:status).group(:status).count
   end
 
-  def call 
-    
-    colors =  ["rgba(137,219,253,255)", "rgba(176,122,106,255)", "rgba(247,218,209,255)", "rgba(32,138,179,255)", "rgb(194,186,188)", "rgba(112,126,126,255)", "rgba(24,93,121,255)", "rgba(224,68,167,255)", "rgba(82,78,71,255)", "rgba(86,7,13,255)", "rgba(54,126,111,255)"]
-
+  def call
     {
+      data: data,
+      options: options
+    }
+  end
+
+  private
+
+    def data
+      {
         labels: statuses,
         datasets: [{
           label: 'My First Dataset',
           data: values,
           backgroundColor: colors,
+          borderWidth: border_width,
           hoverOffset: 4
         }]
-      };
-  end
+      }
+    end
 
-  private 
+    def options
+      {
+        height: '200px',
+        legend: { display: true, position: 'right' },
+      }
+    end
 
-  attr_accessor :statuses, :values
+    def statuses
+      inquiries.keys
+    end
+
+    def values
+      inquiries.values
+    end
+
+    def border_width
+      statuses.map { |status| 0}
+    end
+
+    def colors
+      [
+        'rgba(137,219,253,255)', 'rgba(176,122,106,255)', 'rgba(247,218,209,255)', 'rgba(32,138,179,255)',
+        'rgb(194,186,188)', 'rgba(112,126,126,255)', 'rgba(24,93,121,255)', 'rgba(224,68,167,255)', 'rgba(82,78,71,255)',
+        'rgba(86,7,13,255)', 'rgba(54,126,111,255)', 'rgb(25, 83, 95)', 'rgb(141, 153, 174)', 'rgb(78, 2, 80)',
+        'rgb(150, 230, 179)', 'rgb(218, 62, 82)', 'rgb(216, 17, 89)'
+      ]
+    end
+
+    attr_accessor :inquiries
 end
-  
