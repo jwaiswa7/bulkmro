63f0cb6d505d3e9456e98b3548f6c8ef634c4a6f
Joshua Waiswa <jwaiswa7@gmail.com>
2023-03-16 11:27:01 +0300

    Store code

--- app/services/services/overseers/chart/inquiries_by_isp.rb
+++ app/services/services/overseers/chart/inquiries_by_isp.rb
@@ -1,14 +1,18 @@
 class Services::Overseers::Chart::InquiriesByIsp
 
   def initialize 
-    @inquiries = Inquiry.select(:inside_sales_owner_id,:status).includes(:inside_sales_owner)
-    @statuses = Inquiry.statuses
-    @status_hash = Hash.new
+    # @inquiries = Inquiry.select(:inside_sales_owner_id,:status).includes(:inside_sales_owner)
+    # @statuses = Inquiry.statuses
+    # @status_hash = Hash.new
+    @inquiries = Inquiry.first(50)
   end
   
 
   def call 
-    status_series
+    {
+      data: data ,
+      options: options
+    }
   end
 
   private 
@@ -22,80 +26,71 @@ class Services::Overseers::Chart::InquiriesByIsp
     inquiries.map{|inquiry| inquiry.inside_sales_owner_id }.uniq
   end
 
-  def status_series
-    status_names = statuses.keys 
+  # def status_series
+  #   status_names = statuses.keys 
 
-    inside_sales_owner_ids.each_with_index do |user_id, id_index|
-      user_name = inside_sales_owners[id_index]
-      user_status_count = inquiries.where(inside_sales_owner_id: user_id).map{|x| x.status}.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
-      status_array = status_names.map{|status| 0 }
-      user_status_count.each do |k,v|
-        index = status_names.index(k)
-        next if index.nil?
-        status_array[index] = v
-      end
-      status_hash[user_name] = status_array
-    end
-    status_hash
-    # status_names = statuses.keys 
-    # user_status_count = inquiries.where(inside_sales_owner_id: user_id).map{|x| x.status}.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
-    # status_array = status_names.map{|status| 0 }
-    # user_status_count.each do |k,v|
-    #   index = status_names.index(k)
-    #   status_array[index] = v
-    # end
-    # status_hash[user_name] = status_array
-    # status_hash
+  #   inside_sales_owner_ids.each_with_index do |user_id, id_index|
+  #     user_name = inside_sales_owners[id_index]
+  #     user_status_count = inquiries.where(inside_sales_owner_id: user_id).map{|x| x.status}.group_by{|e| e}.map{|k, v| [k, v.length]}.to_h
+  #     status_array = status_names.map{|status| 0 }
+  #     user_status_count.each do |k,v|
+  #       index = status_names.index(k)
+  #       next if index.nil?
+  #       status_array[index] = v
+  #     end
+  #     status_hash[user_name] = status_array
+  #   end
+  #   status_hash
+  # end
+
+  def labels 
+    inquiries.map{ |inquiry| inquiry.inside_sales_owner.name }.uniq
   end
 
-  # def labels 
-  #   inquiries.map{ |inquiry| inquiry.inside_sales_owner.name }.uniq
-  # end
+  def data_set 
+    labels.map{|label| rand(10..100)}
+  end
 
-  # def data_set 
-  #   labels.map{|label| rand(10..100)}
-  # end
+  def first_colors 
+    labels.map {|label| 'rgba(137,219,253,255)'}
+  end
 
-  # def first_colors 
-  #   labels.map {|label| 'rgba(137,219,253,255)'}
-  # end
+  def second_colors 
+    labels.map {|label| 'rgba(176,122,106,255)'}
+  end
 
-  # def second_colors 
-  #   labels.map {|label| 'rgba(176,122,106,255)'}
-  # end
+  def data 
+    {
+      labels: labels,
+      datasets: [{
+        axis: 'y',
+        label: 'My First Dataset',
+        data: data_set,
+        fill: false,
+        backgroundColor: first_colors,
+      },
+      {
+        axis: 'y',
+        label: 'My second Dataset',
+        data:  data_set,
+        fill: false,
+        backgroundColor: second_colors,
+      }]
+    };
+  end
 
-  # def data 
-  #   {
-  #     labels: labels,
-  #     datasets: [{
-  #       axis: 'y',
-  #       label: 'My First Dataset',
-  #       data: data_set,
-  #       fill: false,
-  #       backgroundColor: first_colors,
-  #     },
-  #     {
-  #       axis: 'y',
-  #       label: 'My second Dataset',
-  #       data:  data_set,
-  #       fill: false,
-  #       backgroundColor: second_colors,
-  #     }]
-  #   };
-  # end
-
-  # def options 
-  #   {
-  #     height: 2500,
-  #     scales: {
-  #       xAxes: [{
-  #           stacked: true
-  #       }],
-  #       yAxes: [{
-  #           stacked: true
-  #       }]
-  #   }
-  #   }
-  # end
+  def options 
+    {
+      height: 700,
+      scales: {
+        xAxes: [{
+            stacked: true
+        }],
+        yAxes: [{
+            stacked: true
+        }]
+    }
+    }
+  end
 end
       
