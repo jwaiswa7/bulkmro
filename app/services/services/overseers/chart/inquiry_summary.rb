b5ec034181c12677637f5dcd6f95dd8b04b358b6
Joshua Waiswa <jwaiswa7@gmail.com>
2023-03-27 12:42:02 +0300

    Consider inquiries for the current financial year

--- app/services/services/overseers/chart/inquiry_summary.rb
+++ app/services/services/overseers/chart/inquiry_summary.rb
@@ -2,6 +2,7 @@ class Services::Overseers::Chart::InquirySummary
     def initialize
       @start_of_financial_year = Date.today.beginning_of_financial_year
       @end_of_financial_year = Date.today.end_of_financial_year
+      @inquiries = Inquiry.where(created_at: start_of_financial_year .. end_of_financial_year)
     end
 
     def call 
@@ -19,46 +20,48 @@ class Services::Overseers::Chart::InquirySummary
 
     private 
 
-    attr_accessor :start_of_financial_year, :end_of_financial_year
+    attr_accessor :start_of_financial_year, :end_of_financial_year, :inquiries
 
     def inquiry_count_order_won
-        Inquiry.where(status: 18).count
+        inquiries.where(status: 18).count
     end
 
     def inquiry_count
-        Inquiry.count
+        inquiries.count
     end
 
     def regret_count
-      Inquiry.where(status: 10).count
+        inquiries.where(status: 10).count
     end
 
     def lost_count 
-        Inquiry.where(status: 9).count
+        inquiries.where(status: 9).count
     end
 
     def inquiry_rejected_count 
         # SO Rejected by Sales Manager + Rejected by Accounts
-        Inquiry.where(status: 17).count + Inquiry.where(status: 19).count 
+        inquiries.where(status: 17).count + inquiries.where(status: 19).count 
     end
 
     def inquiry_rejection_rate
+        return nil if inquiry_rejected_count.zero?
         inquiry_count/inquiry_rejected_count
     end
 
     def conversion_rate 
+        return nil if inquiry_count.zero?
         ((num_order_won/inquiry_count.to_f)*100).round(2)
     end
 
     def num_order_won 
-        Inquiry.where(status: 18).count
+        inquiries.where(status: 18).count
     end
 
     def invoiced_count 
-        SalesInvoice.not_cancelled_invoices.count
+        SalesInvoice.where(created_at: start_of_financial_year .. end_of_financial_year).not_cancelled_invoices.count
     end
 
     def count_sales_quotes 
-        SalesQuote.count
+        SalesQuote.where(created_at: start_of_financial_year .. end_of_financial_year).count
     end
 end
