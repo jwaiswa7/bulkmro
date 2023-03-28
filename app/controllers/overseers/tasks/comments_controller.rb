945374e4f8d303c3c2af64ba9e7c712a98b6dc1d
Jawad Akram <jawad.akram@bulkmro.com>
2023-03-28 10:25:21 +0500

    Fixes in Task Comments

--- app/controllers/overseers/tasks/comments_controller.rb
+++ app/controllers/overseers/tasks/comments_controller.rb
@@ -4,13 +4,13 @@ class Overseers::Tasks::CommentsController < Overseers::Tasks::BaseController
   def index
     @comments = @task.comments.earliest
     @new_comment = @task.comments.build
-    authorize_acl @task
+    authorize_acl @comments
   end
 
   def create
     @comment = @task.comments.build(task_comment_params.merge(overseer: current_overseer))
     @comment.assign_attributes(message: "[#{@comment.tag_user.full_name}] #{task_comment_params[:message]}")
-    authorize_acl @task
+    authorize_acl @comment
     if @comment.save && @comment.tag_user.present?
         @notification.send_task_comment(
           @comment.tag_user_id,
