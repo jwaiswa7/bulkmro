129900826714804243089f6539cd1a0b9a5e9c47
Jawad Akram <jawad.akram@bulkmro.com>
2023-04-04 19:09:29 +0500

    Added Logistics Owner Filter on Invocies Page

--- app/chewy/invoice_requests_index.rb
+++ app/chewy/invoice_requests_index.rb
@@ -13,13 +13,13 @@ class InvoiceRequestsIndex < BaseIndex
       field :updated_by_name, value: -> (record) { record.updated_by.name if record.updated_by_id.present? }, analyzer: 'substring'
       field :logistics_owner_id, value: -> (record) { record.inquiry.company.logistics_owner_id if record.inquiry.present? }, type: 'integer'
       field :logistics_owner, value: -> (record) { record.inquiry.company.logistics_owner&.name if record.inquiry.present? && record.inquiry.company.present? }, analyzer: 'substring'
+      field :purchase_order_owner_id, value: -> (record) { record.purchase_order.logistics_owner_id if record.purchase_order.present? }, type: 'integer'
       field :is_owner_id, value: -> (record) { record.inquiry.inside_sales_owner_id if record.inquiry.present? }, type: 'integer'
       field :is_owner, value: -> (record) { record.inquiry.inside_sales_owner&.name if record.inquiry.present? }, analyzer: 'substring'
       field :created_at, type: 'date'
       field :updated_at, type: 'date'
       field :inside_sales_executive, value: -> (record) { record.inquiry.inside_sales_owner.id if record.inquiry.present? && record.inquiry.inside_sales_owner.present? }
       field :outside_sales_executive, value: -> (record) { record.inquiry.outside_sales_owner.id if record.inquiry.present? && record.inquiry.outside_sales_owner.present? }
-      field :procurement_operations, value: -> (record) { record.inquiry.procurement_operations_id if record.inquiry.present? && record.inquiry.procurement_operations.present? }  
+      field :procurement_operations, value: -> (record) { record.inquiry.procurement_operations_id if record.inquiry.present? && record.inquiry.procurement_operations.present? }
     end
   end
-  
