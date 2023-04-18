740b21dd06fef60a2ebb8194595f939e1e131ffb
Dev Singh <techiedev1987@gmail.com>
2023-04-18 15:02:43 +0530

    New sentry config

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,4 +1,4 @@
 # Initializing Sentry.io
 Raven.configure do |config|
-  config.dsn = 'https://4fafea922f6346d198b8e8a74cecf9a0:482ea4c356e54e9aa61a83223b041b2e@sentry.io/1291091'
+  config.dsn = 'https://78cda51270284ddeae9d1e9b89640bb5@o190863.ingest.sentry.io/4505034438213632'
 end
