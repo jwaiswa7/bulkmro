8b551063b25125135844993cb98871036d08469a
Dev Singh <techiedev1987@gmail.com>
2023-05-30 17:48:15 +0530

    Add missing index1

--- db/migrate/20230530082241_add_missing_index.rb
+++ db/migrate/20230530082241_add_missing_index.rb
@@ -1,6 +1,6 @@
 class AddMissingIndex < ActiveRecord::Migration[5.2]
   def change
-    add_index :customer_products, [:company_id, :sku], unique: false
-    add_index :company_reviews, [:company_id, :survey_type, :created_by_id], unique: false, name: 'index_on_company_review'
+    #add_index :customer_products, [:company_id, :sku], unique: false
+    #add_index :company_reviews, [:company_id, :survey_type, :created_by_id], unique: false, name: 'index_on_company_review'
   end
 end
