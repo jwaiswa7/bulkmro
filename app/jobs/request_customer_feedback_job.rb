b9dd06b49cae7c3919db1230b37b6ebb693af50c
Joshua Waiswa <jwaiswa7@gmail.com>
2023-06-08 09:59:02 +0300

    Disable customer feedback job

--- app/jobs/request_customer_feedback_job.rb
+++ app/jobs/request_customer_feedback_job.rb
@@ -4,6 +4,6 @@ class RequestCustomerFeedbackJob < ActiveJob::Base
   queue_as :default
 
   def perform(company_id)
-    Services::Customers::Feedback.new(company_id).call
+    # Services::Customers::Feedback.new(company_id).call
   end
 end
