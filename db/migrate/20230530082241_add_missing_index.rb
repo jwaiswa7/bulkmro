b1eb0a6362dcbbe520cb15189905b3b0da69bdb2
Dev Singh <techiedev1987@gmail.com>
2023-06-23 18:02:11 +0530

    reindex po_number

--- db/migrate/20230530082241_add_missing_index.rb
+++ db/migrate/20230530082241_add_missing_index.rb
@@ -3,5 +3,6 @@ class AddMissingIndex < ActiveRecord::Migration[5.2]
     #add_index :customer_products, [:company_id, :sku], unique: false
     #add_index :company_reviews, [:company_id, :survey_type, :created_by_id], unique: false, name: 'index_on_company_review'
     add_index :inquiries, :opportunity_uid, unique: false
+    add_index :purchase_orders, :po_number, unique: false
   end
 end
