c1463263b6d12dd780eeb3a60d73129792eb85bf
Joshua Waiswa <jwaiswa7@gmail.com>
2023-06-26 12:34:23 +0300

    Enable inquiry save and syn

--- app/services/services/resources/inquiries/save_and_sync.rb
+++ app/services/services/resources/inquiries/save_and_sync.rb
@@ -5,7 +5,7 @@ class Services::Resources::Inquiries::SaveAndSync < Services::Shared::BaseServic
 
   def call
     if inquiry.save
-      # perform_later(inquiry)
+      perform_later(inquiry)
     end
   end
 
@@ -21,11 +21,11 @@ class Services::Resources::Inquiries::SaveAndSync < Services::Shared::BaseServic
       inquiry.update_attributes(opportunity_uid: ::Resources::SalesOpportunity.create(inquiry))
     end
 
-    if inquiry.attachment_uid.present?
-      ::Resources::Attachment.update(inquiry.attachment_uid, inquiry)
-    elsif inquiry.has_attachment?
-      inquiry.update_attributes(attachment_uid: ::Resources::Attachment.create(inquiry))
-    end
+    # if inquiry.attachment_uid.present?
+    #   ::Resources::Attachment.update(inquiry.attachment_uid, inquiry)
+    # elsif inquiry.has_attachment?
+    #   inquiry.update_attributes(attachment_uid: ::Resources::Attachment.create(inquiry))
+    # end
 
     if inquiry.final_sales_quote.present?
       inquiry.final_sales_quote.save_and_sync
